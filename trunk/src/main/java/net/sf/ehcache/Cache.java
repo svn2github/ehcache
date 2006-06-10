9396080921f2695ece4a9c0eb2fd8980b6d17e34
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2006-06-10 07:22:13 +0000

    Added warning when Cache constructor is used.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@121 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/src/main/java/net/sf/ehcache/Cache.java
+++ trunk/src/main/java/net/sf/ehcache/Cache.java
@@ -256,6 +256,8 @@ public final class Cache implements Ehcache {
                  long diskExpiryThreadIntervalSeconds) {
         this(name, maxElementsInMemory, DEFAULT_MEMORY_STORE_EVICTION_POLICY, overflowToDisk, null,
                 eternal, timeToLiveSeconds, timeToIdleSeconds, diskPersistent, diskExpiryThreadIntervalSeconds, null, null);
+        LOG.warn("An API change between ehcache-1.1 results in the persistence path being set to java.io.tmp when the " +
+                "ehcache-1.1 constructor is used. Please change to the 1.2 constructor");
     }
 
 
