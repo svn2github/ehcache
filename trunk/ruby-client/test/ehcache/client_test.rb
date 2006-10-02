50549759a94f1e27b70ba8ef17a1af253a78fb98
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2006-10-02 07:15:27 +0000

    Move ruby client files to ruby-client
    git-svn-id: http://svn.terracotta.org/svn/ehcache@198 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ trunk/ruby-client/test/ehcache/client_test.rb
@@ -0,0 +1,20 @@
+require 'test/unit'
+require 'test/unit/assertions'
+
+
+class EhcacheTest
+
+
+end
+
+
+class EhcacheTest::GregTest < Test::Unit::TestCase
+
+    def test_call
+        assert_equal(1, 1)
+
+        dave = 12
+        puts "Dave #{dave}"
+    end
+
+end
