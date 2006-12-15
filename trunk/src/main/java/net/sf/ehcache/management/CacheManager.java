eb84ae19093d2a49b5c131f8262844ffba88779a
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2006-12-15 07:00:02 +0000

    Added some JMX tests
    git-svn-id: http://svn.terracotta.org/svn/ehcache@290 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/src/main/java/net/sf/ehcache/management/CacheManager.java
+++ trunk/src/main/java/net/sf/ehcache/management/CacheManager.java
@@ -19,31 +19,42 @@ package net.sf.ehcache.management;
 import net.sf.ehcache.Status;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author Greg Luck
  * @version $Id$
  */
-public interface CacheManagerMBean {
+public class CacheManager implements CacheManagerMBean {
 
+    private net.sf.ehcache.CacheManager cacheManager;
+
+    /**
+     * Create a management CacheManager
+     * @param cacheManager
+     */
+    public CacheManager(net.sf.ehcache.CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
 
     /**
      * Gets the status attribute of the Ehcache
      *
      * @return The status value from the Status enum class
      */
-    public Status getStatus();
+    public Status getStatus() {
+        return cacheManager.getStatus();
+    }
 
-
-
-     /**
+    /**
      * Shuts down the CacheManager.
-     * <p/>
-     * If the shutdown occurs on the singleton, then the singleton is removed, so that if a singleton access method
-     * is called, a new singleton will be created.
-     */
-    public void shutdown();
-
+    * <p/>
+    * If the shutdown occurs on the singleton, then the singleton is removed, so that if a singleton access method
+    * is called, a new singleton will be created.
+    */
+   public void shutdown() {
+        cacheManager.shutdown();
+    }
 
     /**
      * Clears  the contents of all caches in the CacheManager, but without
@@ -52,24 +63,37 @@ public interface CacheManagerMBean {
      * This method is not synchronized. It only guarantees to clear those elements in a cache
      * at the time that the {@link net.sf.ehcache.Ehcache#removeAll()} mehod  on each cache is called.
      */
-    public void clearAll();
-
+    public void clearAll() {
+        cacheManager.clearAll();
+    }
 
     /**
      * Returns a JMX Cache bean
-     *
      */
-    public Cache getCache(String name);
-
+    public Cache getCache(String name) {
+        return new Cache(cacheManager.getCache(name));
+    }
 
     /**
      * Gets the cache names managed by the CacheManager
      */
-    public String[] getCacheNames() throws IllegalStateException;
+    public String[] getCacheNames() throws IllegalStateException {
+        return cacheManager.getCacheNames();
+    }
 
     /**
      * Gets a list of caches in this CacheManager
+     *
      * @return a list of JMX Cache objects
      */
-    public List getCaches();
+    public List getCaches() {
+        List cacheList = new ArrayList();
+        String[] caches = getCacheNames();
+        for (int i = 0; i < caches.length; i++) {
+            String cacheName = caches[i];
+            Cache cache = getCache(cacheName);
+            cacheList.add(cache);
+        }
+        return cacheList;
+    }
 }
