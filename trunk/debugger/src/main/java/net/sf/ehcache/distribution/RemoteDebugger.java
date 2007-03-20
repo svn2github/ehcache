89ba24ee2471129fff3a156980e7d9167cbb3f62
maurotalevi <maurotalevi@b9324663-ca0f-0410-8574-be9b3887307d>
2007-03-20 14:45:49 +0000

    Created debugger module containing RemoteDebugger. Module also produced ueber jar containing all dependendencies bundled in a single jar. Updated Ant build script to use separate debugger module.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@439 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/debugger/src/main/java/net/sf/ehcache/distribution/RemoteDebugger.java
+++ trunk/debugger/src/main/java/net/sf/ehcache/distribution/RemoteDebugger.java
@@ -1,20 +1,5 @@
-/**
- *  Copyright 2003-2007 Greg Luck
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package net.sf.ehcache.distribution;
+
 
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Ehcache;
@@ -27,7 +12,7 @@ import org.apache.commons.logging.LogFactory;
  * It is passed the ehcache configuration to use and cache to monitor
  *
  * @author Greg Luck
- * @version $Id$
+ * @version $Id: RemoteDebugger.java 331 2007-01-29 09:38:41Z gregluck $
  */
 public final class RemoteDebugger {
     private static final int TWO_SECONDS = 2000;
