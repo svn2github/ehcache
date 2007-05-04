e7d43c326e43ffb931bf3aec66029557e324f3c8
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2007-05-04 08:24:14 +0000

    Release 1.3-beta2
    git-svn-id: http://svn.terracotta.org/svn/ehcache@482 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/debugger/src/main/java/net/sf/ehcache/distribution/RemoteDebugger.java
+++ trunk/debugger/src/main/java/net/sf/ehcache/distribution/RemoteDebugger.java
@@ -17,8 +17,6 @@ import org.apache.commons.logging.LogFactory;
 public final class RemoteDebugger {
     private static final int TWO_SECONDS = 2000;
 
-    private static final Log LOG = LogFactory.getLog(RemoteDebugger.class.getName());
-
     /**
      * Utility class. No constructor
      */
@@ -38,16 +36,14 @@ public final class RemoteDebugger {
     public static void main(String[] args) throws InterruptedException {
 
         if (args.length < 1 || args.length > 2) {
-            LOG.info("Command line to list caches to monitor: java -jar ehcache-remote-debugger.jar path_to_ehcache.xml\n" +
+            System.out.println("Command line to list caches to monitor: java -jar ehcache-remote-debugger.jar path_to_ehcache.xml\n" +
                     "Command line to monitor a specific cache: java -jar ehcache-remote-debugger.jar path_to_ehcache.xml" +
                     " cacheName");
             System.exit(2);
         }
 
-        if (!LOG.isTraceEnabled()) {
-            LOG.info("Increase the net.sf.ehcache.distribution logging level to debug or trace to see distributed" +
+        System.out.println("Increase the net.sf.ehcache.distribution logging level to debug or trace to see distributed" +
                     " cache operations as they occur.");
-        }
 
         CacheManager manager = new CacheManager(args[0]);
         String[] cacheNames = manager.getCacheNames();
@@ -57,19 +53,19 @@ public final class RemoteDebugger {
                 String name = cacheNames[i];
                 availableCaches.append(name).append(' ');
             }
-            LOG.info("Available caches are: " + availableCaches);
+            System.out.println("Available caches are: " + availableCaches + ". Choose one and add it to the command line.");
             System.exit(1);
         } else {
             String cacheName = args[1];
             Ehcache cache = manager.getCache(cacheName);
             if (cache == null) {
-                LOG.error("No cache named " + cacheName + " exists. Available caches are: " + availableCaches);
+                System.err.println("No cache named " + cacheName + " exists. Available caches are: " + availableCaches);
             } else {
-                LOG.info("Monitoring cache: " + cacheName);
+                System.out.println("Monitoring cache: " + cacheName);
 
                 while (true) {
                     Thread.sleep(TWO_SECONDS);
-                    LOG.info("Cache size: " + cache.getSize());
+                    System.out.println("Cache size: " + cache.getSize());
                 }
             }
         }
