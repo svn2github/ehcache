7108032504f2964c098d8d527dbfbb2ee7e0d5d0
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2008-02-21 09:01:15 +0000

    ehcache 1.4.1 branch created on release
    git-svn-id: http://svn.terracotta.org/svn/ehcache@584 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/ehcache-1.4.1/debugger/src/test/java/net/sf/ehcache/PlaceholderTest.java
@@ -0,0 +1,19 @@
+package net.sf.ehcache;
+
+import junit.framework.TestCase;
+
+/**
+ * A place holder to stop maven from falling over with an NPE
+ */
+public class PlaceholderTest extends TestCase {
+
+
+    /**
+     * Does nothing
+     */
+    public void testPlaceholder() {
+
+        //noop
+    }
+
+}
