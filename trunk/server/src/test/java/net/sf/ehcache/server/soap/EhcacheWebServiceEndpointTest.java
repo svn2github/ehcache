a7ffa500bbf233b5ef0be17cf8dca94cbb33865f
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2008-06-15 09:02:01 +0000

    Another web service test
    git-svn-id: http://svn.terracotta.org/svn/ehcache@660 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/server/src/test/java/net/sf/ehcache/server/soap/EhcacheWebServiceEndpointTest.java
+++ trunk/server/src/test/java/net/sf/ehcache/server/soap/EhcacheWebServiceEndpointTest.java
@@ -9,18 +9,34 @@ import net.sf.ehcache.server.soap.jaxws.*;
 import net.sf.ehcache.server.soap.jaxws.EhcacheWebServiceEndpoint;
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 public class EhcacheWebServiceEndpointTest {
+    private static EhcacheWebServiceEndpoint endpoint;
+
+    @BeforeClass
+  public static void setup() {
+        endpoint = new EhcacheWebServiceEndpointService().getEhcacheWebServiceEndpointPort();
+  }
 
   @Test
   public void testPing() {
-
-      EhcacheWebServiceEndpoint endpoint = new EhcacheWebServiceEndpointService().getEhcacheWebServiceEndpointPort();
-
       //invoke business method
       String result = endpoint.ping();
-      Assert.assertEquals("pong", result);
-
-
+      assertEquals("pong", result);
   }
+
+  @Test
+  public void testCacheNames() throws IllegalStateException_Exception {
+      //invoke business method
+      List cacheNames = endpoint.cacheNames();
+      //Other tests add caches to the CacheManager
+      assertTrue(cacheNames.size() >= 6);
+  }
+
+    
+
 }
