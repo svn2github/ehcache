de1f32ed6f4970ec1ba80ce49f0052828058f3de
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2008-06-20 12:54:29 +0000

    add checkstyle
    git-svn-id: http://svn.terracotta.org/svn/ehcache@676 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/standalone-server/src/test/java/net/sf/ehcache/server/standalone/ServerTest.java
+++ trunk/standalone-server/src/test/java/net/sf/ehcache/server/standalone/ServerTest.java
@@ -14,11 +14,15 @@ public class ServerTest {
     /**
      * Use for manual testing
      * GF embedded is broken
+     * @throws java.io.IOException
+     * @throws InterruptedException
+     * todo make work with war dep
      */
     @Test
-    public void manualTest() throws IOException {
+    public void testManual() throws IOException, InterruptedException {
         Server.main(new String[]{"8080", "/Users/gluck/work/ehcache/server/target/ehcache-server-1.6.0-beta1.war"});
-        System.in.read();
+        //System.in.read();
+        Thread.sleep(5000);
     }
 
 }
