19429f6b24e520e84dd054d3ec342a1451cfe242
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2008-07-23 08:32:03 +0000

    RESTful getCaches test done
    git-svn-id: http://svn.terracotta.org/svn/ehcache@711 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/server/src/test/java/net/sf/ehcache/server/rest/resources/CachesResourceTest.java
+++ trunk/server/src/test/java/net/sf/ehcache/server/rest/resources/CachesResourceTest.java
@@ -17,8 +17,20 @@
 package net.sf.ehcache.server.rest.resources;
 
 import net.sf.ehcache.server.HttpUtil;
+import net.sf.ehcache.server.soap.jaxws.CacheException_Exception;
+import net.sf.ehcache.server.soap.jaxws.NoSuchCacheException_Exception;
+import net.sf.ehcache.server.soap.jaxws.Cache;
+import net.sf.ehcache.server.soap.jaxws.IllegalStateException_Exception;
+import net.sf.ehcache.server.soap.jaxws.ObjectExistsException_Exception;
+import net.sf.ehcache.server.soap.jaxws.EhcacheWebServiceEndpointService;
+import net.sf.ehcache.server.soap.jaxws.EhcacheWebServiceEndpoint;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.Before;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
@@ -30,10 +42,12 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+import javax.xml.ws.soap.SOAPFaultException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
+import java.util.List;
 import java.net.HttpURLConnection;
 
 
@@ -47,10 +61,95 @@ public class CachesResourceTest {
 
     public static final Logger LOG = Logger.getLogger(CachesResourceTest.class.getName());
 
+    private static EhcacheWebServiceEndpoint cacheService;
+    private String cacheName = "sampleCache1";
+
+    @BeforeClass
+    public static void setup() {
+        cacheService = new EhcacheWebServiceEndpointService().getEhcacheWebServiceEndpointPort();
+
+        //add security credentials
+        // Uncomment and Enable XWSS config files to run tests with security
+//        ((BindingProvider)cacheService).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "Ron");
+//        ((BindingProvider)cacheService).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "noR");
+
+    }
+
+    @Before
+    public void zeroOutCache() throws CacheException_Exception, IllegalStateException_Exception {
+        cacheService.removeAll(cacheName);
+        cacheService.removeAll("sampleCache3");
+    }
+
+
+
+// @Test
+//    public void testGetCache() throws CacheException_Exception, NoSuchCacheException_Exception {
+//        Cache cache = cacheService.getCache("doesnotexist");
+//        assertNull(cache);
+//
+//        cache = cacheService.getCache("sampleCache1");
+//        assertEquals("sampleCache1", cache.getName());
+//        assertEquals("rest/sampleCache1", cache.getUri());
+//        assertTrue(cache.getDescription().indexOf("sampleCache1") != -1);
+//    }
+//
+//    @Test
+//    public void testAddCache() throws CacheException_Exception, NoSuchCacheException_Exception, IllegalStateException_Exception, ObjectExistsException_Exception {
+//
+//        cacheService.addCache("newcache1");
+//        Cache cache = cacheService.getCache("newcache1");
+//        assertNotNull(cache);
+//
+//        try {
+//            cacheService.addCache("newcache1");
+//        } catch (SOAPFaultException e) {
+//            //expected
+//            assertTrue(e.getCause().getMessage().indexOf("Cache newcache1 already exists") != -1);
+//        }
+//    }
+//
+//    @Test
+//    public void testRemoveCache() throws CacheException_Exception, NoSuchCacheException_Exception, IllegalStateException_Exception, ObjectExistsException_Exception {
+//
+//        cacheService.addCache("newcache2");
+//        Cache cache = cacheService.getCache("newcache2");
+//        assertNotNull(cache);
+//
+//        cacheService.removeCache("newcache2");
+//        cache = cacheService.getCache("newcache2");
+//        assertNull(cache);
+//
+//        //should not throw an exception
+//        cacheService.removeCache("newcache2");
+//        cache = cacheService.getCache("newcache2");
+//        assertNull(cache);
+//    }
+//
+//    /**
+//     * Gets the cache names
+//     */
+//    @Test
+//    public void testCacheNames() throws IllegalStateException_Exception {
+//        List cacheNames = cacheService.cacheNames();
+//        //Other tests add caches to the CacheManager
+//        assertTrue(cacheNames.size() >= 6);
+//    }
+
+
+
     @Test
-    public void testGetCaches() throws IOException, ParserConfigurationException, SAXException {
+    public void testGetCaches() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
         HttpURLConnection result = HttpUtil.get("http://localhost:8080/ehcache/rest/");
         assertEquals(200, result.getResponseCode());
+
+        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = documentBuilder.parse(result.getInputStream());
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        String cacheCount = xpath.evaluate("count(//cache)", document);
+        //some other tests create more
+        assertTrue(Integer.parseInt(cacheCount) >= 6);
     }
 
 
