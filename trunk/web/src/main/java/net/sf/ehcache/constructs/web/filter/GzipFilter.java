26ac0b8593f769e4100353b6f3046c137b3dc0f9
gregluck <gregluck@b9324663-ca0f-0410-8574-be9b3887307d>
2009-02-28 20:32:48 +0000

    Create new module for Ehcache web filters
    git-svn-id: http://svn.terracotta.org/svn/ehcache@894 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ trunk/web/src/main/java/net/sf/ehcache/constructs/web/filter/GzipFilter.java
@@ -0,0 +1,156 @@
+/**
+ *  Copyright 2003-2008 Luck Consulting Pty Ltd
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.constructs.web.filter;
+
+import net.sf.ehcache.constructs.web.GenericResponseWrapper;
+import net.sf.ehcache.constructs.web.ResponseUtil;
+
+
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.util.zip.GZIPOutputStream;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+/**
+ * Provides GZIP compression of responses.
+ * <p/>
+ * See the filter-mappings.xml entry for the gzip filter for the URL patterns
+ * which will be gzipped. At present this includes .jsp, .js and .css.
+ * <p/>
+ * @author <a href="mailto:gluck@thoughtworks.com">Greg Luck</a>
+ * @author <a href="mailto:amurdoch@thoughtworks.com">Adam Murdoch</a>
+ * @version $Id: GzipFilter.java 744 2008-08-16 20:10:49Z gregluck $
+ */
+public class GzipFilter extends Filter {
+
+    private static final Logger LOG = Logger.getLogger(GzipFilter.class.getName());
+
+    /**
+     * Performs initialisation.
+     * @param filterConfig
+     */
+    protected void doInit(FilterConfig filterConfig) throws Exception {
+        //nothing required.
+    }
+
+
+    /**
+     * A template method that performs any Filter specific destruction tasks.
+     * Called from {@link #destroy()}
+     */
+    protected void doDestroy() {
+        //noop
+    }
+
+    /**
+     * Performs the filtering for a request.
+     */
+    protected void doFilter(final HttpServletRequest request, final HttpServletResponse response,
+                            final FilterChain chain) throws Exception {
+        if (!isIncluded(request) && acceptsEncoding(request, "gzip")) {
+            // Client accepts zipped content
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine(request.getRequestURL() + ". Writing with gzip compression");
+            }
+
+            // Create a gzip stream
+            final ByteArrayOutputStream compressed = new ByteArrayOutputStream();
+            final GZIPOutputStream gzout = new GZIPOutputStream(compressed);
+
+            // Handle the request
+            final GenericResponseWrapper wrapper = new GenericResponseWrapper(response, gzout);
+            chain.doFilter(request, wrapper);
+            wrapper.flush();
+
+            gzout.close();
+
+            //return on error or redirect code, because response is already committed
+            int statusCode = wrapper.getStatus();
+            if (statusCode != HttpServletResponse.SC_OK) {
+                return;
+            }
+
+            //Saneness checks
+            byte[] compressedBytes = compressed.toByteArray();
+            boolean shouldGzippedBodyBeZero = ResponseUtil.shouldGzippedBodyBeZero(compressedBytes, request);
+            boolean shouldBodyBeZero = ResponseUtil.shouldBodyBeZero(request, wrapper.getStatus());
+            if (shouldGzippedBodyBeZero || shouldBodyBeZero) {
+                compressedBytes = new byte[0];
+            }
+
+            // Write the zipped body
+            ResponseUtil.addGzipHeader(response);
+            response.setContentLength(compressedBytes.length);
+
+
+            response.getOutputStream().write(compressedBytes);
+        } else {
+            // Client does not accept zipped content - don't bother zipping
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine(request.getRequestURL()
+                        + ". Writing without gzip compression because the request does not accept gzip.");
+            }
+            chain.doFilter(request, response);
+        }
+    }
+
+
+    /**
+     * Checks if the request uri is an include.
+     * These cannot be gzipped.
+     */
+    private boolean isIncluded(final HttpServletRequest request) {
+        final String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+        final boolean includeRequest = !(uri == null);
+
+        if (includeRequest && LOG.isLoggable(Level.FINE)) {
+            LOG.fine(request.getRequestURL() + " resulted in an include request. This is unusable, because" +
+                    "the response will be assembled into the overrall response. Not gzipping.");
+        }
+        return includeRequest;
+    }
+
+    /**
+     * Determine whether the user agent accepts GZIP encoding. This feature is part of HTTP1.1.
+     * If a browser accepts GZIP encoding it will advertise this by including in its HTTP header:
+     * <p/>
+     * <code>
+     * Accept-Encoding: gzip
+     * </code>
+     * <p/>
+     * Requests which do not accept GZIP encoding fall into the following categories:
+     * <ul>
+     * <li>Old browsers, notably IE 5 on Macintosh.
+     * <li>Internet Explorer through a proxy. By default HTTP1.1 is enabled but disabled when going
+     * through a proxy. 90% of non gzip requests seen on the Internet are caused by this.
+     * </ul>
+     * As of September 2004, about 34% of Internet requests do not accept GZIP encoding.
+     *
+     * @param request
+     * @return true, if the User Agent request accepts GZIP encoding
+     */
+    protected boolean acceptsGzipEncoding(HttpServletRequest request) {
+        return acceptsEncoding(request, "gzip");
+    }
+
+
+}
---
+++ trunk/web/src/test/java/net/sf/ehcache/constructs/web/filter/SpeedTest.java
@@ -0,0 +1,123 @@
+/**
+ *  Copyright 2003-2008 Luck Consulting Pty Ltd
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.constructs.web.filter;
+
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebResponse;
+import net.sf.ehcache.StopWatch;
+import net.sf.ehcache.constructs.web.AbstractWebTest;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * @author Greg Luck
+ * @version $Id: SpeedTest.java 796 2008-10-09 02:39:03Z gregluck $
+ */
+public class SpeedTest extends AbstractWebTest {
+
+    private static final Logger LOG = Logger.getLogger(SpeedTest.class.getName());
+
+    /**
+     * Time to get 200 Cached Pages
+     * StopWatch time: 947ms
+     */
+    @Test
+    public void testSpeedHttpClientNotCached() throws IOException {
+        StopWatch stopWatch = new StopWatch();
+        String url = "http://localhost:8080/Login.jsp";
+        HttpClient httpClient = new HttpClient();
+        HttpMethod httpMethod = new GetMethod(url);
+        stopWatch.getElapsedTime();
+        for (int i = 0; i < 200; i++) {
+            httpClient.executeMethod(httpMethod);
+            httpMethod.getResponseBodyAsStream();
+        }
+        long time = stopWatch.getElapsedTime();
+        LOG.info("Time for 200 uncached page requests: " + time);
+    }
+
+    /**
+     * Time to get 200 Cached Pages
+     * StopWatch time: 1021ms
+     */
+    @Test
+    public void testSpeedHttpClientCached() throws IOException {
+        StopWatch stopWatch = new StopWatch();
+        String url = "http://localhost:8080/CachedPage.jsp";
+        HttpClient httpClient = new HttpClient();
+        HttpMethod httpMethod = new GetMethod(url);
+        stopWatch.getElapsedTime();
+        for (int i = 0; i < 200; i++) {
+            httpClient.executeMethod(httpMethod);
+            httpMethod.getResponseBodyAsStream();
+        }
+        long time = stopWatch.getElapsedTime();
+        LOG.info("Time for 200 cached page requests: " + time);
+    }
+
+    /**
+     * StopWatch time: 2251ms
+     */
+    @Test
+    public void testSpeedNoDom() throws Exception {
+        StopWatch stopWatch = new StopWatch();
+        final WebConversation conversation = createWebConversation(true);
+
+        String requestUrl = "http://localhost:8080/CachedPage.jsp";
+        stopWatch.getElapsedTime();
+        for (int i = 0; i < 200; i++) {
+            WebResponse response = conversation.getResponse(requestUrl);
+            response.getText().indexOf("timestamp");
+        }
+        long time = stopWatch.getElapsedTime();
+        LOG.info("Time for 200 cached page requests: " + time);
+
+    }
+
+    /**
+     * Test time: 10.2s, 11.5    4.2 without JavaScript library
+     * StopWatch time: 2686ms without JavaScript library
+     * This test gets the CacheLogin twice so we need half as many
+     */
+    @Test
+    public void testSpeedDom() throws Exception {
+        StopWatch stopWatch = new StopWatch();
+        CachingFilterTest cachingFilterTest = new CachingFilterTest();
+        cachingFilterTest.testCachedPageIsCached();
+        stopWatch.getElapsedTime();
+        for (int i = 0; i < 100; i++) {
+            cachingFilterTest.testCachedPageIsCached();
+        }
+        long time = stopWatch.getElapsedTime();
+        LOG.info("Time for 200 cached page requests: " + time);
+
+    }
+
+
+    /**
+     * This is the last test run in the suite. It tells clover to flush.
+     */
+    @Test
+    public void testFlushClover() {
+        ///CLOVER:FLUSH
+    }
+}
