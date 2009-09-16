38f7b791e788b81e4c43c36dc5cf3fd1668aed76
teck <teck@b9324663-ca0f-0410-8574-be9b3887307d>
2009-09-16 00:39:41 +0000

    - fix checkstyle errors - store factory takes an Ehcache (not Cache)
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1044 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-1.7-tc/core/src/main/java/net/sf/ehcache/store/StoreFactory.java
+++ branches/ehcache-1.7-tc/core/src/main/java/net/sf/ehcache/store/StoreFactory.java
@@ -1,6 +1,22 @@
+/**
+ *  Copyright 2003-2009 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package net.sf.ehcache.store;
 
-import net.sf.ehcache.Cache;
+import net.sf.ehcache.Ehcache;
 
 /**
  * Factory for creating Store implementations
@@ -10,6 +26,12 @@ import net.sf.ehcache.Cache;
  */
 public interface StoreFactory {
 
-    Store create(Cache cache);
+    /**
+     * Create a Store for the given cache
+     *
+     * @param cache the cache will backed by the returned store
+     * @return store instance
+     */
+    Store create(Ehcache cache);
 
 }
--- branches/ehcache-1.7-tc/terracotta/src/main/java/net/sf/ehcache/terracotta/StandaloneTerracottaStoreFactory.java
+++ branches/ehcache-1.7-tc/terracotta/src/main/java/net/sf/ehcache/terracotta/StandaloneTerracottaStoreFactory.java
@@ -1,12 +1,12 @@
 package net.sf.ehcache.terracotta;
 
-import net.sf.ehcache.Cache;
+import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.store.Store;
 import net.sf.ehcache.store.StoreFactory;
 
 public class StandaloneTerracottaStoreFactory implements StoreFactory {
 
-	public Store create(Cache cache) {
+	public Store create(Ehcache cache) {
 		throw new UnsupportedOperationException();
 	}
 
