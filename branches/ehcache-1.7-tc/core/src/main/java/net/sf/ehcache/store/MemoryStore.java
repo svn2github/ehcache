4e0755e1ffd58ac0de20ea4bb1f07d7bea42136e
gbevin <gbevin@b9324663-ca0f-0410-8574-be9b3887307d>
2009-09-17 17:44:51 +0000

    Removed AbstractMemoryStore
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1065 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-1.7-tc/core/src/main/java/net/sf/ehcache/store/MemoryStore.java
+++ branches/ehcache-1.7-tc/core/src/main/java/net/sf/ehcache/store/MemoryStore.java
@@ -38,7 +38,7 @@ import net.sf.ehcache.Status;
  * @author <a href="mailto:gbevin@terracottatech.com">Geert Bevin</a>
  * @version $Id$
  */
-public class MemoryStore extends AbstractMemoryStore {
+public class MemoryStore implements Store {
 
     /**
      * This number is magic. It was established using empirical testing of the two approaches
@@ -61,6 +61,11 @@ public class MemoryStore extends AbstractMemoryStore {
     private static final int JUMP_AHEAD = 5;
 
     private static final Logger LOG = Logger.getLogger(MemoryStore.class.getName());
+
+    /**
+     * The cache this store is associated with.
+     */
+    protected Ehcache cache;
 
     /**
      * when sampling elements, whether to iterate or to use the keySample array for faster random access
@@ -106,7 +111,7 @@ public class MemoryStore extends AbstractMemoryStore {
         this.cache = cache;
         this.maximumSize = cache.getCacheConfiguration().getMaxElementsInMemory();
         this.diskStore = diskStore;
-        this.policy = determineEvictionPolicy(cache);
+        this.policy = determineEvictionPolicy();
 
         map = new ConcurrentHashMap(maximumSize, DEFAULT_LOAD_FACTOR, CONCURRENCY_LEVEL);
         if (maximumSize > TOO_LARGE_TO_EFFICIENTLY_ITERATE) {
@@ -136,29 +141,136 @@ public class MemoryStore extends AbstractMemoryStore {
     }
 
     /**
-     * {@inheritDoc}
+     * Puts an item in the store. Note that this automatically results in an eviction if the store is full.
+     *
+     * @param element the element to add
      */
-    @Override
+    public synchronized final void put(final Element element) throws CacheException {
+        if (element != null) {
+            putIntoBackend(element);
+        }
+    }
+
     protected void putIntoBackend(final Element element) throws CacheException {
         map.put(element.getObjectKey(), element);
         doPut(element);
     }
 
     /**
-     * {@inheritDoc}
+     * Gets an item from the cache.
+     * <p/>
+     * The last access time in {@link net.sf.ehcache.Element} is updated.
+     *
+     * @param key the key of the Element
+     * @return the element, or null if there was no match for the key
      */
-    @Override
+    public final Element get(final Object key) {
+
+        if (key == null) {
+            return null;
+        }
+
+        Element element = getFromBackend(key);
+
+        if (element != null) {
+            element.updateAccessStatistics();
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine(cache.getName() + "Cache: " + cache.getName() + "MemoryStore hit for " + key);
+            }
+        } else if (LOG.isLoggable(Level.FINE)) {
+            LOG.fine(cache.getName() + "Cache: " + cache.getName() + "MemoryStore miss for " + key);
+        }
+
+        return element;
+    }
+
     protected Element getFromBackend(final Object key) {
         return (Element) map.get(key);
     }
 
+    /**
+     * Gets an item from the cache, without updating statistics.
+     *
+     * @param key the key of the Element
+     * @return the element, or null if there was no match for the key
+     */
+    public final Element getQuiet(final Object key) {
+        Element cacheElement = getFromBackend(key);
+
+        if (cacheElement != null) {
+            //cacheElement.updateAccessStatistics(); Don't update statistics
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine(cache.getName() + "Cache: " + cache.getName() + "MemoryStore hit for " + key);
+            }
+        } else if (LOG.isLoggable(Level.FINE)) {
+            LOG.fine(cache.getName() + "Cache: " + cache.getName() + "MemoryStore miss for " + key);
+        }
+        return cacheElement;
+    }
+
 
     /**
-     * {@inheritDoc}
+     * Removes an Element from the store.
+     *
+     * @param key the key of the Element, usually a String
+     * @return the Element if one was found, else null
      */
-    @Override
+    public final Element remove(final Object key) {
+
+        if (key == null) {
+            return null;
+        }
+
+        // remove single item.
+        Element element = removeFromBackend(key);
+        if (element != null) {
+            return element;
+        } else {
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.log(Level.FINE, cache.getName() + "Cache: Cannot remove entry as key " + key + " was not found");
+            }
+            return null;
+        }
+    }
+
     protected Element removeFromBackend(final Object key) {
         return (Element)map.remove(key);
+    }
+
+    /**
+     * Memory stores are never backed up and always return false
+     */
+    public final boolean bufferFull() {
+        return false;
+    }
+
+    /**
+     * Expire all elements.
+     * <p/>
+     * This is a default implementation which does nothing. Expiration on demand is only
+     * implemented for disk stores.
+     */
+    public final void expireElements() {
+        //empty implementation
+    }
+
+    /**
+     * Chooses the Policy from the cache configuration
+     *
+     * @param cache
+     */
+    protected final Policy determineEvictionPolicy() {
+        MemoryStoreEvictionPolicy policySelection = cache.getCacheConfiguration().getMemoryStoreEvictionPolicy();
+
+        if (policySelection.equals(MemoryStoreEvictionPolicy.LRU)) {
+            return new LruPolicy();
+        } else if (policySelection.equals(MemoryStoreEvictionPolicy.FIFO)) {
+            return new FifoPolicy();
+        } else if (policySelection.equals(MemoryStoreEvictionPolicy.LFU)) {
+            return new LfuPolicy();
+        }
+        
+        throw new IllegalArgumentException(policySelection + " isn't a valid eviction policy");
     }
 
     /**
