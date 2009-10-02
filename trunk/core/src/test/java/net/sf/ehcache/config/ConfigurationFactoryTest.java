33b8d4f4f2baca21a9e8e5cc0731d00322b0f675
amiller <amiller@b9324663-ca0f-0410-8574-be9b3887307d>
2009-10-02 21:39:23 +0000

    Added a couple more example of terracotta config to the reference config.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1282 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/test/java/net/sf/ehcache/config/ConfigurationFactoryTest.java
+++ trunk/core/src/test/java/net/sf/ehcache/config/ConfigurationFactoryTest.java
@@ -248,7 +248,7 @@ public class ConfigurationFactoryTest extends AbstractCacheTest {
         assertEquals(10000000, defaultCache.getCacheConfiguration().getMaxElementsOnDisk());
 
         //Check caches
-        assertEquals(6, configurationHelper.createCaches().size());
+        assertEquals(7, configurationHelper.createCaches().size());
 
         //check config
         CacheConfiguration sampleCache1Config = (CacheConfiguration) configuration.getCacheConfigurations().get("sampleCache1");
@@ -861,7 +861,7 @@ public class ConfigurationFactoryTest extends AbstractCacheTest {
         assertEquals(true, defaultCache.getCacheConfiguration().isOverflowToDisk());
 
         //Check caches
-        assertEquals(6, configurationHelper.createCaches().size());
+        assertEquals(7, configurationHelper.createCaches().size());
 
         //  <cache name="sampleCache1"
         //  maxElementsInMemory="10000"
