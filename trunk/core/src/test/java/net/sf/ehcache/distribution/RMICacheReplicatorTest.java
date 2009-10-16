a7eed7bf1ca1a1820588f65ba3eb2d3ae637863e
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2009-10-16 20:16:36 +0000

    ignored failed test cases
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1325 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/test/java/net/sf/ehcache/distribution/RMICacheReplicatorTest.java
+++ trunk/core/src/test/java/net/sf/ehcache/distribution/RMICacheReplicatorTest.java
@@ -60,7 +60,11 @@ import java.util.logging.Logger;
  * @version $Id$
  */
 
-@Ignore("MNK-1377")
+
+//
+// Please close jira MNK-1377 after fixing ignored tests below
+//
+
 public class RMICacheReplicatorTest extends AbstractCacheTest {
 
 
@@ -203,6 +207,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
     /**
      * 5 cache managers should means that each cache has four remote peers
      */
+    @Ignore
     @Test
     public void testRemoteCachePeersEqualsNumberOfCacheManagersInCluster() {
 
@@ -214,6 +219,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
     /**
      * Does a new cache manager in the cluster get detected?
      */
+    @Ignore
     @Test
     public void testRemoteCachePeersDetectsNewCacheManager() throws InterruptedException {
 
@@ -234,6 +240,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
     /**
      * Does a down cache manager in the cluster get removed?
      */
+    @Ignore
     @Test
     public void testRemoteCachePeersDetectsDownCacheManager() throws InterruptedException {
 
@@ -255,6 +262,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
     /**
      * Does a down cache manager in the cluster get removed?
      */
+    @Ignore
     @Test
     public void testRemoteCachePeersDetectsDownCacheManagerSlow() throws InterruptedException {
 
@@ -341,6 +349,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
      * Tests what happens when a CacheManager in the cluster comes and goes. In ehcache-1.2.4 this would cause the new RMI CachePeers in the CacheManager to
      * be permanently corrupt.
      */
+    @testPutProgagatesFromAndToEveryCacheManagerAndCacheDirty
     @Test
     public void testPutProgagatesFromAndToEveryCacheManagerAndCacheDirty() throws CacheException, InterruptedException {
 
@@ -458,6 +467,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
      * r39 - Batching asyn replicator. Send all queued messages in one RMI call once per second.
      * 2 seconds to get 2000 notifications with 6 peers, Elements with 400 byte payload (5 second heartbeat)
      */
+    @Ignore
     @Test
     public void testBigPutsProgagatesAsynchronous() throws CacheException, InterruptedException {
 
@@ -499,6 +509,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
      * Performance and capacity tests.
      * <p/>
      */
+    @Ignore
     @Test
     public void testBootstrap() throws CacheException, InterruptedException, RemoteException {
 
@@ -782,6 +793,7 @@ public class RMICacheReplicatorTest extends AbstractCacheTest {
      * CacheEventListeners that are not CacheReplicators should receive cache events originated from receipt
      * of a remote event by a CachePeer.
      */
+    @Ignore
     @Test
     public void testRemotelyReceivedPutNotifiesCountingListener() throws InterruptedException {
 
