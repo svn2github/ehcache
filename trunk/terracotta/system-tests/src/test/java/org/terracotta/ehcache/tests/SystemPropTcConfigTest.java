08747c2d4f920332f6a6d1acbadcdc0318eddd88
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2009-11-11 20:55:22 +0000

    add assertions
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1425 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/terracotta/system-tests/src/test/java/org/terracotta/ehcache/tests/SystemPropTcConfigTest.java
+++ trunk/terracotta/system-tests/src/test/java/org/terracotta/ehcache/tests/SystemPropTcConfigTest.java
@@ -1,6 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
- * notice. All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice. All rights
+ * reserved.
  */
 package org.terracotta.ehcache.tests;
 
@@ -12,12 +12,12 @@ public class SystemPropTcConfigTest extends AbstractStandaloneCacheTest {
   public SystemPropTcConfigTest() {
     super("basic-cache-test.xml");
   }
-  
+
   @Override
   protected Class getApplicationClass() {
     return SystemPropTcConfigTest.App.class;
   }
-  
+
   public static class App extends AbstractStandaloneCacheTest.App {
     public App(String appId, ApplicationConfig cfg, ListenerProvider listenerProvider) {
       super(appId, cfg, listenerProvider);
@@ -28,6 +28,20 @@ public class SystemPropTcConfigTest extends AbstractStandaloneCacheTest {
       System.out.println("adding extra -Dtc.config");
       addClientJvmarg("-Dtc.config=tc-config.xml");
       runClient(Client1.class);
+    }
+
+    @Override
+    protected void evaluateClientOutput(String clientName, int exitCode, String clientOutput) throws AssertionError {
+      if (exitCode == 0) throw new AssertionError("Client should fail but didn't");
+      if (!clientOutput.contains("net.sf.ehcache.CacheException")) {
+        //
+        throw new AssertionError("Expecting client to fail with exception: net.sf.ehcache.CacheException");
+      }
+      String errorMessage = "The Terracotta config file should not be set through -Dtc.config in this usage. It must be embedded in ehcache configuration file.";
+      if (!clientOutput.contains(errorMessage)) {
+        //
+        throw new AssertionError("Expecting client to fail with message: " + errorMessage);
+      }
     }
   }
 }
