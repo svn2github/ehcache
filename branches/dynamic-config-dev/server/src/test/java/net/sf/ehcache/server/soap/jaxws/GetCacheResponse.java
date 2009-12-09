7acc07e9fa5f4e0075e021287b1f20e557812cba
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2009-12-09 16:54:54 +0000

    Create private branch for dynamic configuration development work
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1506 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/dynamic-config-dev/server/src/test/java/net/sf/ehcache/server/soap/jaxws/GetCacheResponse.java
@@ -0,0 +1,55 @@
+package net.sf.ehcache.server.soap.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getCacheResponse complex type.
+ * <p/>
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p/>
+ * <pre>
+ * &lt;complexType name="getCacheResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://soap.server.ehcache.sf.net/}cache" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getCacheResponse", propOrder = {
+        "_return"
+})
+public class GetCacheResponse {
+
+    @XmlElement(name = "return")
+    protected Cache _return;
+
+    /**
+     * Gets the value of the return property.
+     *
+     * @return possible object is
+     *         {@link Cache }
+     */
+    public Cache getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     *
+     * @param value allowed object is
+     *              {@link Cache }
+     */
+    public void setReturn(Cache value) {
+        this._return = value;
+    }
+
+}
