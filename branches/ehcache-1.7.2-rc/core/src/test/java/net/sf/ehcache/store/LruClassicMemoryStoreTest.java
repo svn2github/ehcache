c93e2f08111e65a409a811e877c7266df2916c57
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2009-12-12 00:22:23 +0000

    RC branch for 1.7.2
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1515 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/ehcache-1.7.2-rc/core/src/test/java/net/sf/ehcache/store/LruClassicMemoryStoreTest.java
@@ -0,0 +1,41 @@
+package net.sf.ehcache.store;
+
+import net.sf.ehcache.Cache;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests the pre ehcache-1.6 LruMemoryStore, which can be switched in
+ * @author Greg Luck
+ */
+public class LruClassicMemoryStoreTest extends LruMemoryStoreTest {
+
+    /**
+     * setup test
+     */
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty(Cache.NET_SF_EHCACHE_USE_CLASSIC_LRU, "true");
+        super.setUp();
+    }
+
+
+    /**
+     * teardown
+     */
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+        System.setProperty(Cache.NET_SF_EHCACHE_USE_CLASSIC_LRU, "false");
+    }
+
+    /**
+     * Test the LRU policy
+     */
+    @Test
+    public void testProbabilisticEvictionPolicy() throws Exception {
+        super.testProbabilisticEvictionPolicy();
+    }
+
+}
---
+++ branches/ehcache-1.7.2-rc/web/src/main/webapp/non_ok/PageNotFoundGzip.jsp
@@ -0,0 +1,3 @@
+<%
+    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+%>
