7951beaf6119e03e961fe39254d14a0788bf02dd
gluck <gluck@b9324663-ca0f-0410-8574-be9b3887307d>
2009-12-15 22:24:58 +0000

    Add ehcache-explicitlocking template
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1533 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/explicitlocking/src/test/java/net/sf/ehcache/constructs/locking/AppTest.java
+++ trunk/explicitlocking/src/test/java/net/sf/ehcache/constructs/locking/AppTest.java
@@ -1,38 +1,19 @@
 package net.sf.ehcache.constructs.locking;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest {
+    
     /**
      * Create the test case
      *
-     * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void testNothing() {
+        //
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
