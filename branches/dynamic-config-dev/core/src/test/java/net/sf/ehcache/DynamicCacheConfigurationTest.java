7dc3489e1016f36dc99b96c8816002b544a26910
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2009-12-17 21:39:57 +0000

    remove unused import
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1546 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/dynamic-config-dev/core/src/test/java/net/sf/ehcache/DynamicCacheConfigurationTest.java
+++ branches/dynamic-config-dev/core/src/test/java/net/sf/ehcache/DynamicCacheConfigurationTest.java
@@ -7,7 +7,6 @@ package net.sf.ehcache;
 import net.sf.ehcache.config.CacheConfiguration;
 import net.sf.ehcache.config.Configuration;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.slf4j.Logger;
