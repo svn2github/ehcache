d4878a0c2f67a55b21cec0129e62508c625be223
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2009-12-22 23:33:04 +0000

    bad tag
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1571 b9324663-ca0f-0410-8574-be9b3887307d

--- tags/openjpa-0.2.0/standalone-server/src/assemble/daemon_stop.sh
+++
@@ -1,1 +0,0 @@
-jsvc -stop -pidfile ../pid/ehcache_server.pid -cp ../lib/commons-daemon-1.0.1.jar:../lib/${project.build.finalName}.jar:../lib/glassfish-embedded-all-3.0-Prelude-Embedded-b14.jar  net.sf.ehcache.server.standalone.Server 8080 ../war
