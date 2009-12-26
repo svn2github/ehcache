9498430fedbf4020d3a9e3e715e4caaeec5aaa6d
steve <steve@b9324663-ca0f-0410-8574-be9b3887307d>
2009-12-26 17:30:38 +0000

    Added a test for putIfAbsent
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1585 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/explicitlocking/src/test/java/net/sf/ehcache/constructs/locking/ExplicitLockingCacheTest.java
+++ trunk/explicitlocking/src/test/java/net/sf/ehcache/constructs/locking/ExplicitLockingCacheTest.java
@@ -1,58 +1,49 @@
 package net.sf.ehcache.constructs.locking;
 
+import junit.framework.TestCase;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class ExplicitLockingCacheTest {
+public class ExplicitLockingCacheTest extends TestCase {
 
-	/**
-	 * Verify that multiple readers can read the field at the same time
-	 */
-	@Test
-	public void testMultipleReaders() {
+    private static final String TEST_CACHE = "ExplicitLockCacheTest";
+    private ExplicitLockingCache testCache;
 
-	}
+    protected void setUp() throws Exception {
+        super.setUp();
+        Configuration configuration = new Configuration();
+        CacheConfiguration defaultCacheConfiguration = new CacheConfiguration();
+        defaultCacheConfiguration.setEternal(false);
+        defaultCacheConfiguration.setName("default");
+        configuration.addDefaultCache(defaultCacheConfiguration);
 
-	/**
-	 * Show that a write blocks other writers and readers
-	 */
-	@Test
-	public void testWriterBlocksWritersAndReaders() {
+        CacheConfiguration theCache = new CacheConfiguration();
+        theCache.setName(TEST_CACHE);
+        theCache.setEternal(false);
+        theCache.setMaxElementsInMemory(10000);
+        // add more settings here!
+        configuration.addCache(theCache);
 
-	}
+        CacheManager mgr = new CacheManager(configuration);
+        testCache = new ExplicitLockingCache(mgr.getCache(TEST_CACHE));
+    }
 
-	/**
-	 * Show that a write blocks other writers and readers
-	 */
-	@Test
-	public void testTryLockUnheld() {
-
-	}
-
-	/**
-	 * Show that a write blocks other writers and readers
-	 */
-	@Test
-	public void testTryLockHeldWriter() {
-
-	}
-
-	/**
-	 * Show that a write blocks other writers and readers
-	 */
-	@Test
-	public void testTryLockHeldReader() {
-
-	}
-
-	/**
-	 * Show that a write blocks other writers and readers
-	 */
-	@Test
-	public void testPutIfAbsent() {
-
-	}
-
+    /**
+     * Show that if an entry already exists for the given key it is not re-added
+     */
+    @Test
+    public void testPutIfAbsent() {
+        assertEquals(testCache.get("putIfAbsent"), null);
+        testCache.putIfAbsent(new Element("putIfAbsent", "1"));
+        assertEquals(testCache.get("putIfAbsent").getValue(), "1");
+        testCache.putIfAbsent(new Element("putIfAbsent", "2"));
+        assertEquals(testCache.get("putIfAbsent").getValue(), "1");
+    }
 }
