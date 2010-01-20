ed2d3ff0db406f2672b07477aa5c80b7577ee9fb
nelrahma <nelrahma@b9324663-ca0f-0410-8574-be9b3887307d>
2010-01-20 05:39:19 +0000

    missed checking in test
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1678 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/jtabranch/core/src/test/java/net/sf/ehcache/store/Xa2PCTransactionalStoreTest.java
@@ -0,0 +1,15 @@
+package net.sf.ehcache.store;
+
+import org.junit.Test;
+
+/**
+ * @author Alex Snaps
+ */
+public class Xa2PCTransactionalStoreTest {
+
+    @Test
+    public void testIsBlockingDuring2PC() {
+
+    }
+
+}
