3c782dbf30a44cd804f8760618b31b9f5f00e9aa
jvoegele <jvoegele@b9324663-ca0f-0410-8574-be9b3887307d>
2010-01-29 16:30:58 +0000

    Created RC branch for ehcache-core-2.0.0-beta
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1793 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/ehcache-core-2.0.0-beta/src/main/java/net/sf/ehcache/distribution/MulticastKeepaliveHeartbeatSender.java
@@ -0,0 +1,281 @@
+/**
+ *  Copyright 2003-2009 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.distribution;
+
+import net.sf.ehcache.CacheManager;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Sends heartbeats to a multicast group containing a compressed list of URLs.
+ * <p/>
+ * You can control how far the multicast packets propagate by setting the badly misnamed "TTL".
+ * Using the multicast IP protocol, the TTL value indicates the scope or range in which a packet may be forwarded.
+ * By convention:
+ * <ul>
+ * <li>0 is restricted to the same host
+ * <li>1 is restricted to the same subnet
+ * <li>32 is restricted to the same site
+ * <li>64 is restricted to the same region
+ * <li>128 is restricted to the same continent
+ * <li>255 is unrestricted
+ * </ul>
+ * You can also control how often the heartbeat sends by setting the interval.
+ *
+ * @author Greg Luck
+ * @version $Id$
+ */
+public final class MulticastKeepaliveHeartbeatSender {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(MulticastKeepaliveHeartbeatSender.class.getName());
+
+    private static final int DEFAULT_HEARTBEAT_INTERVAL = 5000;
+    private static final int MINIMUM_HEARTBEAT_INTERVAL = 1000;
+    private static final int MAXIMUM_PEERS_PER_SEND = 150;
+
+    private static long heartBeatInterval = DEFAULT_HEARTBEAT_INTERVAL;
+
+    private final InetAddress groupMulticastAddress;
+    private final Integer groupMulticastPort;
+    private final Integer timeToLive;
+    private MulticastServerThread serverThread;
+    private volatile boolean stopped;
+    private final CacheManager cacheManager;
+    private InetAddress hostAddress;
+
+    /**
+     * Constructor.
+     *
+     * @param cacheManager     the bound CacheManager. Each CacheManager has a maximum of one sender
+     * @param multicastAddress
+     * @param multicastPort
+     * @param timeToLive       See class description for the meaning of this parameter.
+     */
+    public MulticastKeepaliveHeartbeatSender(CacheManager cacheManager,
+                                             InetAddress multicastAddress, Integer multicastPort,
+                                             Integer timeToLive,
+                                             InetAddress hostAddress) {
+        this.cacheManager = cacheManager;
+        this.groupMulticastAddress = multicastAddress;
+        this.groupMulticastPort = multicastPort;
+        this.timeToLive = timeToLive;
+        this.hostAddress = hostAddress;
+
+    }
+
+    /**
+     * Start the heartbeat thread
+     */
+    public final void init() {
+        serverThread = new MulticastServerThread();
+        serverThread.start();
+    }
+
+    /**
+     * Shutdown this heartbeat sender
+     */
+    public final synchronized void dispose() {
+        stopped = true;
+        notifyAll();
+        serverThread.interrupt();
+    }
+
+    /**
+     * A thread which sends a multicast heartbeat every second
+     */
+    private final class MulticastServerThread extends Thread {
+
+        private MulticastSocket socket;
+        private List compressedUrlListList = new ArrayList();
+        private int cachePeersHash;
+
+
+        /**
+         * Constructor
+         */
+        public MulticastServerThread() {
+            super("Multicast Heartbeat Sender Thread");
+            setDaemon(true);
+        }
+
+        @Override
+        public final void run() {
+            while (!stopped) {
+                try {
+                    socket = new MulticastSocket(groupMulticastPort.intValue());
+                    if (hostAddress != null) {
+                        socket.setInterface(hostAddress);
+                    }
+                    socket.setTimeToLive(timeToLive.intValue());
+                    socket.joinGroup(groupMulticastAddress);
+
+                    while (!stopped) {
+                        List buffers = createCachePeersPayload();
+                        for (Iterator iter = buffers.iterator(); iter.hasNext();) {
+                            byte[] buffer = (byte[]) iter.next();
+                            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, groupMulticastAddress,
+                                    groupMulticastPort.intValue());
+                            socket.send(packet);
+                        }
+                        try {
+                            synchronized (this) {
+                                wait(heartBeatInterval);
+                            }
+                        } catch (InterruptedException e) {
+                            if (!stopped) {
+                                LOG.error("Error receiving heartbeat. Initial cause was " + e.getMessage(), e);
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    LOG.debug("Error on multicast socket", e);
+                } catch (Throwable e) {
+                    LOG.info("Unexpected throwable in run thread. Continuing..." + e.getMessage(), e);
+                } finally {
+                    closeSocket();
+                }
+                if (!stopped) {
+                    try {
+                        sleep(heartBeatInterval);
+                    } catch (InterruptedException e) {
+                        LOG.error("Sleep after error interrupted. Initial cause was " + e.getMessage(), e);
+                    }
+                }
+            }
+        }
+
+        /**
+         * Creates a gzipped payload.
+         * <p/>
+         * The last gzipped payload is retained and only recalculated if the list of cache peers
+         * has changed.
+         *
+         * @return a gzipped byte[]
+         */
+        private List createCachePeersPayload() {
+            List localCachePeers = cacheManager.getCachePeerListener("RMI").getBoundCachePeers();
+            int newCachePeersHash = localCachePeers.hashCode();
+            if (cachePeersHash != newCachePeersHash) {
+                cachePeersHash = newCachePeersHash;
+                compressedUrlListList = PayloadUtil.createCompressedPayloadList(localCachePeers, MAXIMUM_PEERS_PER_SEND);
+            }
+            return compressedUrlListList;
+        }
+
+
+        /**
+         * Interrupts this thread.
+         * <p/>
+         * <p> Unless the current thread is interrupting itself, which is
+         * always permitted, the {@link #checkAccess() checkAccess} method
+         * of this thread is invoked, which may cause a {@link
+         * SecurityException} to be thrown.
+         * <p/>
+         * <p> If this thread is blocked in an invocation of the {@link
+         * Object#wait() wait()}, {@link Object#wait(long) wait(long)}, or {@link
+         * Object#wait(long,int) wait(long, int)} methods of the {@link Object}
+         * class, or of the {@link #join()}, {@link #join(long)}, {@link
+         * #join(long,int)}, {@link #sleep(long)}, or {@link #sleep(long,int)},
+         * methods of this class, then its interrupt status will be cleared and it
+         * will receive an {@link InterruptedException}.
+         * <p/>
+         * <p> If this thread is blocked in an I/O operation upon an {@link
+         * java.nio.channels.InterruptibleChannel </code>interruptible
+         * channel<code>} then the channel will be closed, the thread's interrupt
+         * status will be set, and the thread will receive a {@link
+         * java.nio.channels.ClosedByInterruptException}.
+         * <p/>
+         * <p> If this thread is blocked in a {@link java.nio.channels.Selector}
+         * then the thread's interrupt status will be set and it will return
+         * immediately from the selection operation, possibly with a non-zero
+         * value, just as if the selector's {@link
+         * java.nio.channels.Selector#wakeup wakeup} method were invoked.
+         * <p/>
+         * <p> If none of the previous conditions hold then this thread's interrupt
+         * status will be set. </p>
+         *
+         * @throws SecurityException if the current thread cannot modify this thread
+         */
+        @Override
+        public final void interrupt() {
+            closeSocket();
+            super.interrupt();
+        }
+
+        private void closeSocket() {
+            try {
+                if (socket != null && !socket.isClosed()) {
+                    try {
+                        socket.leaveGroup(groupMulticastAddress);
+                    } catch (IOException e) {
+                        LOG.error("Error leaving multicast group. Message was " + e.getMessage());
+                    }
+                    socket.close();
+                }
+            } catch (NoSuchMethodError e) {
+                LOG.debug("socket.isClosed is not supported by JDK1.3");
+                try {
+                    socket.leaveGroup(groupMulticastAddress);
+                } catch (IOException ex) {
+                    LOG.error("Error leaving multicast group. Message was " + ex.getMessage());
+                }
+                socket.close();
+            }
+        }
+
+    }
+
+    /**
+     * Sets the heartbeat interval to something other than the default of 5000ms. This is useful for testing,
+     * but not recommended for production. This method is static and so affects the heartbeat interval of all
+     * senders. The change takes effect after the next scheduled heartbeat.
+     *
+     * @param heartBeatInterval a time in ms, greater than 1000
+     */
+    public static void setHeartBeatInterval(long heartBeatInterval) {
+        if (heartBeatInterval < MINIMUM_HEARTBEAT_INTERVAL) {
+            LOG.warn("Trying to set heartbeat interval too low. Using MINIMUM_HEARTBEAT_INTERVAL instead.");
+            MulticastKeepaliveHeartbeatSender.heartBeatInterval = MINIMUM_HEARTBEAT_INTERVAL;
+        } else {
+            MulticastKeepaliveHeartbeatSender.heartBeatInterval = heartBeatInterval;
+        }
+    }
+
+    /**
+     * Returns the heartbeat interval.
+     */
+    public static long getHeartBeatInterval() {
+        return heartBeatInterval;
+    }
+
+    /**
+     * @return the TTL
+     */
+    public Integer getTimeToLive() {
+        return timeToLive;
+    }
+}
---
+++ branches/ehcache-core-2.0.0-beta/src/main/java/net/sf/ehcache/exceptionhandler/ExceptionHandlingDynamicCacheProxy.java
@@ -0,0 +1,186 @@
+/**
+ *  Copyright 2003-2009 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.exceptionhandler;
+
+import net.sf.ehcache.Ehcache;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A dynamic proxy which provides CacheException handling.
+ * <p/>
+ * The ehcache configuration will create and register in the <code>CacheManager</code> {@link Ehcache}s decorated
+ * with this dynamic proxy. See following for programmatic use.
+ * <p/>
+ * The createProxy factory method may be used to simply create a proxy. Otherwise the calling client
+ * will need code similar to:
+ * <pre>
+ * (Ehcache) Proxy.newProxyInstance(ehcache.getClass().getClassLoader(), new Class[]{ Ehcache.class },
+ * new ExceptionHandlingDynamicCacheProxy(ehcache));</pre>
+ * <p/>
+ * A common usage is to create a proxy and then register the proxy in <code>CacheManager</code> in place of the
+ * underlying cache. To do that create a proxy and then call
+ * <pre>
+ * cacheManager.replaceCacheWithDecoratedCache(Ehcache cache, Ehcache decoratedCache);
+ * </pre>
+ * All clients accessing the cache through<code>cacheManager.getEhcache()</code> will then receive proxy references.
+ * <p/>
+ * See CacheTest for a perf test.
+ *
+ * @author <a href="mailto:gluck@gregluck.com">Greg Luck</a>
+ * @version $Id$
+ */
+public final class ExceptionHandlingDynamicCacheProxy implements InvocationHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionHandlingDynamicCacheProxy.class.getName());
+
+    private Ehcache ehcache;
+
+    /**
+     * Constructor: Use with something like:
+     * <pre>
+     * (Ehcache) Proxy.newProxyInstance(ehcache.getClass().getClassLoader(), new Class[]{ Ehcache.class },
+     * new ExceptionHandlingDynamicCacheProxy(ehcache));</pre>
+     * @param ehcache the backing ehcache
+     */
+    public ExceptionHandlingDynamicCacheProxy(Ehcache ehcache) {
+        this.ehcache = ehcache;
+    }
+
+    /**
+     * A simple factory method to hide the messiness of creating the proxy from clients.
+     *
+     * @param ehcache the target cache
+     * @return a proxied Ehcache
+     */
+    public static Ehcache createProxy(Ehcache ehcache) {
+        return (Ehcache) Proxy.newProxyInstance(ehcache.getClass().getClassLoader(), new Class[]{Ehcache.class},
+                new ExceptionHandlingDynamicCacheProxy(ehcache));
+    }
+
+
+
+    /**
+     * Processes a method invocation on a proxy instance and returns
+     * the result.  This method will be invoked on an invocation handler
+     * when a method is invoked on a proxy instance that it is
+     * associated with.
+     *
+     * @param proxy  the proxy instance that the method was invoked on
+     * @param method the <code>Method</code> instance corresponding to
+     *               the interface method invoked on the proxy instance.  The declaring
+     *               class of the <code>Method</code> object will be the interface that
+     *               the method was declared in, which may be a superinterface of the
+     *               proxy interface that the proxy class inherits the method through.
+     * @param args   an array of objects containing the values of the
+     *               arguments passed in the method invocation on the proxy instance,
+     *               or <code>null</code> if interface method takes no arguments.
+     *               Arguments of primitive types are wrapped in instances of the
+     *               appropriate primitive wrapper class, such as
+     *               <code>java.lang.Integer</code> or <code>java.lang.Boolean</code>.
+     * @return the value to return from the method invocation on the
+     *         proxy instance.  If the declared return type of the interface
+     *         method is a primitive type, then the value returned by
+     *         this method must be an instance of the corresponding primitive
+     *         wrapper class; otherwise, it must be a type assignable to the
+     *         declared return type.  If the value returned by this method is
+     *         <code>null</code> and the interface method's return type is
+     *         primitive, then a <code>NullPointerException</code> will be
+     *         thrown by the method invocation on the proxy instance.  If the
+     *         value returned by this method is otherwise not compatible with
+     *         the interface method's declared return type as described above,
+     *         a <code>ClassCastException</code> will be thrown by the method
+     *         invocation on the proxy instance.
+     * @throws Throwable the exception to throw from the method
+     *                   invocation on the proxy instance.  The exception's type must be
+     *                   assignable either to any of the exception types declared in the
+     *                   <code>throws</code> clause of the interface method or to the
+     *                   unchecked exception types <code>java.lang.RuntimeException</code>
+     *                   or <code>java.lang.Error</code>.  If a checked exception is
+     *                   thrown by this method that is not assignable to any of the
+     *                   exception types declared in the <code>throws</code> clause of
+     *                   the interface method, then an
+     *                   {@link java.lang.reflect.UndeclaredThrowableException} containing the
+     *                   exception that was thrown by this method will be thrown by the
+     *                   method invocation on the proxy instance.
+     * @see java.lang.reflect.UndeclaredThrowableException
+     *
+     */
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object invocationResult = null;
+        try {
+            invocationResult = method.invoke(ehcache, args);
+        } catch (Exception e) {
+            CacheExceptionHandler cacheExceptionHandler = ehcache.getCacheExceptionHandler();
+            if (cacheExceptionHandler != null) {
+                String keyAsString = null;
+                //should be a CacheException
+                Throwable cause = e.getCause();
+                if (cause != null) {
+                    keyAsString = extractKey(cause.getMessage());
+                }
+                Exception causeAsException = null;
+                try {
+                    causeAsException = (Exception) cause;
+                } catch (ClassCastException cce) {
+                    //we only handle exceptions, not errors.
+                        LOG.debug("Underlying cause was not an Exception: {}", cce);
+                }
+
+                cacheExceptionHandler.onException(ehcache, keyAsString, causeAsException);
+            } else {
+                throw e.getCause();
+            }
+
+        }
+        return invocationResult;
+    }
+
+    /**
+     * Extracts the key from the message, if any
+     */
+    static String extractKey(String message) {
+        if (message == null) {
+            return null;
+        }
+        int beginIndex = message.lastIndexOf("key ");
+        if (beginIndex < 0) {
+            return null;
+        }
+        beginIndex += "key ".length();
+        int endIndex = beginIndex;
+        for (int i = beginIndex; i < message.length(); i++) {
+            char character = message.charAt(i);
+            if (character == ' ') {
+                break;
+            }
+            endIndex = i;
+        }
+        endIndex++;
+        if (endIndex > message.length()) {
+            endIndex = message.length();
+        }
+        String key = message.substring(beginIndex, endIndex);
+        return key;
+    }
+}
