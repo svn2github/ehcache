f5180f45f351976ef65384c5402817b62b9afef2
jvoegele <jvoegele@b9324663-ca0f-0410-8574-be9b3887307d>
2010-03-04 20:55:40 +0000

    Tag ehcache-server-0.9
    git-svn-id: http://svn.terracotta.org/svn/ehcache@1981 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ tags/ehcache-server-0.9/src/test/java/net/sf/ehcache/server/soap/jaxws/Cache.java
@@ -0,0 +1,99 @@
+package net.sf.ehcache.server.soap.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for cache complex type.
+ * <p/>
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p/>
+ * <pre>
+ * &lt;complexType name="cache">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="uri" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "cache", propOrder = {
+        "description",
+        "name",
+        "uri"
+})
+public class Cache {
+
+    protected String description;
+    protected String name;
+    protected String uri;
+
+    /**
+     * Gets the value of the description property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    /**
+     * Gets the value of the name property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the uri property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * Sets the value of the uri property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setUri(String value) {
+        this.uri = value;
+    }
+
+}
