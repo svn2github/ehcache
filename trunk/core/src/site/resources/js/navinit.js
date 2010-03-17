f8015782dc3c81ab004981376b3690a5168c80a3
gluck <gluck@b9324663-ca0f-0410-8574-be9b3887307d>
2010-03-17 14:40:28 +0000

    Never Commit
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2017 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/site/resources/js/navinit.js
+++ trunk/core/src/site/resources/js/navinit.js
@@ -6,7 +6,7 @@ function initMenu() {
   $('#navList ul').filter(function(index) {return $('strong', this).length == 1;}).parent().parent().find('img.icon').attr('src', 'http://ehcache.org/images/arrow_open.png') ;
   //$("img").css('color',"red");
 
-
+ /*
   $('#navList li a.parent').mouseover(
     function() {
 	$('#navList ul').filter(function(index) {return $("strong", this).length == 0}).parent().find('img.icon').attr('src', 'http://ehcache.org/images/arrow_closed.png');
@@ -33,7 +33,29 @@ function initMenu() {
 		$('#navList ul').filter(function(index) {return $("strong", this).length == 0}).parent().find('img.icon').attr('src', 'http://ehcache.org/images/arrow_closed.png');
 
       }
+    );        */
+
+    $('#navList li a.parent').click(
+    function() {
+	
+      $(this).parent().find('img.icon').attr('src', 'http://ehcache.org/images/arrow_open.png') ;
+
+      var checkElement = $(this).next();
+      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+         $(this).parent().find('img.icon').attr('src', 'http://ehcache.org/images/arrow_closed.png') ;
+         checkElement.slideUp('normal');
+          return false;
+        }
+      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+        //$('#navList ul').filter(function(index) {return $("strong", this).length == 0}).slideUp('normal');
+
+        checkElement.slideDown('normal');
+        return false;
+        }
+      }
     );
+
+  
    
 
     $('li:parent').css('margin', '0px');
