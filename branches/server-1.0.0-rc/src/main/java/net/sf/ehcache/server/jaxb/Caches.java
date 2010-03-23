a3c170cf18d27c03de372e64180d01d2a723f1fa
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2010-03-23 00:28:27 +0000

    RC branch for server-1.0.0
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2058 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/server-1.0.0-rc/src/main/java/net/sf/ehcache/server/jaxb/Caches.java
@@ -0,0 +1,66 @@
+/**
+ *  Copyright 2003-2009 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.server.jaxb;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * A collection of Cache representations available from a CacheManager
+ *
+ * @author Greg Luck
+ */
+@XmlRootElement
+public class Caches {
+
+    private List<Cache> caches;
+
+    /**
+     * Empty Constructor
+     */
+    public Caches() {
+    }
+
+    /**
+     * Constructor using a list of cache representations
+     *
+     * @param caches a list of caches that belong in this CacheManager
+     */
+    public Caches(List<Cache> caches) {
+        setCaches(caches);
+    }
+
+    /**
+     * Gets the list of caches
+     *
+     * @return a list of Cache representations
+     */
+    public List<Cache> getCaches() {
+        return caches;
+    }
+
+    /**
+     * Sets the list of caches
+     *
+     * @param caches a list of cache representations
+     */
+    public void setCaches(List<Cache> caches) {
+        this.caches = caches;
+    }
+
+
+}
