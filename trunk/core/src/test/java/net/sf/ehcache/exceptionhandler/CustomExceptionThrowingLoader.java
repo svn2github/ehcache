ee8e4d2d724e51b508d4e6c3d15581a3f6879a18
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2010-04-06 02:45:52 +0000

    update copyright to 2010
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2154 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/test/java/net/sf/ehcache/exceptionhandler/CustomExceptionThrowingLoader.java
+++ trunk/core/src/test/java/net/sf/ehcache/exceptionhandler/CustomExceptionThrowingLoader.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2003-2009 Terracotta, Inc.
+ *  Copyright 2003-2010 Terracotta, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package net.sf.ehcache.exceptionhandler;
 
 /**
- *  Copyright 2003-2009 Terracotta, Inc.
+ *  Copyright 2003-2010 Terracotta, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
