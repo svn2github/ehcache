18ca382e9ebd9b70fce92e2e55c5a4e3f3c88c16
gluck <gluck@b9324663-ca0f-0410-8574-be9b3887307d>
2010-04-17 17:16:48 +0000

    popup for API now utilises HTML stored in invisible divs on the index page (right down the bottom in the code)
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2275 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/site/resources/js/tooltip.js
+++ trunk/core/src/site/resources/js/tooltip.js
@@ -14,19 +14,44 @@ function mouseUpdate(x,y, title){
 	    $("#popup").fadeIn(100);
    }
     var caption = "";
-    switch(title){
+    /*switch(title){
         case "hibernate":
                 caption = "Hibernate Cache Provider";
         break;
+        case "core":
+                caption = "EhCache Core";
+        break;
+        case "rest":
+                caption = "RAST/SOAP";
+        break;
+        case "jms":
+                caption = "JMS Replication";
+        break;
+        case "rmi":
+                caption = "RMI Replication";
+        break;
+        case "jgroup":
+                caption = "JGroups Replication";
+        break;
+        case "cluster":
+                caption = "Terracotta Distributed Caching";
+        break;
+        case "cluster":
+                caption = "Terracotta Distributed Caching";
+        break;
+
+        default:
+              caption = title;
+        break;
 
 
+    }  */
 
-    }
 
-
-    g("popupTitle").innerHTML = "<strong>"+ caption+"</strong>";
-    g("popupTitle").style.fontSize = "15";
-
+   // g("popupTitle").innerHTML = "<strong>"+ title+"</strong>";
+  //  g("popupTitle").style.fontSize = "13";
+      g("popupContent").innerHTML = "";
+      g("popupContent").innerHTML = g(title).innerHTML;
     
 
 
@@ -41,26 +66,42 @@ function popupHide(){
 	
 }
 
-$('#popup').mouseOver(function() {
+$('#popupTitle').mouseover(function() {
 
-  if(g("popup").style.display != "none"){
-        $("#popup").fadeOut(100);
+       alert();
 
-   }
+
 });
 
-$('#popupTitle').mouseOver(function() {
 
-  if(g("popup").style.display != "none"){
-        $("#popup").fadeOut(100);
 
-   }
-});
 
-$('#popupContent').mouseOver(function() {
 
-  if(g("popup").style.display != "none"){
-        $("#popup").fadeOut(100);
+function mouseX(evt) {if (!evt) evt = window.event; if (evt.pageX) return evt.pageX;
 
-   }
-});
+else if (evt.clientX)return evt.clientX + (document.documentElement.scrollLeft ?  document.documentElement.scrollLeft : document.body.scrollLeft); else return 0;
+
+}
+
+
+function mouseY(evt) {if (!evt) evt = window.event; if (evt.pageY) return evt.pageY; else if (evt.clientY)return evt.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop); else return 0;}
+
+function follow(evt) {
+	if (document.getElementById) {
+        var obj = document.getElementById('popup').style;
+        //obj.visibility = 'visible';
+
+		obj.left = (parseInt(mouseX(evt))+20) + 'px';
+		obj.top = (parseInt(mouseY(evt))-25) + 'px';
+	}
+
+}
+
+
+document.onmousemove = follow;
+
+$('#Wrapper').mouseover(
+    function() {
+      popupHide();
+        
+}); 
