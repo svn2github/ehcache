91f56894d8899b06451929d9f936dbb69a781085
alexsnaps <alexsnaps@b9324663-ca0f-0410-8574-be9b3887307d>
2010-05-02 08:26:27 +0000

    Added copy strategy that will only copy the Element, but reuse its value: which is fine for immutable values
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2370 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ trunk/core/src/main/java/net/sf/ehcache/store/compound/ImmutableValueElementCopyStrategy.java
@@ -0,0 +1,33 @@
+package net.sf.ehcache.store.compound;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Element;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author Alex Snaps
+ */
+public class ImmutableValueElementCopyStrategy implements CopyStrategy {
+
+    CopyStrategy defaultCopyStrategy = new SerializationCopyStrategy();
+
+    public <T> T copy(final T value) {
+
+        final T newValue;
+        if(value instanceof Element) {
+            Element element = (Element) value;
+            Element newElement = new Element(element.getObjectKey(), element.getObjectValue(), element.getVersion(),
+                element.getCreationTime(), element.getLastAccessTime(), element.getHitCount(), element.usesCacheDefaultLifespan(),
+                element.getTimeToLive(), element.getTimeToIdle(), element.getLastUpdateTime());
+            newValue = (T) newElement;
+        } else {
+            newValue = defaultCopyStrategy.copy(value);
+        }
+
+        return newValue;
+    }
+}
