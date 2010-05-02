6f8b8c2c5a36a2b4fae085fdcc192c600521f937
alexsnaps <alexsnaps@b9324663-ca0f-0410-8574-be9b3887307d>
2010-05-02 08:28:50 +0000

    Fixed checkstyle... for real :P Argghhh
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2372 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/main/java/net/sf/ehcache/store/compound/ImmutableValueElementCopyStrategy.java
+++ trunk/core/src/main/java/net/sf/ehcache/store/compound/ImmutableValueElementCopyStrategy.java
@@ -1,20 +1,32 @@
+/**
+ *  Copyright 2003-2010 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package net.sf.ehcache.store.compound;
 
-import net.sf.ehcache.CacheException;
 import net.sf.ehcache.Element;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 /**
  * @author Alex Snaps
  */
 public class ImmutableValueElementCopyStrategy implements CopyStrategy {
 
-    CopyStrategy defaultCopyStrategy = new SerializationCopyStrategy();
+    private CopyStrategy defaultCopyStrategy = new SerializationCopyStrategy();
 
+    /**
+     * @inheritDoc
+     */
     public <T> T copy(final T value) {
 
         final T newValue;
