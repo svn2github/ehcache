4fc4b78182605a8c6f201483e7af391703e753c4
gluck <gluck@b9324663-ca0f-0410-8574-be9b3887307d>
2010-05-12 09:44:56 +0000

    Add decorator test
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2432 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/explicitlocking/src/test/java/net/sf/ehcache/constructs/locking/ExplicitLockingCacheTest.java
+++ trunk/explicitlocking/src/test/java/net/sf/ehcache/constructs/locking/ExplicitLockingCacheTest.java
@@ -2,10 +2,14 @@ package net.sf.ehcache.constructs.locking;
 
 import junit.framework.TestCase;
 import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Element;
 import net.sf.ehcache.config.CacheConfiguration;
 import net.sf.ehcache.config.Configuration;
 
+import net.sf.ehcache.exceptionhandler.ExceptionHandlingDynamicCacheProxy;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -15,9 +19,10 @@ public class ExplicitLockingCacheTest extends TestCase {
 
     private static final String TEST_CACHE = "ExplicitLockCacheTest";
     private ExplicitLockingCache testCache;
+    private CacheManager manager;
 
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void setUp() throws Exception {
         Configuration configuration = new Configuration();
         CacheConfiguration defaultCacheConfiguration = new CacheConfiguration("default", 2000);
         configuration.addDefaultCache(defaultCacheConfiguration);
@@ -26,8 +31,24 @@ public class ExplicitLockingCacheTest extends TestCase {
         // add more settings here!
         configuration.addCache(theCache);
 
-        CacheManager mgr = new CacheManager(configuration);
-        testCache = new ExplicitLockingCache(mgr.getCache(TEST_CACHE));
+        manager = new CacheManager(configuration);
+        testCache = new ExplicitLockingCache(manager.getCache(TEST_CACHE));
+    }
+
+    @After
+    public void teardown() {
+        manager.shutdown();
+    }
+
+
+    /**
+     * Can we add one as a decorated cache?
+     * https://jira.terracotta.org/jira/browse/EHC-709 reports that this does not work
+     */
+    @Test
+    public void testCanAddAsDecoratedCache() {
+        manager.replaceCacheWithDecoratedCache(manager.getCache(TEST_CACHE), testCache);
+        assertNotNull(manager.getEhcache(TEST_CACHE));
     }
 
     /**
