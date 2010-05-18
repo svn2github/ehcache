d1e9e1ab40a541ae401469b4d1d2e77f982467ed
gluck <gluck@b9324663-ca0f-0410-8574-be9b3887307d>
2010-05-18 05:59:55 +0000

    Updated monitor doco. Added video demo on Monitor 1.0
    git-svn-id: http://svn.terracotta.org/svn/ehcache@2454 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/upload.sh
+++ trunk/core/upload.sh
@@ -1,7 +1,7 @@
 # This script maintains a list of paths that are owned by Marketing and should not be generated with Maven. 
 # It also uploads the site to ehcache-stage using a non-destructive rsync command which will not delete target paths.
 # You must be connected to the VPN to run this script, otherwise the upload will timeout
-* You will be prompted for a password for ehcache-stage. See IT for access.
+# You will be prompted for a password for ehcache-stage. See IT for access.
 
 # change directory to generated site root
 cd target/site
