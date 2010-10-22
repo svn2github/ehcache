1eccb8e7a69966485bf8d05cd6ae211e0592f135
teck <teck@b9324663-ca0f-0410-8574-be9b3887307d>
2010-10-22 23:16:52 +0000

    kill search branch
    git-svn-id: http://svn.terracotta.org/svn/ehcache@3025 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/search/standalone-server/src/assemble/daemon_start.sh
+++
@@ -1,2 +0,0 @@
-jsvc -pidfile ../pid/ehcache_server.pid -outfile ../logs/ehcache-server.log -errfile '&1' -cp ../lib/commons-daemon-1.0.1.jar:../lib/${project.build.finalName}.jar:../lib/glassfish-embedded-all-3.0-Prelude-Embedded-b14.jar  net.sf.ehcache.server.standalone.Server 8080 ../war
-
--- branches/search/web/web-system-tests/src/main/webapp/NoFiltersPage.jsp
+++
@@ -1,9 +0,0 @@
-<%
-    response.setDateHeader("Last-Modified", System.currentTimeMillis());
-%>
-
-<html>
-<body>
-This page is not included in any filters
-</body>
-</html>
