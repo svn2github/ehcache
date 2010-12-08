39e8659512eae3bee189812a6a89fec0384bac26
jvoegele <jvoegele@b9324663-ca0f-0410-8574-be9b3887307d>
2010-12-08 01:30:04 +0000

    Tag ehcache-core-2.3.1 so we have a reliable URL from which to pull the CHANGELOG.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@3296 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ tags/ehcache-core-2.3.1/src/test/java/net/sf/ehcache/writer/TestCacheWriterSlow.java
@@ -0,0 +1,69 @@
+package net.sf.ehcache.writer;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.ehcache.CacheEntry;
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Element;
+
+public class TestCacheWriterSlow extends AbstractTestCacheWriter {
+    private final Map<Object, Element> writtenElements = new HashMap<Object, Element>();
+    private final Map<Object, Element> deletedElements = new HashMap<Object, Element>();
+
+    public TestCacheWriterSlow() {
+    }
+
+    public Map<Object, Element> getWrittenElements() {
+        return writtenElements;
+    }
+
+    public Map<Object, Element> getDeletedElements() {
+        return deletedElements;
+    }
+
+    @Override
+    public synchronized void write(Element element) throws CacheException {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new CacheException(e);
+        }
+        writtenElements.put(element.getObjectKey(), element);
+    }
+
+    @Override
+    public synchronized void writeAll(Collection<Element> elements) throws CacheException {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new CacheException(e);
+        }
+        for (Element element : elements) {
+            writtenElements.put(element.getObjectKey() + "-batched", element);
+        }
+    }
+
+    @Override
+    public synchronized void delete(CacheEntry entry) throws CacheException {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new CacheException(e);
+        }
+        deletedElements.put(entry.getKey(), entry.getElement());
+    }
+
+    @Override
+    public synchronized void deleteAll(Collection<CacheEntry> entries) throws CacheException {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new CacheException(e);
+        }
+        for (CacheEntry entry : entries) {
+            deletedElements.put(entry.getKey() + "-batched", entry.getElement());
+        }
+    }
+}
