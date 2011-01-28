d3e61327f4036a08fd7b83bbb07c6c6c14cf21db
jvoegele <jvoegele@b9324663-ca0f-0410-8574-be9b3887307d>
2011-01-28 21:25:06 +0000

    Tag ehcache-core-2.4.0
    git-svn-id: http://svn.terracotta.org/svn/ehcache@3537 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ tags/ehcache-core-2.4.0/src/main/java/net/sf/ehcache/transaction/AbstractTransactionStore.java
@@ -0,0 +1,252 @@
+/**
+ *  Copyright 2003-2010 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package net.sf.ehcache.transaction;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Status;
+import net.sf.ehcache.config.InvalidConfigurationException;
+import net.sf.ehcache.search.Attribute;
+import net.sf.ehcache.search.Results;
+import net.sf.ehcache.search.attribute.AttributeExtractor;
+import net.sf.ehcache.store.AbstractStore;
+import net.sf.ehcache.store.Policy;
+import net.sf.ehcache.store.Store;
+import net.sf.ehcache.store.StoreQuery;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Abstract transactional store which provides implementation of all non-transactional methods
+ *
+ * @author Ludovic Orban
+ */
+public abstract class AbstractTransactionStore extends AbstractStore {
+
+    /**
+     * The underlying store wrapped by this store
+     */
+    protected final Store underlyingStore;
+
+    /**
+     * Constructor
+     * @param underlyingStore the underlying store
+     */
+    protected AbstractTransactionStore(Store underlyingStore) {
+        this.underlyingStore = underlyingStore;
+    }
+
+    /* non-transactional methods */
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getInMemorySize() {
+        return underlyingStore.getInMemorySize();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getOffHeapSize() {
+        return underlyingStore.getOffHeapSize();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getOnDiskSize() {
+        return underlyingStore.getOnDiskSize();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long getInMemorySizeInBytes() {
+        return underlyingStore.getInMemorySizeInBytes();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long getOffHeapSizeInBytes() {
+        return underlyingStore.getOffHeapSizeInBytes();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long getOnDiskSizeInBytes() {
+        return underlyingStore.getOnDiskSizeInBytes();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean containsKeyOnDisk(Object key) {
+        return underlyingStore.containsKeyOnDisk(key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean containsKeyOffHeap(Object key) {
+        return underlyingStore.containsKeyOffHeap(key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean containsKeyInMemory(Object key) {
+        return underlyingStore.containsKeyInMemory(key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void dispose() {
+        underlyingStore.dispose();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Status getStatus() {
+        return underlyingStore.getStatus();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void expireElements() {
+        underlyingStore.expireElements();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void flush() throws IOException {
+        underlyingStore.flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean bufferFull() {
+        return underlyingStore.bufferFull();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Policy getInMemoryEvictionPolicy() {
+        return underlyingStore.getInMemoryEvictionPolicy();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setInMemoryEvictionPolicy(Policy policy) {
+        underlyingStore.setInMemoryEvictionPolicy(policy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object getInternalContext() {
+        return underlyingStore.getInternalContext();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object getMBean() {
+        return underlyingStore.getMBean();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setNodeCoherent(boolean coherent) {
+        if (!coherent) {
+            throw new InvalidConfigurationException("a transactional cache cannot be incoherent");
+        }
+        underlyingStore.setNodeCoherent(coherent);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see net.sf.ehcache.store.Store#isNodeCoherent()
+     */
+    @Override
+    public boolean isNodeCoherent() {
+        return underlyingStore.isNodeCoherent();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see net.sf.ehcache.store.Store#isCacheCoherent()
+     */
+    @Override
+    public boolean isCacheCoherent() {
+        return underlyingStore.isCacheCoherent();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see net.sf.ehcache.store.Store#isClusterCoherent()
+     */
+    @Override
+    public boolean isClusterCoherent() {
+        return underlyingStore.isClusterCoherent();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waitUntilClusterCoherent() {
+        underlyingStore.waitUntilClusterCoherent();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setAttributeExtractors(Map<String, AttributeExtractor> extractors) {
+        underlyingStore.setAttributeExtractors(extractors);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Results executeQuery(StoreQuery query) {
+        return underlyingStore.executeQuery(query);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T> Attribute<T> getSearchAttribute(String attributeName) throws CacheException {
+        return underlyingStore.getSearchAttribute(attributeName);
+    }
+}
