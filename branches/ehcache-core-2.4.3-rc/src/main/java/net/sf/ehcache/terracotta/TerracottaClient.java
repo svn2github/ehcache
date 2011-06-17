4f0ee6e8414e3ec2da4fd2f7b462d8122615c53b
twu <twu@b9324663-ca0f-0410-8574-be9b3887307d>
2011-06-17 00:19:42 +0000

    MNK-2487: Merge r4083
    git-svn-id: http://svn.terracotta.org/svn/ehcache@4113 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-core-2.4.3-rc/src/main/java/net/sf/ehcache/terracotta/TerracottaClient.java
+++ branches/ehcache-core-2.4.3-rc/src/main/java/net/sf/ehcache/terracotta/TerracottaClient.java
@@ -47,7 +47,7 @@ public class TerracottaClient {
     private static final int REJOIN_SLEEP_MILLIS_ON_EXCEPTION = Integer.getInteger("net.sf.ehcache.rejoin.sleepMillisOnException", 5000);
 
     private final TerracottaClientConfiguration terracottaClientConfiguration;
-    private volatile ClusteredInstanceFactory clusteredInstanceFactory;
+    private volatile ClusteredInstanceFactoryWrapper clusteredInstanceFactory;
     private final TerracottaCacheCluster cacheCluster = new TerracottaCacheCluster();
     private final RejoinWorker rejoinWorker = new RejoinWorker();
     private final TerracottaClientRejoinListener rejoinListener;
@@ -167,7 +167,7 @@ public class TerracottaClient {
         rejoinWorker.shutdown();
     }
 
-    private synchronized ClusteredInstanceFactory createNewClusteredInstanceFactory(Map<String, CacheConfiguration> cacheConfigs) {
+    private synchronized ClusteredInstanceFactoryWrapper createNewClusteredInstanceFactory(Map<String, CacheConfiguration> cacheConfigs) {
         if (clusteredInstanceFactory != null) {
             LOGGER.info("Shutting down old ClusteredInstanceFactory...");
             // shut down the old factory
@@ -182,8 +182,11 @@ public class TerracottaClient {
             underlyingCacheCluster.addTopologyListener(new NodeLeftListener(this, underlyingCacheCluster.waitUntilNodeJoinsCluster()));
         }
 
-        // set up the cacheCluster with the new underlying cache cluster
-        cacheCluster.setUnderlyingCacheCluster(underlyingCacheCluster);
+        if (!rejoinWorker.isRejoinInProgress()) {
+            // set up the cacheCluster with the new underlying cache cluster if rejoin is not in progress
+            // else defer until rejoin is complete (to have node joined, online fired just before rejoin event)
+            cacheCluster.setUnderlyingCacheCluster(underlyingCacheCluster);
+        }
 
         return new ClusteredInstanceFactoryWrapper(this, factory);
     }
@@ -239,6 +242,10 @@ public class TerracottaClient {
             }
         }
 
+        public boolean isRejoinInProgress() {
+            return rejoinStatus.isRejoinInProgress();
+        }
+
         private void sleep(long sleepMillis) {
             try {
                 Thread.sleep(sleepMillis);
@@ -274,6 +281,8 @@ public class TerracottaClient {
         }
 
         private void fireClusterRejoinedEvent(final ClusterNode oldNodeReference) {
+            // set up the cacheCluster with the new underlying cache cluster
+            cacheCluster.setUnderlyingCacheCluster(clusteredInstanceFactory.getActualFactory().getTopology());
             try {
                 cacheCluster.fireNodeRejoinedEvent(oldNodeReference, cacheCluster.getCurrentNode());
             } catch (Throwable e) {
