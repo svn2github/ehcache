c64745b3ef4c14c2de25113b046376beaad8f881
lorban <lorban@b9324663-ca0f-0410-8574-be9b3887307d>
2011-06-28 14:56:51 +0000

    always remove default sizeof engine system property after the test ran
    git-svn-id: http://svn.terracotta.org/svn/ehcache@4208 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/cachemanager_pools/src/test/java/net/sf/ehcache/pool/PoolableStoresTest.java
+++ branches/cachemanager_pools/src/test/java/net/sf/ehcache/pool/PoolableStoresTest.java
@@ -3,6 +3,8 @@ package net.sf.ehcache.pool;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -16,11 +18,20 @@ public class PoolableStoresTest {
 
     public static final String DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP = "net.sf.ehcache.sizeofengine.default";
 
-    @Test
-    public void test() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         System.getProperties().setProperty(DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP,
                 "net.sf.ehcache.pool.impl.ConstantSizeOfEngine");
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        System.getProperties().remove(DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP);
+        assertThat(System.getProperties().getProperty(DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP), nullValue());
+    }
+
+    @Test
+    public void test() throws Exception {
         CacheManager cm = new CacheManager(PoolableStoresTest.class.getResourceAsStream("/pool/ehcache-heap-disk.xml"));
 
         Cache memoryOnlyCache = cm.getCache("memoryOnly");
@@ -40,9 +51,6 @@ public class PoolableStoresTest {
 
 
         cm.shutdown();
-
-        System.getProperties().remove(DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP);
-        assertThat(System.getProperties().getProperty(DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP), nullValue());
     }
 
 }
