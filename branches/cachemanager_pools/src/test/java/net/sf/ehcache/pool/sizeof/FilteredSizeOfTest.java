a13f79fd080fcb19817b200c26008f36da2bead4
lorban <lorban@b9324663-ca0f-0410-8574-be9b3887307d>
2011-06-30 14:29:56 +0000

    added hotspot CMS assertions
    git-svn-id: http://svn.terracotta.org/svn/ehcache@4239 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/cachemanager_pools/src/test/java/net/sf/ehcache/pool/sizeof/FilteredSizeOfTest.java
+++ branches/cachemanager_pools/src/test/java/net/sf/ehcache/pool/sizeof/FilteredSizeOfTest.java
@@ -3,14 +3,10 @@ package net.sf.ehcache.pool.sizeof;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import net.sf.ehcache.pool.sizeof.JvmInformation;
-import net.sf.ehcache.pool.sizeof.SizeOf;
-import net.sf.ehcache.pool.sizeof.SizeOfAgent;
 import net.sf.ehcache.pool.sizeof.filter.AnnotationSizeOfFilter;
 import net.sf.ehcache.pool.sizeof.filter.IgnoreSizeOf;
 import net.sf.ehcache.pool.sizeof.filter.ResourceSizeOfFilter;
 
-import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -18,11 +14,13 @@ public class FilteredSizeOfTest extends AbstractSizeOfTest {
 
   @BeforeClass
   public static void setup() {
-    Assume.assumeThat(JvmInformation.MINIMUM_OBJECT_SIZE, is(JvmInformation.OBJECT_ALIGNMENT));
-    
     new CrossCheckingSizeOf().deepSizeOf(new Object());
-    System.err.println("Testing for a " + System.getProperty("java.version") + " JDK (agent: " + SizeOfAgent.isAvailable() + ") on a "
-                       + System.getProperty("sun.arch.data.model") + "bit VM (compressed-oops: " + COMPRESSED_OOPS + ")");
+    System.out.println("Testing for a " + System.getProperty("java.version") +
+                       " JDK (agent: " + SizeOfAgent.isAvailable() +
+                       ") on a " + System.getProperty("sun.arch.data.model") + "-bit VM " +
+                       "(compressed-oops: " + COMPRESSED_OOPS +
+                       ", Hotspot CMS: " + HOTSPOT_CMS +
+                       ")");
   }
   
   @Test
@@ -30,65 +28,103 @@ public class FilteredSizeOfTest extends AbstractSizeOfTest {
     SizeOf sizeOf = new CrossCheckingSizeOf(new AnnotationSizeOfFilter());
     if (System.getProperty("java.version").startsWith("1.5")) {
       if (System.getProperty("sun.arch.data.model").equals("64")) {
+        System.out.println("asserting 1.5 / 64-bit values");
         assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(192L));
         assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(200L));
-        assertThat(sizeOf.deepSizeOf(new AnnotationFilteredRefferer()), is(224L));
+        assertThat(sizeOf.deepSizeOf(new AnnotationFilteredReferrer()), is(224L));
       } else {
+        System.out.println("asserting 1.5 / 32-bit values");
         assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(168L));
         assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(168L));
-        assertThat(sizeOf.deepSizeOf(new AnnotationFilteredRefferer()), is(184L));
+        assertThat(sizeOf.deepSizeOf(new AnnotationFilteredReferrer()), is(184L));
       }
     } else {
       if (System.getProperty("sun.arch.data.model").equals("64")) {
         if (COMPRESSED_OOPS) {
-          assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(168L));
-          assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(176L));
-          assertThat(sizeOf.deepSizeOf(new AnnotationFilteredRefferer()), is(192L));
+          if (HOTSPOT_CMS) {
+            System.out.println("asserting 1.6+ / 64-bit / compressed OOPs / Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(168L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(176L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredReferrer()), is(200L));
+          } else {
+            System.out.println("asserting 1.6+ / 64-bit / compressed OOPs / non-Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(168L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(176L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredReferrer()), is(192L));
+          }
         } else {
-          assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(192L));
-          assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(200L));
-          assertThat(sizeOf.deepSizeOf(new AnnotationFilteredRefferer()), is(224L));
+          if (HOTSPOT_CMS) {
+            System.out.println("asserting 1.6+ / 64-bit / plain OOPs / Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(192L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(200L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredReferrer()), is(224L));
+          } else {
+            System.out.println("asserting 1.6+ / 64-bit / plain OOPs / non-Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(192L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(200L));
+            assertThat(sizeOf.deepSizeOf(new AnnotationFilteredReferrer()), is(224L));
+          }
         }
       } else {
+        System.out.println("asserting 1.6+ / 32-bit values");
         assertThat(sizeOf.deepSizeOf(new AnnotationFilteredClass()), is(168L));
         assertThat(sizeOf.deepSizeOf(new AnnotationFilteredSubclass()), is(168L));
-        assertThat(sizeOf.deepSizeOf(new AnnotationFilteredRefferer()), is(184L));
+        assertThat(sizeOf.deepSizeOf(new AnnotationFilteredReferrer()), is(184L));
       }
     }
   }
-  
+
   @Test
   public void testResourceFiltering() throws Exception {
     SizeOf sizeOf = new CrossCheckingSizeOf(new ResourceSizeOfFilter(FilteredSizeOfTest.class.getClassLoader().getResource("sizeof.filter.fields")));
     if (System.getProperty("java.version").startsWith("1.5")) {
       if (System.getProperty("sun.arch.data.model").equals("64")) {
+        System.out.println("asserting 1.5 / 64-bit values");
         assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(192L));
         assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(200L));
-        assertThat(sizeOf.deepSizeOf(new ResourceFilteredRefferer()), is(224L));
+        assertThat(sizeOf.deepSizeOf(new ResourceFilteredReferrer()), is(224L));
       } else {
+        System.out.println("asserting 1.5 / 32-bit values");
         assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(168L));
         assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(168L));
-        assertThat(sizeOf.deepSizeOf(new ResourceFilteredRefferer()), is(184L));
+        assertThat(sizeOf.deepSizeOf(new ResourceFilteredReferrer()), is(184L));
       }
     } else {
       if (System.getProperty("sun.arch.data.model").equals("64")) {
         if (COMPRESSED_OOPS) {
-          assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(168L));
-          assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(176L));
-          assertThat(sizeOf.deepSizeOf(new ResourceFilteredRefferer()), is(192L));
+          if (HOTSPOT_CMS) {
+            System.out.println("asserting 1.6+ / 64-bit / compressed OOPs / Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(168L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(176L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredReferrer()), is(200L));
+          } else {
+            System.out.println("asserting 1.6+ / 64-bit / compressed OOPs / non-Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(168L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(176L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredReferrer()), is(200L));
+          }
         } else {
-          assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(192L));
-          assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(200L));
-          assertThat(sizeOf.deepSizeOf(new ResourceFilteredRefferer()), is(224L));
+          if (HOTSPOT_CMS) {
+            System.out.println("asserting 1.6+ / 64-bit / plain OOPs / Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(192L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(200L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredReferrer()), is(224L));
+          } else {
+            System.out.println("asserting 1.6+ / 64-bit / plain OOPs / non-Hotspot CMS values");
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(192L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(200L));
+            assertThat(sizeOf.deepSizeOf(new ResourceFilteredReferrer()), is(224L));
+          }
         }
       } else {
+        System.out.println("asserting 1.6+ / 32-bit values");
         assertThat(sizeOf.deepSizeOf(new ResourceFilteredClass()), is(168L));
         assertThat(sizeOf.deepSizeOf(new ResourceFilteredSubclass()), is(168L));
-        assertThat(sizeOf.deepSizeOf(new ResourceFilteredRefferer()), is(184L));
+        assertThat(sizeOf.deepSizeOf(new ResourceFilteredReferrer()), is(184L));
       }
     }
   }
-  
+
   @SuppressWarnings("unused")
   public static class AnnotationFilteredClass {
     @IgnoreSizeOf
@@ -96,27 +132,27 @@ public class FilteredSizeOfTest extends AbstractSizeOfTest {
 
     @IgnoreSizeOf
     private final byte[] mediumArray = new byte[1024];
-    
-    private final byte[] smallArray = new byte[128];    
+
+    private final byte[] smallArray = new byte[128];
   }
-  
+
   @SuppressWarnings("unused")
   public static class AnnotationFilteredSubclass extends AnnotationFilteredClass {
     private final int field = 0;
   }
 
   @SuppressWarnings("unused")
-  public static class AnnotationFilteredRefferer {
+  public static class AnnotationFilteredReferrer {
     private final AnnotationFilteredSubclass reference = new AnnotationFilteredSubclass();
   }
-  
+
   @SuppressWarnings("unused")
   public static class ResourceFilteredClass {
     private final byte[] bigArray = new byte[16 * 1024];
 
     private final byte[] mediumArray = new byte[1024];
-    
-    private final byte[] smallArray = new byte[128];    
+
+    private final byte[] smallArray = new byte[128];
   }
 
   @SuppressWarnings("unused")
@@ -125,7 +161,7 @@ public class FilteredSizeOfTest extends AbstractSizeOfTest {
   }
 
   @SuppressWarnings("unused")
-  public static class ResourceFilteredRefferer {
+  public static class ResourceFilteredReferrer {
     private final ResourceFilteredSubclass reference = new ResourceFilteredSubclass();
   }
 }
