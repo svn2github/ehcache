51cb458d75f61ae7bc43f2be8e81839e1a21dc6e
lorban <lorban@b9324663-ca0f-0410-8574-be9b3887307d>
2011-07-18 15:32:58 +0000

    DEV-5951: sizeof-agent.jar does not need to be on the system classpath anymore
    git-svn-id: http://svn.terracotta.org/svn/ehcache@4376 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/sizeof-agent/src/main/java/net/sf/ehcache/pool/sizeof/SizeOfAgent.java
+++ trunk/sizeof-agent/src/main/java/net/sf/ehcache/pool/sizeof/SizeOfAgent.java
@@ -8,23 +8,29 @@ import java.lang.instrument.Instrumentation;
 public class SizeOfAgent {
 
   private static volatile Instrumentation instrumentation;
+  private static final String INSTRUMENTATION_INSTANCE_SYSTEM_PROPERTY_NAME = "net.sf.ehcache.sizeof.agent.instrumentation";
+  private static final String NO_INSTRUMENTATION_SYSTEM_PROPERTY_NAME = "net.sf.ehcache.sizeof.agent.noInstrumentationSystemProperty";
 
   public static void premain(String options, Instrumentation inst) {
     SizeOfAgent.instrumentation = inst;
+    registerSystemProperty();
   }
 
   public static void agentmain(String options, Instrumentation inst) {
     SizeOfAgent.instrumentation = inst;
+    registerSystemProperty();
   }
 
-  public static long sizeOf(final Object obj) {
-    return instrumentation.getObjectSize(obj);
+  private static void registerSystemProperty() {
+    if (!Boolean.getBoolean(NO_INSTRUMENTATION_SYSTEM_PROPERTY_NAME)) {
+      System.getProperties().put(INSTRUMENTATION_INSTANCE_SYSTEM_PROPERTY_NAME, instrumentation);
+    }
   }
 
-  public static boolean isAvailable() {
-    return instrumentation != null;
+  public static Instrumentation getInstrumentation() {
+    return instrumentation;
   }
-  
+
   private SizeOfAgent() {
     //not instantiable
   }
