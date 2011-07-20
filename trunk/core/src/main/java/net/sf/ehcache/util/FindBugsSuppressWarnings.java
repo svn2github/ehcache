fd4b153cbf298aaf32baf49fc9853f2948867454
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2011-07-20 20:20:29 +0000

    fix checkstyle
    git-svn-id: http://svn.terracotta.org/svn/ehcache@4401 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/main/java/net/sf/ehcache/util/FindBugsSuppressWarnings.java
+++ trunk/core/src/main/java/net/sf/ehcache/util/FindBugsSuppressWarnings.java
@@ -1,7 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *  Copyright 2003-2010 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 package net.sf.ehcache.util;
 
 import java.lang.annotation.ElementType;
@@ -10,11 +22,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *
- * @author cdennis
+ * Annotation used to suppress FindBugs warnings in Ehcache core code.
+ * 
+ * @author Chris Dennis
  */
 @Retention(RetentionPolicy.CLASS)
-@Target(value = {ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.CONSTRUCTOR, ElementType.LOCAL_VARIABLE})
+@Target(value = {ElementType.TYPE, ElementType.FIELD, ElementType.METHOD,
+                 ElementType.PARAMETER, ElementType.CONSTRUCTOR, ElementType.LOCAL_VARIABLE })
 public @interface FindBugsSuppressWarnings {
+  /**
+   * List of suppressed FindBugs warnings
+   */
   String[] value();
 }
