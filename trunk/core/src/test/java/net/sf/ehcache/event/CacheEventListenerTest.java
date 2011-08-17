37f9a9d0c1bdf97f90c1a2d7a97e6c2bd85a8572
teck <teck@b9324663-ca0f-0410-8574-be9b3887307d>
2011-08-17 21:41:42 +0000

    removed unused import
    git-svn-id: http://svn.terracotta.org/svn/ehcache@4550 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/test/java/net/sf/ehcache/event/CacheEventListenerTest.java
+++ trunk/core/src/test/java/net/sf/ehcache/event/CacheEventListenerTest.java
@@ -32,7 +32,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
