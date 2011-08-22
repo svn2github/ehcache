1caa313ec125de10cffc1d9b6f8efe8b3ba115b0
alexsnaps <alexsnaps@b9324663-ca0f-0410-8574-be9b3887307d>
2011-08-22 12:58:28 +0000

    Adding actual asserts - this is a version for no disk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@4579 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/test/java/net/sf/ehcache/event/EvictionListenerTest.java
+++ trunk/core/src/test/java/net/sf/ehcache/event/EvictionListenerTest.java
@@ -6,11 +6,15 @@ import net.sf.ehcache.Element;
 import net.sf.ehcache.config.CacheConfiguration;
 import net.sf.ehcache.config.Configuration;
 import net.sf.ehcache.config.DiskStoreConfiguration;
+import net.sf.ehcache.config.MemoryUnit;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Alex Snaps
@@ -30,24 +34,26 @@ public class EvictionListenerTest {
 
     @Before
     public void setup() {
-        CacheConfiguration configuration = new CacheConfiguration(CACHE_NAME, 100)
-            .overflowToDisk(true)
-            .maxEntriesLocalDisk(2000);
+        CacheConfiguration configuration = new CacheConfiguration()
+            .name(CACHE_NAME)
+            .maxBytesLocalHeap(100, MemoryUnit.KILOBYTES);
+//            .overflowToDisk(true)
+//            .maxEntriesLocalDisk(2000)
         cache = new Cache(configuration);
         cacheManager.addCache(cache);
     }
 
     @Test
     public void testEvictedOnlyOnce() throws InterruptedException {
+        CountingCacheEventListener.resetCounters();
         CountingCacheEventListener countingCacheEventListener = new CountingCacheEventListener();
         cache.getCacheEventNotificationService().registerListener(countingCacheEventListener);
-        for(int i = 0; i < 10000; i++) {
+        final int amountOfElements = 10000;
+        for(int i = 0; i < amountOfElements; i++) {
             cache.get("key" + (1000 + (i % 10)));
             cache.put(new Element("key" + i, "value" + i));
         }
-        for (Object o : countingCacheEventListener.getCacheElementsEvicted(cache)) {
-            System.out.println(o.getClass().getSimpleName() + ": " + o);
-        }
+        assertThat(amountOfElements - CountingCacheEventListener.getCacheElementsEvicted(cache).size(), equalTo(cache.getSize()));
     }
 
     @After
