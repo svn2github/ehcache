9080ac74dede9370dad054ab594a92af9142e4cd
asingh <asingh@b9324663-ca0f-0410-8574-be9b3887307d>
2011-11-30 23:42:34 +0000

    DEV-6575: null reference to compoundStore and lockProvider on dispose, stop attempting rejoin on Error's
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5007 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/core/src/main/java/net/sf/ehcache/terracotta/TerracottaClient.java
+++ trunk/core/src/main/java/net/sf/ehcache/terracotta/TerracottaClient.java
@@ -356,11 +356,28 @@ public class TerracottaClient {
                             break;
                         }
                         LOGGER.warn("Caught exception while trying to rejoin cluster", e);
-                        info("Trying to rejoin again in " + REJOIN_SLEEP_MILLIS_ON_EXCEPTION + " msecs...");
-                        sleep(REJOIN_SLEEP_MILLIS_ON_EXCEPTION);
+                        if (isError(e)) {
+                            info("Rejoin worker thread exiting - unrecoverable error condition", e);
+                            shutdown = true;
+                            break;
+                        } else {
+                            info("Trying to rejoin again in " + REJOIN_SLEEP_MILLIS_ON_EXCEPTION + " msecs...");
+                            sleep(REJOIN_SLEEP_MILLIS_ON_EXCEPTION);
+                        }
                     }
                 }
             }
+        }
+
+        private boolean isError(Throwable t) {
+            while (t != null) {
+                if (t instanceof Error) {
+                    return true;
+                } else {
+                    t = t.getCause();
+                }
+            }
+            return false;
         }
 
         public synchronized boolean getAndClearForcedShutdown() {
