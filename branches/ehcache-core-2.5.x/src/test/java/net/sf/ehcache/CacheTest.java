2adf5db30ecb5ff966541a0d0d82535d69885dbe
alexsnaps <alexsnaps@b9324663-ca0f-0410-8574-be9b3887307d>
2011-12-05 17:14:26 +0000

    MNK-2856 Merged r5021 from trunk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5022 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-core-2.5.x/src/test/java/net/sf/ehcache/CacheTest.java
+++ branches/ehcache-core-2.5.x/src/test/java/net/sf/ehcache/CacheTest.java
@@ -1459,40 +1459,6 @@ public class CacheTest extends AbstractCacheTest {
     }
 
 
-    /**
-     * Shows the effect of jamming large amounts of puts into a cache that overflows to disk.
-     * The DiskStore should cause puts to back off and avoid an out of memory error.
-     */
-    @Test
-    public void testBehaviourOnDiskStoreBackUp() throws Exception {
-        Cache cache = new Cache(new CacheConfiguration().name("testBehaviourOnDiskStoreBackUp")
-                .maxElementsInMemory(1000)
-                .overflowToDisk(true)
-                .eternal(false)
-                .timeToLiveSeconds(100)
-                .timeToIdleSeconds(200)
-                .diskPersistent(false)
-                .diskExpiryThreadIntervalSeconds(0)
-                .diskSpoolBufferSizeMB(10));
-        manager.addCache(cache);
-
-        assertEquals(0, cache.getMemoryStoreSize());
-
-        Element a = null;
-        int i = 0;
-        try {
-            for (; i < 150000; i++) {
-                String key = i + "";
-                String value = key;
-                a = new Element(key, value + "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
-                cache.put(a);
-            }
-        } catch (OutOfMemoryError e) {
-            LOG.info("OutOfMemoryError: " + e.getMessage() + " " + i);
-            fail();
-        }
-    }
-
 
     /**
      * Tests using elements with null values. They should work as normal.
