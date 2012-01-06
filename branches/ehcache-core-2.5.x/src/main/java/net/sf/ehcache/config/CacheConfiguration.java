abed3eff8320d58646087c0f7f4e1d416c125937
alexsnaps <alexsnaps@b9324663-ca0f-0410-8574-be9b3887307d>
2012-01-06 20:06:07 +0000

    DEV-6576 Merged r5110 from trunk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5111 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-core-2.5.x/src/main/java/net/sf/ehcache/config/CacheConfiguration.java
+++ branches/ehcache-core-2.5.x/src/main/java/net/sf/ehcache/config/CacheConfiguration.java
@@ -1719,7 +1719,7 @@ public class CacheConfiguration implements Cloneable {
         if ((isMaxBytesLocalDiskPercentageSet() || getMaxBytesLocalDisk() > 0)
             && managerMaxBytesLocalDisk > 0 && managerMaxBytesLocalDisk - totalOnDiskAssignedMemory < 0) {
             configErrors.add(new ConfigError("Cache '" + getName()
-                                                    + "' re-allocates CacheManager's localOnDisk limit!"));
+                                                    + "' over-allocates CacheManager's localOnDisk limit!"));
         }
     }
 
@@ -1728,7 +1728,7 @@ public class CacheConfiguration implements Cloneable {
         if ((isMaxBytesLocalOffHeapPercentageSet() || getMaxBytesLocalOffHeap() > 0)
             && managerMaxBytesLocalOffHeap > 0 && managerMaxBytesLocalOffHeap - totalOffHeapAssignedMemory < 0) {
             configErrors.add(new ConfigError("Cache '" + getName()
-                                            + "' re-allocates CacheManager's localOffHeap limit!"));
+                                            + "' over-allocates CacheManager's localOffHeap limit!"));
         }
     }
 
