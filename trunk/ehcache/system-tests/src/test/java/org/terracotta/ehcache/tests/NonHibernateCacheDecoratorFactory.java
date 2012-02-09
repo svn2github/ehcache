99847137d2a44899802a05a995a7481c75457563
teck <teck@b9324663-ca0f-0410-8574-be9b3887307d>
2012-02-09 04:36:59 +0000

    clear some warnings in eclipse
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5275 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/system-tests/src/test/java/org/terracotta/ehcache/tests/NonHibernateCacheDecoratorFactory.java
+++ trunk/ehcache/system-tests/src/test/java/org/terracotta/ehcache/tests/NonHibernateCacheDecoratorFactory.java
@@ -5,21 +5,20 @@ import net.sf.ehcache.constructs.CacheDecoratorFactory;
 
 import java.util.Properties;
 
-
 public class NonHibernateCacheDecoratorFactory extends CacheDecoratorFactory {
 
-	  private static final String DAO_NAME = "daoName";
+  // private static final String DAO_NAME = "daoName";
 
-	  @Override
-	  public Ehcache createDecoratedEhcache(Ehcache cache, Properties properties) {
-	    NonHibernateCacheDecorator cacheDecorator = new NonHibernateCacheDecorator(cache);
-	    return cacheDecorator;
-	  }
+  @Override
+  public Ehcache createDecoratedEhcache(Ehcache cache, Properties properties) {
+    NonHibernateCacheDecorator cacheDecorator = new NonHibernateCacheDecorator(cache);
+    return cacheDecorator;
+  }
 
-	  @Override
-	  public Ehcache createDefaultDecoratedEhcache(Ehcache cache, Properties properties) {
-		    NonHibernateCacheDecorator cacheDecorator = new NonHibernateCacheDecorator(cache);
-		    return cacheDecorator;
-	  }
+  @Override
+  public Ehcache createDefaultDecoratedEhcache(Ehcache cache, Properties properties) {
+    NonHibernateCacheDecorator cacheDecorator = new NonHibernateCacheDecorator(cache);
+    return cacheDecorator;
+  }
 
 }
