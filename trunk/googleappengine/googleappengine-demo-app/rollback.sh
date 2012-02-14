09fd19f6804edd25e9458f2fbe666c1e2b290fac
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2012-02-14 18:42:59 +0000

    fixed version
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5287 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/googleappengine/googleappengine-demo-app/rollback.sh
+++ trunk/googleappengine/googleappengine-demo-app/rollback.sh
@@ -6,7 +6,7 @@ appname=ehcache-gae-demo
 echo "This script will roll back the app that monkeys used to test $appname, not the default one in the pom"
 echo "The script attempts to fix the occasional transaction error while deploying test app to Google server"
 
-mvn clean install -Dgae.home=$gaehome -Dgae.application.name=ehcache-gae-demo
+mvn clean install -Dgae.home=$gaehome -Dgae.application.name=ehcache-gae-demo $*
 cd target
 $gaehome/bin/appcfg.sh rollback googleappengine-demo-app-1.0-SNAPSHOT
 
