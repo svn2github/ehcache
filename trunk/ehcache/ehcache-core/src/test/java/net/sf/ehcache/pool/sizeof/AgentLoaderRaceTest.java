26555d0d07f20cfebc0204032f92dd2f54eb7083
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2012-02-17 23:59:10 +0000

    MNK-3433 : work around Oracle attach bug in JDK7/Apple JDK6 in this test - not foolproof I imagine
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5295 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/pool/sizeof/AgentLoaderRaceTest.java
+++ trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/pool/sizeof/AgentLoaderRaceTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,6 +17,28 @@ import static org.hamcrest.core.IsNull.nullValue;
  * @author Chris Dennis
  */
 public class AgentLoaderRaceTest {
+
+    private static final String JAVA_IO_TMPDIR;
+    static {
+        String tmpdir = System.getenv("TMPDIR");
+        if (tmpdir == null) {
+            tmpdir = System.getenv("TEMP");
+        }
+        if (tmpdir == null) {
+            JAVA_IO_TMPDIR = System.getProperty("java.io.tmpdir");
+        } else {
+            JAVA_IO_TMPDIR = System.setProperty("java.io.tmpdir", tmpdir);
+        }
+     }
+    
+    @AfterClass
+    public static void resetJavaIoTmpDir() {
+        if (JAVA_IO_TMPDIR == null) {
+            System.clearProperty("java.io.tmpdir");
+        } else {
+            System.setProperty("java.io.tmpdir", JAVA_IO_TMPDIR);
+        }
+    }
     
     /*
      * This test tries to expose an agent loading race seen in MNK-3255.
