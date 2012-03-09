0398db5cfbea64b08fac33727347343185b05db1
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2012-03-09 01:07:36 +0000

    removed unused imports
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5362 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/main/java/net/sf/ehcache/terracotta/TerracottaClusteredInstanceHelper.java
+++ trunk/ehcache/ehcache-core/src/main/java/net/sf/ehcache/terracotta/TerracottaClusteredInstanceHelper.java
@@ -20,8 +20,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
