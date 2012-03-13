fa3f6b0f9f7c077cdbb106d6eda7e6667ae121ba
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2012-03-13 20:31:59 +0000

    MNK-3549 : split apart latency and correctness testing
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5394 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/util/TimestamperTest.java
+++ trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/util/TimestamperTest.java
@@ -1,14 +1,16 @@
 package net.sf.ehcache.util;
 
-import org.junit.Test;
-
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static org.hamcrest.CoreMatchers.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.OrderingComparison.lessThan;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -20,32 +22,21 @@ public class TimestamperTest {
     public static final int THREADS  = 8;
 
     @Test
-    public void testNext() throws Exception {
-        final AtomicBoolean stopped = new AtomicBoolean(false);
+    public void testCorrectness() throws Exception {
         final ConcurrentMap<Long, Integer> values = new ConcurrentHashMap<Long, Integer>();
         final AtomicLong errors = new AtomicLong();
-        final AtomicLong totalRuns = new AtomicLong();
-        final AtomicLong stoppedAt = new AtomicLong();
         
         Thread[] threads = new Thread[THREADS];
         for(int i =0; i < THREADS; i++) {
             threads[i] = new Thread() {
 
-                long runs;
-
                 @Override
                 public void run() {
-                    while (!stopped.get() && runs < (TOTAL_RUNS / THREADS)) {
-//                        Timestamper.next();
-                        ++runs;
+                    for (int i = 0; i < (TOTAL_RUNS / THREADS); i++) {
                         if(values.putIfAbsent(Timestamper.next(), 0) != null) {
                             errors.incrementAndGet();
                         }
                     }
-                    if (stopped.compareAndSet(false, true)) {
-                      stoppedAt.set(System.nanoTime());
-                    }
-                    totalRuns.addAndGet(runs);
                 }
             };
         }
@@ -55,10 +46,48 @@ public class TimestamperTest {
         for (Thread thread : threads) {
             thread.join();
         }
-        assertThat("Shouldn't wait that long for all threads to join!",
-            TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - stoppedAt.get()), is(0L));
-        // Only meaning full if you don't put values in the chm!
-//        System.out.println(totalRuns.get() / DURATION / THREADS + " tps per thread" );
         assertThat(errors.get(), is(0L));
+    }
+    
+    @Test
+    public void testLatency() throws Exception {
+        final AtomicBoolean stopped = new AtomicBoolean(false);
+        final long[] maxima = new long[THREADS];
+        final int[] loops = new int[THREADS];
+        Thread[] threads = new Thread[THREADS];
+        for(int i =0; i < THREADS; i++) {
+            final int index = i;
+            threads[index] = new Thread() {
+
+                @Override
+                public void run() {
+                    long max = 0;
+                    int runs;
+                    for (runs = 0; !stopped.get() && runs < (TOTAL_RUNS / THREADS); runs++) {
+                        long start = System.nanoTime();
+                        Timestamper.next();
+                        long duration = System.nanoTime() - start;
+                        max = Math.max(max, duration);
+                    }
+                    stopped.set(true);
+                    maxima[index] = max;
+                    loops[index] = runs;
+                }
+            };
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        
+        for (int i = 0; i < THREADS; i++) {
+            System.out.println(threads[i] + " " + loops[i] + " runs, maximum latency " + TimeUnit.NANOSECONDS.toMillis(maxima[i]) + "ms");
+        }
+        
+        for (int i = 0; i < THREADS; i++) {
+            Assert.assertThat(maxima[i], lessThan(TimeUnit.MILLISECONDS.toNanos(200)));
+        }
     }
 }
