843fa810305cb559edd08aebac3e1751f7d603ea
npurwar <npurwar@b9324663-ca0f-0410-8574-be9b3887307d>
2012-04-04 10:21:28 +0000

    merge 5424
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5447 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/march-madness-rc/src/main/java/net/sf/ehcache/constructs/nonstop/concurrency/NonstopSync.java
+++ branches/march-madness-rc/src/main/java/net/sf/ehcache/constructs/nonstop/concurrency/NonstopSync.java
@@ -16,8 +16,11 @@
 
 package net.sf.ehcache.constructs.nonstop.concurrency;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import net.sf.ehcache.concurrent.LockType;
 import net.sf.ehcache.concurrent.Sync;
+import net.sf.ehcache.config.NonstopConfiguration;
 import net.sf.ehcache.config.TimeoutBehaviorConfiguration.TimeoutBehaviorType;
 import net.sf.ehcache.constructs.nonstop.NonstopActiveDelegateHolder;
 import net.sf.ehcache.constructs.nonstop.store.NonstopStore;
@@ -35,6 +38,7 @@ class NonstopSync implements Sync {
     private final ExplicitLockingContextThreadLocal explicitLockingContextThreadLocal;
     private final Object key;
     private final NonstopActiveDelegateHolder nonstopActiveDelegateHolder;
+    private final NonstopConfiguration nonstopConfiguration;
 
     /**
      * Constructor accepting the {@link NonstopStore} and the actual {@link Sync}
@@ -45,11 +49,12 @@ class NonstopSync implements Sync {
      * @param key2
      */
     public NonstopSync(NonstopStore nonstopStore, NonstopActiveDelegateHolder nonstopActiveDelegateHolder,
-            ExplicitLockingContextThreadLocal explicitLockingContextThreadLocal, Object key) {
+            ExplicitLockingContextThreadLocal explicitLockingContextThreadLocal, Object key, NonstopConfiguration nonstopConfiguration) {
         this.nonstopStore = nonstopStore;
         this.nonstopActiveDelegateHolder = nonstopActiveDelegateHolder;
         this.explicitLockingContextThreadLocal = explicitLockingContextThreadLocal;
         this.key = key;
+        this.nonstopConfiguration = nonstopConfiguration;
     }
 
     /**
@@ -81,20 +86,16 @@ class NonstopSync implements Sync {
      * {@inheritDoc}
      */
     public void lock(final LockType type) {
-        final ExplicitLockingContext appThreadLockContext = explicitLockingContextThreadLocal.getCurrentThreadLockContext();
-        nonstopStore.executeClusterOperation(new ExplicitLockingClusterOperation<Void>() {
+        boolean acquired = false;
+        try {
+            acquired = tryLock(type, nonstopConfiguration.getTimeoutMillis());
+        } catch (InterruptedException e) {
+            // NonStop exception would be thrown automatically as acquired would be false
+        }
 
-            public Void performClusterOperation() {
-                nonstopActiveDelegateHolder.getUnderlyingCacheLockProvider().getSyncForKey(key).lock(type);
-                appThreadLockContext.lockAcquired(NonstopThreadUniqueIdProvider.getCurrentNonstopThreadUniqueId());
-                return null;
-            }
-
-            public Void performClusterOperationTimedOut(final TimeoutBehaviorType configuredTimeoutBehavior) {
-                // throw exception for all behaviors
-                throw new LockOperationTimedOutNonstopException("lock() timed out");
-            }
-        });
+        if (!acquired) {
+            throw new LockOperationTimedOutNonstopException("Lock timed out");
+        }
     }
 
     /**
@@ -102,22 +103,8 @@ class NonstopSync implements Sync {
      */
     public boolean tryLock(final LockType type, final long msec) throws InterruptedException {
         final ExplicitLockingContext appThreadLockContext = explicitLockingContextThreadLocal.getCurrentThreadLockContext();
-        return nonstopStore.executeClusterOperation(new ExplicitLockingClusterOperation<Boolean>() {
-
-            public Boolean performClusterOperation() throws Exception {
-                final boolean lockAcquired = nonstopActiveDelegateHolder.getUnderlyingCacheLockProvider().getSyncForKey(key)
-                        .tryLock(type, msec);
-                if (lockAcquired) {
-                    appThreadLockContext.lockAcquired(NonstopThreadUniqueIdProvider.getCurrentNonstopThreadUniqueId());
-                }
-                return lockAcquired;
-            }
-
-            public Boolean performClusterOperationTimedOut(final TimeoutBehaviorType configuredTimeoutBehavior) {
-                // throw exception for all behaviors
-                throw new LockOperationTimedOutNonstopException("tryLock() timed out");
-            }
-        });
+        return nonstopStore.executeClusterOperation(new ExplicitLockingClusterOperationImpl(type, msec, appThreadLockContext,
+                LockOperationType.TRY_LOCK));
     }
 
     /**
@@ -125,11 +112,107 @@ class NonstopSync implements Sync {
      */
     public void unlock(final LockType type) {
         final ExplicitLockingContext appThreadLockContext = explicitLockingContextThreadLocal.getCurrentThreadLockContext();
-        nonstopStore.executeClusterOperation(new ExplicitLockingClusterOperation<Void>() {
+        nonstopStore.executeClusterOperation(new ExplicitLockingClusterOperationImpl(type, -1, appThreadLockContext,
+                LockOperationType.UNLOCK));
+    }
 
-            public Void performClusterOperation() {
+    /**
+     * For try lock and unlock non stop cases
+     *
+     * @author npurwar
+     */
+    private class ExplicitLockingClusterOperationImpl implements ExplicitLockingClusterOperation<Boolean> {
+        private final AtomicBoolean operationCompleted = new AtomicBoolean(false);
+        private final LockType type;
+        private final long timeout;
+        private final LockOperationType lockOperationType;
+        private final ExplicitLockingContext appThreadLockContext;
+        private volatile OperationState state = OperationState.EXECUTING;
+
+        public ExplicitLockingClusterOperationImpl(LockType type, long timeout, ExplicitLockingContext appThreadLockContext,
+                LockOperationType lockOperationType) {
+            this.type = type;
+            this.timeout = timeout;
+            this.appThreadLockContext = appThreadLockContext;
+            this.lockOperationType = lockOperationType;
+        }
+
+        public Boolean performClusterOperation() throws Exception {
+            final boolean success = lockOperationType.performOperation(appThreadLockContext, nonstopActiveDelegateHolder, key, timeout,
+                    type, nonstopConfiguration);
+            executionComplete();
+
+            if (!isExecutionComplete()) {
+                lockOperationType.rollback(appThreadLockContext, nonstopActiveDelegateHolder, key, type, success);
+            }
+
+            return isExecutionComplete() && success;
+        }
+
+        public Boolean performClusterOperationTimedOut(final TimeoutBehaviorType configuredTimeoutBehavior) {
+            operationTimedOut();
+
+            if (isOperationTimedOut()) {
+                // throw exception for all behaviors
+                throw new LockOperationTimedOutNonstopException("tryLock() timed out");
+            }
+            return true;
+        }
+
+        private synchronized void executionComplete() {
+            this.state = state.executionComplete();
+        }
+
+        private synchronized void operationTimedOut() {
+            this.state = state.operationTimedOut();
+        }
+
+        private synchronized boolean isOperationTimedOut() {
+            return state.isOperationTimedOut();
+        }
+
+        private synchronized boolean isExecutionComplete() {
+            return state.isExecutionComplete();
+        }
+
+    }
+
+    /**
+     * enum for try lock and unlock opertation types
+     *
+     * @author npurwar
+     */
+    private enum LockOperationType {
+        TRY_LOCK {
+            @Override
+            public boolean performOperation(ExplicitLockingContext appThreadLockContext,
+                    NonstopActiveDelegateHolder nonstopActiveDelegateHolder, Object key, long timeout, LockType type,
+                    NonstopConfiguration config) throws Exception {
+                timeout = Math.min(config.getTimeoutMillis(), timeout);
+                boolean success = nonstopActiveDelegateHolder.getUnderlyingCacheLockProvider().getSyncForKey(key).tryLock(type, timeout);
+                if (success) {
+                    appThreadLockContext.lockAcquired(NonstopThreadUniqueIdProvider.getCurrentNonstopThreadUniqueId());
+                }
+                return success;
+            }
+
+            @Override
+            public void rollback(ExplicitLockingContext appThreadLockContext, NonstopActiveDelegateHolder nonstopActiveDelegateHolder,
+                    Object key, LockType type, boolean success) {
+                if (success) {
+                    nonstopActiveDelegateHolder.getUnderlyingCacheLockProvider().getSyncForKey(key).unlock(type);
+                    appThreadLockContext.lockReleased();
+                }
+            }
+        },
+        UNLOCK {
+            @Override
+            public boolean performOperation(ExplicitLockingContext appThreadLockContext,
+                    NonstopActiveDelegateHolder nonstopActiveDelegateHolder, Object key, long timeout, LockType type,
+                    NonstopConfiguration config) throws Exception {
                 try {
-                    if (appThreadLockContext.areLocksAcquiredByOtherThreads(NonstopThreadUniqueIdProvider.getCurrentNonstopThreadUniqueId())) {
+                    if (appThreadLockContext
+                            .areLocksAcquiredByOtherThreads(NonstopThreadUniqueIdProvider.getCurrentNonstopThreadUniqueId())) {
                         // some other thread has acquired locks other than this current nonstop thread
                         // this means rejoin has happened, lock acquired by previous nonstop thread
                         throw new InvalidLockStateAfterRejoinException();
@@ -140,16 +223,86 @@ class NonstopSync implements Sync {
                     // clean up the lock stack anyway
                     appThreadLockContext.lockReleased();
                 }
-                return null;
+                return true;
             }
 
-            public Void performClusterOperationTimedOut(final TimeoutBehaviorType configuredTimeoutBehavior) {
-                // always clean up lock stack for unlock even on timeouts
-                appThreadLockContext.lockReleased();
-                // throw exception for all behaviors
-                throw new LockOperationTimedOutNonstopException("unlock() timed out");
+            @Override
+            public void rollback(ExplicitLockingContext appThreadLockContext, NonstopActiveDelegateHolder nonstopActiveDelegateHolder,
+                    Object key, LockType type, boolean success) {
+                // NO OP
             }
-        });
+        };
+
+        public abstract boolean performOperation(ExplicitLockingContext appThreadLockContext,
+                NonstopActiveDelegateHolder nonstopActiveDelegateHolder, Object key, long timeout, LockType type,
+                NonstopConfiguration config) throws Exception;
+
+        public abstract void rollback(ExplicitLockingContext appThreadLockContext, NonstopActiveDelegateHolder nonstopActiveDelegateHolder,
+                Object key, LockType type, boolean success);
+    }
+
+    /**
+     * enum for lock state diagram
+     *
+     * @author npurwar
+     *
+     */
+    private enum OperationState {
+        EXECUTING {
+            @Override
+            OperationState executionComplete() {
+                return EXECUTION_COMPLETE;
+            }
+
+            @Override
+            OperationState operationTimedOut() {
+                return OPERATION_TIMED_OUT;
+            }
+        },
+        EXECUTION_COMPLETE {
+            @Override
+            OperationState executionComplete() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            OperationState operationTimedOut() {
+                return EXECUTION_COMPLETE;
+            }
+
+            @Override
+            boolean isExecutionComplete() {
+                return true;
+            }
+        },
+        OPERATION_TIMED_OUT {
+            @Override
+            OperationState executionComplete() {
+                return OPERATION_TIMED_OUT;
+            }
+
+            @Override
+            OperationState operationTimedOut() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            boolean isOperationTimedOut() {
+                return true;
+            }
+        };
+
+        abstract OperationState executionComplete();
+
+        abstract OperationState operationTimedOut();
+
+        boolean isOperationTimedOut() {
+            return false;
+        }
+
+        boolean isExecutionComplete() {
+            return false;
+        }
     }
 
 }
