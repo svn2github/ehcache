34ab143f08294fed9414b780ad8b73ef6bcd88dc
gkeim <gkeim@b9324663-ca0f-0410-8574-be9b3887307d>
2012-04-06 17:50:07 +0000

    fix build
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5463 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/main/java/net/sf/ehcache/Cache.java
+++ trunk/ehcache/ehcache-core/src/main/java/net/sf/ehcache/Cache.java
@@ -1636,7 +1636,9 @@ public class Cache implements InternalEhcache, StoreListener {
             return null;
         }
 
-        if(keys.isEmpty()) return Collections.EMPTY_MAP;
+        if (keys.isEmpty()) {
+            return Collections.EMPTY_MAP;
+        }
 
         if (isStatisticsEnabled()) {
             long start = System.currentTimeMillis();
