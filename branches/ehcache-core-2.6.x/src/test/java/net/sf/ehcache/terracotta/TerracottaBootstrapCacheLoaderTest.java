a920123b9cf4c1a711ec322ab1390a33a732bd40
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2012-04-30 22:14:23 +0000

    unused import
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5551 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-core-2.6.x/src/test/java/net/sf/ehcache/terracotta/TerracottaBootstrapCacheLoaderTest.java
+++ branches/ehcache-core-2.6.x/src/test/java/net/sf/ehcache/terracotta/TerracottaBootstrapCacheLoaderTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashSet;
