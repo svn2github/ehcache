e68c962cdb67b4560319e7c664cf23e9f42f31cc
byoukste <byoukste@b9324663-ca0f-0410-8574-be9b3887307d>
2012-05-17 22:50:15 +0000

    Management Tools DEV-7275:            - Eliminate spring in the embedded agent by introducing a ServiceLocator pattern for dependency injection.            - Some minor packaging refactor, fixing object relationships
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5685 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/management-ehcache-impl-2.6.x/management-ehcache-impl/src/test/java/net/sf/ehcache/management/service/impl/TestCacheEntityBuilder.java
+++ branches/management-ehcache-impl-2.6.x/management-ehcache-impl/src/test/java/net/sf/ehcache/management/service/impl/TestCacheEntityBuilder.java
@@ -2,7 +2,7 @@
  * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
  * notice. All rights reserved.
  */
-package net.sf.ehcache.management.services.impl;
+package net.sf.ehcache.management.service.impl;
 
 import junit.framework.Assert;
 import net.sf.ehcache.management.resource.CacheEntity;
