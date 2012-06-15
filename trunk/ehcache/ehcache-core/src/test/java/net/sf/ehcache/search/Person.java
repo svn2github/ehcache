9acf902b81de5bd073c06f2317c6fcdc3126bd74
teck <teck@b9324663-ca0f-0410-8574-be9b3887307d>
2012-06-15 18:32:40 +0000

    added generated equals and hashcode
    git-svn-id: http://svn.terracotta.org/svn/ehcache@5796 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/search/Person.java
+++ trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/search/Person.java
@@ -41,6 +41,43 @@ public class Person implements Serializable {
         this.dept = dept;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + age;
+        result = prime * result + ((dept == null) ? 0 : dept.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (age != other.age)
+            return false;
+        if (dept == null) {
+            if (other.dept != null)
+                return false;
+        } else if (!dept.equals(other.dept))
+            return false;
+        if (gender != other.gender)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
     public String getName() {
         return name;
     }
@@ -59,7 +96,8 @@ public class Person implements Serializable {
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + "(name:" + name + ", age:" + age + ", sex:" + gender.name().toLowerCase() +", dept: " + dept + ")";
+        return getClass().getSimpleName() + "(name:" + name + ", age:" + age + ", sex:" + gender.name().toLowerCase() + ", dept: " + dept
+                + ")";
     }
 
     public enum Gender {
