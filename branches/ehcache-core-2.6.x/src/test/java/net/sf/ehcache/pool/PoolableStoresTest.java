d5a346dc293f3b184374eba492571aa8e7cf4ed3
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2012-07-25 14:58:13 +0000

    MNK-3837 : merge 5316 from trunk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6057 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-core-2.6.x/src/test/java/net/sf/ehcache/pool/PoolableStoresTest.java
+++ branches/ehcache-core-2.6.x/src/test/java/net/sf/ehcache/pool/PoolableStoresTest.java
@@ -3,6 +3,7 @@ package net.sf.ehcache.pool;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
+import net.sf.ehcache.Status;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,7 @@ import static org.junit.Assert.assertThat;
 public class PoolableStoresTest {
 
     public static final String DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP = "net.sf.ehcache.sizeofengine.default";
+    private CacheManager cacheManager;
 
     @Before
     public void setUp() throws Exception {
@@ -26,16 +28,19 @@ public class PoolableStoresTest {
 
     @After
     public void tearDown() throws Exception {
+        if(cacheManager != null && cacheManager.getStatus() == Status.STATUS_ALIVE) {
+            cacheManager.shutdown();
+        }
         System.getProperties().remove(DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP);
         assertThat(System.getProperties().getProperty(DEFAULT_CACHE_MANAGER_SIZE_OF_ENGINE_PROP), nullValue());
     }
 
     @Test
     public void test() throws Exception {
-        CacheManager cm = new CacheManager(PoolableStoresTest.class.getResourceAsStream("/pool/ehcache-heap-disk.xml"));
+        cacheManager = new CacheManager(PoolableStoresTest.class.getResourceAsStream("/pool/ehcache-heap-disk.xml"));
 
-        Cache memoryOnlyCache = cm.getCache("memoryOnly");
-        Cache overflowToDiskCache = cm.getCache("overflowToDisk");
+        Cache memoryOnlyCache = cacheManager.getCache("memoryOnly");
+        Cache overflowToDiskCache = cacheManager.getCache("overflowToDisk");
 
         for (int i = 0; i < 100; i++) {
             memoryOnlyCache.put(new Element(i, "" + i));
@@ -50,7 +55,6 @@ public class PoolableStoresTest {
         assertEquals(64, memoryOnlyCache.getSize() + overflowToDiskCache.getSize());
 
 
-        cm.shutdown();
     }
 
 }
