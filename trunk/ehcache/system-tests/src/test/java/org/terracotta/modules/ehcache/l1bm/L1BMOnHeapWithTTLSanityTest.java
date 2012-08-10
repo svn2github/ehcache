fdd9292806bf422d9890e70fbc25b2fb9c152d44
twu <twu@b9324663-ca0f-0410-8574-be9b3887307d>
2012-08-10 22:23:17 +0000

    MNK-3931: Merged r36235
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6132 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/system-tests/src/test/java/org/terracotta/modules/ehcache/l1bm/L1BMOnHeapWithTTLSanityTest.java
+++ trunk/ehcache/system-tests/src/test/java/org/terracotta/modules/ehcache/l1bm/L1BMOnHeapWithTTLSanityTest.java
@@ -34,6 +34,8 @@ public class L1BMOnHeapWithTTLSanityTest extends AbstractCacheTestBase {
   public static class App extends ClientBase {
     private final ToolkitBarrier barrier;
 
+    private int                  index;
+
     public App(String[] args) {
       super(args);
       this.barrier = getClusteringToolkit().getBarrier("test", NODE_COUNT);
@@ -70,13 +72,13 @@ public class L1BMOnHeapWithTTLSanityTest extends AbstractCacheTestBase {
 
     private void testL1BigMemorySanity(Cache cache, boolean shouldWait) throws InterruptedException,
         BrokenBarrierException {
-      int index = barrier.await();
+      index = barrier.await();
       int numOfElements = 500;
       if (index == 0) {
         for (int i = 0; i < numOfElements; i++) {
           cache.put(new Element("key" + i, "val" + i));
         }
-        System.out.println("XXXXX done with putting " + cache.getSize() + " entries");
+        println("done with putting " + cache.getSize() + " entries");
       }
       barrier.await();
       if (shouldWait) {
@@ -85,7 +87,7 @@ public class L1BMOnHeapWithTTLSanityTest extends AbstractCacheTestBase {
         }
       }
       Assert.assertEquals(numOfElements, cache.getSize());
-      System.out.println("XXXXXX client " + index + " cache size: " + cache.getSize() + " local: "
+      println("client " + index + " cache size: " + cache.getSize() + " local: "
                          + cache.getMemoryStoreSize());
       if (index == 0) {
         Assert.assertTrue(cache.getMemoryStoreSize() > 0);
@@ -95,14 +97,14 @@ public class L1BMOnHeapWithTTLSanityTest extends AbstractCacheTestBase {
 
       barrier.await();
 
-      System.out.println("XXXXXX testing get");
+      println("testing get");
       for (int i = 0; i < numOfElements; i++) {
         Assert.assertNotNull("value for key" + i + " is null", cache.get("key" + i));
       }
       Assert.assertTrue(cache.getMemoryStoreSize() > 0);
 
       barrier.await();
-      System.out.println("XXXX done with basic get, now removing random entries...");
+      println("done with basic get, now removing random entries...");
       Set removedKeySet = new HashSet<String>();
       for (int i = 0; i < numOfElements; i++) {
         if (i % 10 == 0) {
@@ -120,26 +122,34 @@ public class L1BMOnHeapWithTTLSanityTest extends AbstractCacheTestBase {
       }
       barrier.await();
 
-      System.out.println("XXXXX removed " + removedKeySet.size() + " elemets. Cache size: " + cache.getSize());
+      println("removed " + removedKeySet.size() + " elemets. Cache size: " + cache.getSize());
 
-      System.out.println("XXXX testing get after remove.");
+      println("testing get after remove.");
       for (int i = 0; i < numOfElements; i++) {
         String key = "key" + i;
         if (removedKeySet.contains(key)) {
-          Assert.assertNull("value for " + key + " is not null", cache.get(key));
+          Assert.assertNull(nodeId() + " value for " + key + " is not null", cache.get(key));
         } else {
-          Assert.assertNotNull("value for " + key + " is null", cache.get(key));
+          Assert.assertNotNull(nodeId() + " value for " + key + " is null", cache.get(key));
         }
       }
 
-      // wait for 90 secs to get element expired
-      System.out.println("waiting fo elements to get expired...");
+      // wait for 30 secs to get element expired
+      println("waiting fo elements to get expired...");
       Thread.sleep(30000);
-      System.out.println("All elements should be null after expiration...");
+      println("All elements should be null after expiration...");
       for (int i = 0; i < numOfElements; i++) {
-        Assert.assertNull("value for key" + i + " is not null", cache.get("key" + i));
+        Assert.assertNull(nodeId() + " value for key" + i + " is not null", cache.get("key" + i));
       }
-      System.out.println("XXXXXX done with " + cache.getName());
+      println("done with " + cache.getName());
+    }
+
+    private void println(String msg) {
+      System.out.println(nodeId() + " " + msg);
+    }
+
+    private String nodeId() {
+      return "Node[" + index + "]";
     }
 
     private Cache createCache(String cacheName, CacheManager cm, Consistency consistency,
