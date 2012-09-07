aa953bb050387ac9b1affd57e083aa59c0226995
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2012-09-07 14:55:41 +0000

    MNK-4009 : merge 6242 from trunk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6244 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-core-2.5.x/src/test/java/net/sf/ehcache/distribution/ManualRMIPeerProviderTest.java
+++ branches/ehcache-core-2.5.x/src/test/java/net/sf/ehcache/distribution/ManualRMIPeerProviderTest.java
@@ -16,26 +16,20 @@
 
 package net.sf.ehcache.distribution;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import net.sf.ehcache.AbstractCacheTest;
 import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Ehcache;
 
 import org.junit.Before;
-import org.junit.Test;
 
 /**
  * @author <a href="mailto:gluck@thoughtworks.com">Greg Luck</a>
  * @version $Id$
  */
 public class ManualRMIPeerProviderTest extends MulticastRMIPeerProviderTest {
-
 
     /**
      * {@inheritDoc}
@@ -57,40 +51,4 @@ public class ManualRMIPeerProviderTest extends MulticastRMIPeerProviderTest {
         //Allow cluster setup
         waitForClusterMembership(10, TimeUnit.SECONDS, Collections.singleton("sampleCache1"), manager1, manager2, manager3);
     }
-
-
-    /**
-     * test remote cache peers
-     */
-    @Override
-    @Test
-    public void testProviderFromCacheManager() throws InterruptedException {
-        Ehcache m1sampleCache1 = manager1.getCache("sampleCache1");
-        Thread.sleep(2000);
-
-        List peerUrls = manager1.getCacheManagerPeerProvider("RMI").listRemoteCachePeers(m1sampleCache1);
-        assertEquals(expectedPeers(), peerUrls.size());
-
-        Ehcache m2sampleCache1 = manager2.getCache("sampleCache1");
-        assertFalse(m1sampleCache1.getGuid().equals(m2sampleCache1.getGuid()));
-
-        List peerUrls2 = manager2.getCacheManagerPeerProvider("RMI").listRemoteCachePeers(m2sampleCache1);
-        assertEquals(expectedPeers(), peerUrls2.size());
-
-        Ehcache m3sampleCache1 = manager3.getCache("sampleCache1");
-        assertFalse(m1sampleCache1.getGuid().equals(m3sampleCache1.getGuid()));
-
-        List peerUrls3 = manager3.getCacheManagerPeerProvider("RMI").listRemoteCachePeers(m3sampleCache1);
-        assertEquals(expectedPeers(), peerUrls3.size());
-
-        //Now remove a node, wait for the cluster to self-heal and then test
-        manager1.shutdown();
-        Thread.sleep(1000);
-        peerUrls3 = manager3.getCacheManagerPeerProvider("RMI").listRemoteCachePeers(m3sampleCache1);
-        //The manual provider removes the cache peer that was not reachable
-        assertEquals(1, peerUrls3.size());
-
-    }
-
-
 }
