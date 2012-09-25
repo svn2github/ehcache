d707e64f934bdff7444810373b4b55ed9259848a
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2012-09-25 20:33:19 +0000

    git-svn-id: http://svn.terracotta.org/svn/ehcache@6353 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/findbugs/ehcache-core/src/main/java/net/sf/ehcache/store/disk/DiskStorageFactory.java
+++ branches/findbugs/ehcache-core/src/main/java/net/sf/ehcache/store/disk/DiskStorageFactory.java
@@ -718,6 +718,7 @@ public class DiskStorageFactory {
             hitCount++;
             hitCount++;
             hitCount++;
+            hitCount++;
             this.expiry = element.getExpirationTime();
         }
 
