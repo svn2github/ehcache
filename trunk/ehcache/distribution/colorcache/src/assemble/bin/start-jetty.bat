8b6315ead3fee81b9e03f597d326220940ba3396
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2012-10-12 16:39:07 +0000

    fixed for toolkit renaming in trunk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6412 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/distribution/colorcache/src/assemble/bin/start-jetty.bat
+++ trunk/ehcache/distribution/colorcache/src/assemble/bin/start-jetty.bat
@@ -32,6 +32,7 @@ set start_jar=%jetty_home%\start.jar
 set /a stop_port=jetty_instance + 2
 
 cd %jetty_work_dir%
+mkdir logs
 echo Starting Jetty %jetty_instance%...
 start "Jetty %jetty_instance%" %JAVA_HOME%\bin\java -Dtc.install-root=%tc_install_dir% -Xmx256m -XX:MaxPermSize=128m -Dconfig.home=%jetty_work_dir% -Djetty.home=%jetty_home% -DSTOP.PORT=%stop_port% -DSTOP.KEY=secret -jar %start_jar% conf.xml 
 echo.
