edcc78bfb05e236e7f4bf8db716e58b45ba8cf75
nbangarw <nbangarw@b9324663-ca0f-0410-8574-be9b3887307d>
2012-11-30 14:40:46 +0000

    pvt nonstop merge from trunk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6598 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/nonstop/system-tests/src/test/java/org/terracotta/modules/ehcache/ToolkitClientAccessor.java
+++ branches/nonstop/system-tests/src/test/java/org/terracotta/modules/ehcache/ToolkitClientAccessor.java
@@ -6,7 +6,6 @@ package org.terracotta.modules.ehcache;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheAccessor;
 import net.sf.ehcache.config.CacheConfiguration;
-import net.sf.ehcache.config.TerracottaConfiguration;
 import net.sf.ehcache.store.Store;
 
 import org.terracotta.toolkit.Toolkit;
@@ -29,12 +28,6 @@ public class ToolkitClientAccessor {
       throw new AssertionError("Toolkit can only be looked up for Clustered Caches - unclustered ehcache name: "
                                + cache.getName());
     }
-  }
-
-  private static boolean isNonStopEnabled(CacheConfiguration cacheConfiguration) {
-    TerracottaConfiguration terracottaConfiguration = cacheConfiguration.getTerracottaConfiguration();
-    return terracottaConfiguration == null || terracottaConfiguration.isNonstopEnabled();
-
   }
 
 }
