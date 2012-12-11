a4a924bcdae4f6945f1b786ff9099e7668fbfde0
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2012-12-11 16:18:19 +0000

    add queries for basic cache operations
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6678 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-statistics-rework/ehcache-core/src/main/java/net/sf/ehcache/statisticsV2/extended/OperationType.java
+++ branches/ehcache-statistics-rework/ehcache-core/src/main/java/net/sf/ehcache/statisticsV2/extended/OperationType.java
@@ -4,52 +4,62 @@
  */
 package net.sf.ehcache.statisticsV2.extended;
 
+import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheOperationOutcomes;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.store.Store;
 import net.sf.ehcache.store.StoreOperationOutcomes;
 import net.sf.ehcache.transaction.xa.XaCommitOutcome;
 import net.sf.ehcache.transaction.xa.XaRecoveryOutcome;
 import net.sf.ehcache.transaction.xa.XaRollbackOutcome;
 import org.terracotta.context.query.Query;
 
+import static org.terracotta.context.query.Matchers.*;
+import org.terracotta.context.query.QueryBuilder;
+import static org.terracotta.context.query.QueryBuilder.*;
+import org.terracotta.statistics.OperationStatistic;
+
 /**
  *
  * @author cdennis
  */
  enum OperationType {
-    CACHE_GET(CacheOperationOutcomes.GetOutcome.class),
-    CACHE_PUT(CacheOperationOutcomes.PutOutcome.class),
-    CACHE_REMOVE(CacheOperationOutcomes.RemoveOutcome.class),
+    CACHE_GET(CacheOperationOutcomes.GetOutcome.class, cache().chain(childStatistic("get").build()).ensureUnique().build()),
+    CACHE_PUT(CacheOperationOutcomes.PutOutcome.class, cache().chain(childStatistic("put").build()).ensureUnique().build()),
+    CACHE_REMOVE(CacheOperationOutcomes.RemoveOutcome.class, cache().chain(childStatistic("remove").build()).ensureUnique().build()),
     
-    HEAP_GET(StoreOperationOutcomes.GetOutcome.class),
-    HEAP_PUT(StoreOperationOutcomes.PutOutcome.class),
-    HEAP_REMOVE(StoreOperationOutcomes.RemoveOutcome.class),
+    HEAP_GET(StoreOperationOutcomes.GetOutcome.class, queryBuilder().empty().build()),
+    HEAP_PUT(StoreOperationOutcomes.PutOutcome.class, queryBuilder().empty().build()),
+    HEAP_REMOVE(StoreOperationOutcomes.RemoveOutcome.class, queryBuilder().empty().build()),
     
-    OFFHEAP_GET(StoreOperationOutcomes.GetOutcome.class),
-    OFFHEAP_PUT(StoreOperationOutcomes.PutOutcome.class),
-    OFFHEAP_REMOVE(StoreOperationOutcomes.RemoveOutcome.class),
+    OFFHEAP_GET(StoreOperationOutcomes.GetOutcome.class, queryBuilder().empty().build()),
+    OFFHEAP_PUT(StoreOperationOutcomes.PutOutcome.class, queryBuilder().empty().build()),
+    OFFHEAP_REMOVE(StoreOperationOutcomes.RemoveOutcome.class, queryBuilder().empty().build()),
     
-    DISK_GET(StoreOperationOutcomes.GetOutcome.class),
-    DISK_PUT(StoreOperationOutcomes.PutOutcome.class),
-    DISK_REMOVE(StoreOperationOutcomes.RemoveOutcome.class),
+    DISK_GET(StoreOperationOutcomes.GetOutcome.class, queryBuilder().empty().build()),
+    DISK_PUT(StoreOperationOutcomes.PutOutcome.class, queryBuilder().empty().build()),
+    DISK_REMOVE(StoreOperationOutcomes.RemoveOutcome.class, queryBuilder().empty().build()),
     
-    XA_COMMIT(XaCommitOutcome.class),
-    XA_ROLLBACK(XaRollbackOutcome.class),
-    XA_RECOVERY(XaRecoveryOutcome.class),
+    XA_COMMIT(XaCommitOutcome.class, queryBuilder().empty().build()),
+    XA_ROLLBACK(XaRollbackOutcome.class, queryBuilder().empty().build()),
+    XA_RECOVERY(XaRecoveryOutcome.class, queryBuilder().empty().build()),
     
-    SEARCH(CacheOperationOutcomes.SearchOutcome.class) {
+    SEARCH(CacheOperationOutcomes.SearchOutcome.class, queryBuilder().empty().build()) {
         @Override
         long interval() {
             return 10;
         }
     },
     
-    EVICTED(null),
-    EXPIRED(null);
+    EVICTED(null, queryBuilder().empty().build()),
+    EXPIRED(null, queryBuilder().empty().build());
 
     private final Class<? extends Enum> type;
+    private final Query query;
     
-    private OperationType(Class<? extends Enum> type) {
+    private OperationType(Class<? extends Enum> type, Query query) {
         this.type = type;
+        this.query = query;
     }
 
     final Class<? extends Enum> type() {
@@ -57,7 +67,7 @@ import org.terracotta.context.query.Query;
     }
     
     Query query() {
-        throw new UnsupportedOperationException();
+        return query;
     }
 
     int history() {
@@ -72,4 +82,11 @@ import org.terracotta.context.query.Query;
         return 1;
     }
   
+    static QueryBuilder cache() {
+        return queryBuilder().children().filter(context(identifier(subclassOf(Cache.class))));
+    }
+
+    static QueryBuilder childStatistic(String name) {
+        return queryBuilder().children().filter(context(allOf(identifier(subclassOf(OperationStatistic.class)), attributes(hasAttribute("name", name)))));
+    }
 }
