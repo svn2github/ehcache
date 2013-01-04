9de5699bebd4d7c40cda8f31e61a92ba429f63fe
nbangarw <nbangarw@b9324663-ca0f-0410-8574-be9b3887307d>
2013-01-04 11:53:17 +0000

    make cache event replicator nonstop
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6784 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/event/ClusteredEventReplicatorFactory.java
+++ trunk/ehcache/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/event/ClusteredEventReplicatorFactory.java
@@ -4,6 +4,8 @@
 package org.terracotta.modules.ehcache.event;
 
 import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.config.NonstopConfiguration;
+import net.sf.ehcache.event.CacheEventListener;
 
 import org.terracotta.modules.ehcache.ToolkitInstanceFactory;
 
@@ -11,19 +13,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ClusteredEventReplicatorFactory {
-  private final Map<String, ClusteredEventReplicator> eventReplicators = new HashMap<String, ClusteredEventReplicator>();
-  private final ToolkitInstanceFactory                toolkitInstanceFactory;
+  private final Map<String, CacheEventListener> eventReplicators = new HashMap<String, CacheEventListener>();
+  private final ToolkitInstanceFactory          toolkitInstanceFactory;
 
   public ClusteredEventReplicatorFactory(ToolkitInstanceFactory toolkitInstanceFactory) {
     this.toolkitInstanceFactory = toolkitInstanceFactory;
   }
 
-  public synchronized ClusteredEventReplicator getOrCreateClusteredEventReplicator(Ehcache cache) {
+  public synchronized CacheEventListener getOrCreateClusteredEventReplicator(Ehcache cache) {
     String fullyQualifiedCacheName = toolkitInstanceFactory.getFullyQualifiedCacheName(cache);
-    ClusteredEventReplicator replicator = eventReplicators.get(fullyQualifiedCacheName);
+    CacheEventListener replicator = eventReplicators.get(fullyQualifiedCacheName);
     if (replicator == null) {
-      replicator = new ClusteredEventReplicator(cache, fullyQualifiedCacheName,
-                                                toolkitInstanceFactory.getOrCreateCacheEventNotifier(cache), this);
+      NonstopConfiguration nonStopConfiguration = cache.getCacheConfiguration().getTerracottaConfiguration()
+          .getNonstopConfiguration();
+      CacheEventListener clusteredEventReplicator = new ClusteredEventReplicator(
+                                                                                 cache,
+                                                                                 fullyQualifiedCacheName,
+                                                                                 toolkitInstanceFactory
+                                                                                     .getOrCreateCacheEventNotifier(cache),
+                                                                                 this);
+      replicator = new NonStopEventReplicator(clusteredEventReplicator, toolkitInstanceFactory, nonStopConfiguration);
       eventReplicators.put(fullyQualifiedCacheName, replicator);
     }
     return replicator;
