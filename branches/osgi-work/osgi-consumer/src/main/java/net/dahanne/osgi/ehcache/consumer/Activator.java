853ecf0bf503ad47c6568e6d30930e47875c56de
adahanne <adahanne@b9324663-ca0f-0410-8574-be9b3887307d>
2013-01-04 20:23:18 +0000

    slf4j being used in the osgi-consumer, using the ThreadContext CLass Loader
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6788 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/osgi-work/osgi-consumer/src/main/java/net/dahanne/osgi/ehcache/consumer/Activator.java
+++ branches/osgi-work/osgi-consumer/src/main/java/net/dahanne/osgi/ehcache/consumer/Activator.java
@@ -2,14 +2,23 @@ package net.dahanne.osgi.ehcache.consumer;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
 
-	public void start(BundleContext context) throws Exception {
-		System.out.println(String.format("Start - %s", this.getClass().getName()));
+  private static final Logger LOG = LoggerFactory.getLogger(Activator.class.getName());
 
-		
-		Demo demo = new Demo();
+
+  public void start(BundleContext context) throws Exception {
+		System.out.println(String.format("Start - %s", this.getClass().getName()));
+  LOG.error("hello !!!");
+
+
+    Thread.currentThread().setContextClassLoader(Activator.class.getClassLoader());
+    ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+
+    Demo demo = new Demo();
 		try {
 			demo.go();
 		} catch (Exception e) {
