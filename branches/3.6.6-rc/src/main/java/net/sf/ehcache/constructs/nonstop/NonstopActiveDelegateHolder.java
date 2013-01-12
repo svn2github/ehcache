3a030567622a8cc54d0e6550c006c64e8bf52b7a
cruise <cruise@b9324663-ca0f-0410-8574-be9b3887307d>
2013-01-12 11:49:04 +0000

    3.6.6-rc
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6848 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/3.6.6-rc/src/main/java/net/sf/ehcache/constructs/nonstop/NonstopActiveDelegateHolder.java
@@ -0,0 +1,75 @@
+/**
+ *  Copyright 2003-2010 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.constructs.nonstop;
+
+import net.sf.ehcache.concurrent.CacheLockProvider;
+import net.sf.ehcache.constructs.nonstop.store.NonstopStore;
+import net.sf.ehcache.event.CacheEventListener;
+import net.sf.ehcache.store.TerracottaStore;
+
+/**
+ * Interface for resolving the active TerracottaStore, nonstop Executor Service, underlying {@link CacheLockProvider}, all of which can
+ * change on rejoin
+ *
+ * @author Abhishek Sanoujam
+ *
+ */
+public interface NonstopActiveDelegateHolder {
+
+    /**
+     * {@link TerracottaStore} got initialized again on rejoin
+     *
+     * @param newTerracottaStore
+     */
+    public void terracottaStoreInitialized(TerracottaStore newTerracottaStore);
+
+    /**
+     * Returns the current underlying {@link TerracottaStore}
+     *
+     * @return the current underlying {@link TerracottaStore}
+     */
+    public TerracottaStore getUnderlyingTerracottaStore();
+
+    /**
+     * Returns the current underlying NonstopExecutorService
+     *
+     * @return the current underlying NonstopExecutorService
+     */
+    public NonstopExecutorService getNonstopExecutorService();
+
+    /**
+     * Returns the current underlying {@link CacheLockProvider}
+     *
+     * @return the current underlying {@link CacheLockProvider}
+     */
+    public CacheLockProvider getUnderlyingCacheLockProvider();
+
+    /**
+     * Returns the nonstop store
+     *
+     * @return the nonstop store
+     */
+    public NonstopStore getNonstopStore();
+
+    /**
+     * Returns the cache event replicator
+     *
+     * @return the cache event replicator
+     */
+    public CacheEventListener getCacheEventReplicator();
+
+}
---
+++ branches/3.6.6-rc/src/test/java/net/sf/ehcache/constructs/MockDecoratorFactory.java
@@ -0,0 +1,76 @@
+/**
+ *  Copyright 2003-2010 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.constructs;
+
+import java.util.Properties;
+
+import net.sf.ehcache.Ehcache;
+
+/**
+ * @author Abhishek Sanoujam
+ */
+public class MockDecoratorFactory extends CacheDecoratorFactory {
+
+    @Override
+    public Ehcache createDecoratedEhcache(Ehcache cache, Properties properties) {
+        return new MockDecoratorFactoryCache(cache, properties, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Ehcache createDefaultDecoratedEhcache(Ehcache cache, Properties properties) {
+        return new MockDecoratorFactoryCache(cache, properties, true);
+    }
+
+    public static class MockDecoratorFactoryCache extends EhcacheDecoratorAdapter {
+
+        private final Properties properties;
+        private final String name;
+
+        public MockDecoratorFactoryCache(Ehcache underlyingCache, Properties properties, boolean forDefaultCache) {
+            super(underlyingCache);
+            this.properties = properties;
+            String tmpName = properties.getProperty("name");
+            if (forDefaultCache) {
+                if (tmpName == null || tmpName.trim().equals("")) {
+                    tmpName = underlyingCache.getName();
+                } else {
+                    tmpName = CacheDecoratorFactory.generateDefaultDecoratedCacheName(underlyingCache, tmpName);
+                }
+            }
+            this.name = tmpName;
+        }
+
+        public Properties getProperties() {
+            return properties;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public String toString() {
+            return "DecoratedTestEhcache[name=" + name + "]";
+        }
+
+    }
+
+}
