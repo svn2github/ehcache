0c5c2997261646b9a9dcba0230e410520db1da7e
cruise <cruise@b9324663-ca0f-0410-8574-be9b3887307d>
2013-01-12 12:20:23 +0000

    Tag ehcache-core-2.5.7 from 3.6.6-rc branch
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6849 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ tags/ehcache-core-2.5.7/src/test/java/net/sf/ehcache/distribution/AbstractRMITest.java
@@ -0,0 +1,176 @@
+/**
+ *  Copyright 2003-2010 Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache.distribution;
+
+import static net.sf.ehcache.util.RetryAssert.assertBy;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.rmi.server.RMISocketFactory;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.ConfigurationFactory;
+
+import org.hamcrest.collection.IsEmptyCollection;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractRMITest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractRMITest.class);
+
+    protected Configuration getConfiguration(String fileName) {
+        return ConfigurationFactory.parseConfiguration(new File(fileName));
+    }
+
+    @BeforeClass
+    public static void installRMISocketFactory() {
+        RMISocketFactory current = RMISocketFactory.getSocketFactory();
+        if (current == null) {
+            current = RMISocketFactory.getDefaultSocketFactory();
+        }
+        assertNotNull(current);
+        try {
+            RMISocketFactory.setSocketFactory(new SocketReusingRMISocketFactory(current));
+            LOG.info("Installed the SO_REUSEADDR setting socket factory");
+        } catch (IOException e) {
+            LOG.warn("Couldn't register the SO_REUSEADDR setting socket factory", e);
+        }
+    }
+
+    @BeforeClass
+    public static void checkActiveThreads() {
+        assertThat(getActiveReplicationThreads(), IsEmptyCollection.<Thread>empty());
+    }
+
+    protected static Set<Thread> getActiveReplicationThreads() {
+        Set<Thread> threads = new HashSet<Thread>();
+        for (Thread thread : JVMUtil.enumerateThreads()) {
+            if (thread.getName().equals("Replication Thread")) {
+                threads.add(thread);
+            }
+        }
+        return threads;
+    }
+    
+    @Before
+    public void setupMulticastTiming() {
+        MulticastKeepaliveHeartbeatSender.setHeartBeatInterval(1000);
+        MulticastKeepaliveHeartbeatSender.setHeartBeatStaleTime(30000);
+    }
+
+    protected static final void setHeapDumpOnOutOfMemoryError(boolean value) {
+        try {
+            MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+            ObjectName beanName = ObjectName.getInstance("com.sun.management:type=HotSpotDiagnostic");
+            Object vmOption = server.invoke(beanName, "setVMOption", new Object[] { "HeapDumpOnOutOfMemoryError", Boolean.toString(value) },
+                                                                     new String[] { "java.lang.String", "java.lang.String" });
+            LOG.info("Set HeapDumpOnOutOfMemoryError to: " + value);
+        } catch (Throwable t) {
+            LOG.info("Set HeapDumpOnOutOfMemoryError to: " + value + " - failed", t);
+        }
+    }
+
+    protected static Collection<Throwable> runTasks(Collection<Callable<Void>> tasks) {
+        final long endTime = System.nanoTime() + TimeUnit.SECONDS.toNanos(10);
+        final Collection<Throwable> errors = new ArrayList<Throwable>();
+
+        // Spin up the threads
+        Collection<Thread> threads = new ArrayList<Thread>(tasks.size());
+        for (final Callable<Void> task : tasks) {
+            Assert.assertNotNull(task);
+            threads.add(new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        // Run the thread until the given end time
+                        while (System.nanoTime() < endTime) {
+                            task.call();
+                        }
+                    } catch (Throwable t) {
+                        // Hang on to any errors
+                        errors.add(t);
+                    }
+                }
+
+            });
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        boolean interrupted = false;
+        try {
+            for (Thread t : threads) {
+                while (t.isAlive()) {
+                    try {
+                        t.join();
+                    } catch (InterruptedException e) {
+                        interrupted = true;
+                    }
+                }
+            }
+        } finally {
+            if (interrupted) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        return errors;
+    }
+
+    protected static void waitForClusterMembership(int time, TimeUnit unit, final Collection<String> cacheNames, final CacheManager ... managers) {
+        assertBy(time, unit, new Callable<Integer>() {
+
+            public Integer call() throws Exception {
+                Integer minimumPeers = null;
+                for (CacheManager manager : managers) {
+                    CacheManagerPeerProvider peerProvider = manager.getCacheManagerPeerProvider("RMI");
+                    for (String cacheName : cacheNames) {
+                        int peers = peerProvider.listRemoteCachePeers(manager.getEhcache(cacheName)).size();
+                        if (minimumPeers == null || peers < minimumPeers) {
+                            minimumPeers = peers;
+                        }
+                    }
+                }
+                if (minimumPeers == null) {
+                    return 0;
+                } else {
+                    return minimumPeers + 1;
+                }
+            }
+        }, is(managers.length));
+    }
+}
---
+++ tags/ehcache-core-2.5.7/src/test/java/net/sf/ehcache/hibernate/HibernateCacheTest.java
@@ -0,0 +1,288 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sf.ehcache.hibernate;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.hibernate.domain.Event;
+import net.sf.ehcache.hibernate.domain.EventManager;
+
+import net.sf.ehcache.hibernate.domain.Item;
+import net.sf.ehcache.hibernate.domain.Person;
+import net.sf.ehcache.hibernate.domain.PhoneNumber;
+import net.sf.ehcache.hibernate.domain.VersionedItem;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cache.access.SoftLock;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.stat.QueryStatistics;
+
+import org.hibernate.stat.SecondLevelCacheStatistics;
+import org.hibernate.stat.Statistics;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.collection.IsMapContainingKey.hasKey;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Chris Dennis
+ */
+public class HibernateCacheTest {
+
+    private static SessionFactory sessionFactory;
+    private static Configuration config;
+
+    public synchronized static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                sessionFactory = config.buildSessionFactory();
+            } catch (HibernateException ex) {
+                System.err.println("Initial SessionFactory creation failed." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+        }
+        return sessionFactory;
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("derby.system.home", "target/derby");
+        config = new Configuration().configure("/hibernate-config/hibernate.cfg.xml");
+        config.setProperty("hibernate.hbm2ddl.auto", "create");
+        getSessionFactory().getStatistics().setStatisticsEnabled(true);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        getSessionFactory().close();
+    }
+
+    @Test
+    public void testQueryCacheInvalidation() throws Exception {
+        Session s = getSessionFactory().openSession();
+        Transaction t = s.beginTransaction();
+        Item i = new Item();
+        i.setName("widget");
+        i.setDescription("A really top-quality, full-featured widget.");
+        s.persist(i);
+        t.commit();
+        s.close();
+
+        SecondLevelCacheStatistics slcs = s.getSessionFactory().getStatistics().getSecondLevelCacheStatistics(Item.class.getName());
+
+        assertEquals(slcs.getPutCount(), 1);
+        assertEquals(slcs.getElementCountInMemory(), 1);
+        assertEquals(slcs.getEntries().size(), 1);
+
+        s = getSessionFactory().openSession();
+        t = s.beginTransaction();
+        i = (Item) s.get(Item.class, i.getId());
+
+        assertEquals(slcs.getHitCount(), 1);
+        assertEquals(slcs.getMissCount(), 0);
+
+        i.setDescription("A bog standard item");
+
+        t.commit();
+        s.close();
+
+        assertEquals(slcs.getPutCount(), 2);
+
+        final Map<Long, ?> entries = slcs.getEntries();
+        assertThat(entries, hasKey(i.getId()));
+        Object entry = entries.get(i.getId());
+        Map map;
+        if (entry instanceof Map) {
+            map = (Map) entry;
+        } else {
+            Method valueMethod = entry.getClass().getDeclaredMethod("getValue", (Class[]) null);
+            valueMethod.setAccessible(true);
+            map = (Map) valueMethod.invoke(entry, (Object[]) null);
+        }
+        assertTrue(map.get("description").equals("A bog standard item"));
+        assertTrue(map.get("name").equals("widget"));
+
+        // cleanup
+        s = getSessionFactory().openSession();
+        t = s.beginTransaction();
+        s.delete(i);
+        t.commit();
+        s.close();
+    }
+
+    @Test
+    public void testEmptySecondLevelCacheEntry() throws Exception {
+        getSessionFactory().evictEntity(Item.class.getName());
+        Statistics stats = getSessionFactory().getStatistics();
+        stats.clear();
+        SecondLevelCacheStatistics statistics = stats.getSecondLevelCacheStatistics(Item.class.getName());
+        Map cacheEntries = statistics.getEntries();
+        assertEquals(0, cacheEntries.size());
+    }
+
+    @Test
+    public void testStaleWritesLeaveCacheConsistent() {
+        Session s = getSessionFactory().openSession();
+        Transaction txn = s.beginTransaction();
+        VersionedItem item = new VersionedItem();
+        item.setName("steve");
+        item.setDescription("steve's item");
+        s.save(item);
+        txn.commit();
+        s.close();
+
+        Long initialVersion = item.getVersion();
+
+        // manually revert the version property
+        item.setVersion(new Long(item.getVersion().longValue() - 1));
+
+        try {
+            s = getSessionFactory().openSession();
+            txn = s.beginTransaction();
+            s.update(item);
+            txn.commit();
+            s.close();
+            fail("expected stale write to fail");
+        } catch (Throwable expected) {
+            // expected behavior here
+            if (txn != null) {
+                try {
+                    txn.rollback();
+                } catch (Throwable ignore) {
+                }
+            }
+        } finally {
+            if (s != null && s.isOpen()) {
+                try {
+                    s.close();
+                } catch (Throwable ignore) {
+                }
+            }
+        }
+
+        // check the version value in the cache...
+        SecondLevelCacheStatistics slcs = getSessionFactory().getStatistics().getSecondLevelCacheStatistics(VersionedItem.class.getName());
+
+        Object entry = slcs.getEntries().get(item.getId());
+        Long cachedVersionValue;
+        if (entry instanceof SoftLock) {
+            //FIXME don't know what to test here
+            //cachedVersionValue = new Long( ( (ReadWriteCache.Lock) entry).getUnlockTimestamp() );
+        } else {
+            cachedVersionValue = (Long) ((Map) entry).get("_version");
+            assertEquals(initialVersion.longValue(), cachedVersionValue.longValue());
+        }
+
+
+        // cleanup
+        s = getSessionFactory().openSession();
+        txn = s.beginTransaction();
+        item = (VersionedItem) s.load(VersionedItem.class, item.getId());
+        s.delete(item);
+        txn.commit();
+        s.close();
+
+    }
+    
+    @Test
+    public void testUnpinsOnRemoval() {
+        getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Cache entityCache = CacheManager.getCacheManager("tc").getCache(Item.class.getName());
+        long maxEntriesLocalHeap = 100;
+        entityCache.getCacheConfiguration().setMaxEntriesLocalHeap(maxEntriesLocalHeap);
+
+        session.getTransaction().begin();
+        for (int j = 0; j < 500; j++) {
+            Item entity = new Item();
+            entity.setName(UUID.randomUUID().toString());
+            entity.setDescription(UUID.randomUUID().toString());
+            session.save(entity);
+            session.delete(entity); // This leaves the SoftLock in the cache...
+        }
+        session.getTransaction().commit(); // ... but they all should be writeable & unpinned after this!
+
+        assertThat(entityCache.getCacheConfiguration().getMaxEntriesLocalHeap(), equalTo(maxEntriesLocalHeap));
+        for (Object o : entityCache.getKeys()) {
+            final Element e = entityCache.get(o);
+            assertFalse(e + " shouldn't be pinned anymore!", entityCache.isPinned(o));
+        }
+        assertThat(entityCache.getSize(), equalTo((int)maxEntriesLocalHeap));
+    }
+
+    @Test
+    public void testGeneralUsage() {
+        EventManager mgr = new EventManager(getSessionFactory());
+        Statistics stats = getSessionFactory().getStatistics();
+
+        // create 3 persons Steve, Orion, Tim
+        Person stevePerson = new Person();
+        stevePerson.setFirstname("Steve");
+        stevePerson.setLastname("Harris");
+        Long steveId = mgr.createAndStorePerson(stevePerson);
+        mgr.addEmailToPerson(steveId, "steve@tc.com");
+        mgr.addEmailToPerson(steveId, "sharrif@tc.com");
+        mgr.addTalismanToPerson(steveId, "rabbit foot");
+        mgr.addTalismanToPerson(steveId, "john de conqueroo");
+
+        PhoneNumber p1 = new PhoneNumber();
+        p1.setNumberType("Office");
+        p1.setPhone(111111);
+        mgr.addPhoneNumberToPerson(steveId, p1);
+
+        PhoneNumber p2 = new PhoneNumber();
+        p2.setNumberType("Home");
+        p2.setPhone(222222);
+        mgr.addPhoneNumberToPerson(steveId, p2);
+
+        Person orionPerson = new Person();
+        orionPerson.setFirstname("Orion");
+        orionPerson.setLastname("Letizi");
+        Long orionId = mgr.createAndStorePerson(orionPerson);
+        mgr.addEmailToPerson(orionId, "orion@tc.com");
+        mgr.addTalismanToPerson(orionId, "voodoo doll");
+
+        Long timId = mgr.createAndStorePerson("Tim", "Teck");
+        mgr.addEmailToPerson(timId, "teck@tc.com");
+        mgr.addTalismanToPerson(timId, "magic decoder ring");
+
+        Long engMeetingId = mgr.createAndStoreEvent("Eng Meeting", stevePerson, new Date());
+        mgr.addPersonToEvent(steveId, engMeetingId);
+        mgr.addPersonToEvent(orionId, engMeetingId);
+        mgr.addPersonToEvent(timId, engMeetingId);
+
+        Long docMeetingId = mgr.createAndStoreEvent("Doc Meeting", orionPerson, new Date());
+        mgr.addPersonToEvent(steveId, docMeetingId);
+        mgr.addPersonToEvent(orionId, docMeetingId);
+
+        for (Event event : (List<Event>) mgr.listEvents()) {
+            mgr.listEmailsOfEvent(event.getId());
+        }
+
+        getSessionFactory().close();
+
+        QueryStatistics queryStats = stats.getQueryStatistics("from Event");
+        assertEquals("Cache Miss Count", 1L, queryStats.getCacheMissCount());
+        assertEquals("Cache Hit Count", 0L, queryStats.getCacheHitCount());
+        assertEquals("Cache Put Count", 1L, queryStats.getCachePutCount());
+    }
+}
