9fe013ae6f0d742334b39aaa8dbb2b77a839bbd4
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2013-01-23 14:37:15 +0000

    JRockit's compiler is broken and cannot handle this trailing comma
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6953 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/server/src/test/java/net/sf/ehcache/server/soap/jaxws/Statistics.java
+++ trunk/server/src/test/java/net/sf/ehcache/server/soap/jaxws/Statistics.java
@@ -32,7 +32,7 @@ import javax.xml.bind.annotation.XmlType;
         "cacheHits",
         "evictionCount",
         "inMemoryHits",
-        "onDiskHits",
+        "onDiskHits"
 })
 public class Statistics {
 
