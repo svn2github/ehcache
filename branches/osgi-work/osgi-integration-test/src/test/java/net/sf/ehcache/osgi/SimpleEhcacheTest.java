9b7a58d9a33f8616d06cf468a094ac478a5b4b4f
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2013-01-25 08:05:55 +0000

    framework to do OSGi integration test
    git-svn-id: http://svn.terracotta.org/svn/ehcache@6970 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/osgi-work/osgi-integration-test/src/test/java/net/sf/ehcache/osgi/SimpleEhcacheTest.java
@@ -0,0 +1,39 @@
+package net.sf.ehcache.osgi;
+
+import static net.sf.ehcache.util.TestUtils.testMavenBundle;
+import static org.junit.Assert.assertEquals;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class SimpleEhcacheTest {
+
+	@Configuration
+	public Option[] config() {
+		return options(testMavenBundle("net.sf.ehcache", "ehcache"), junitBundles());
+	}
+
+	@Test
+	public void testSimpleCache() throws Exception {
+		CacheManager manager = new CacheManager(
+		    SimpleEhcacheTest.class.getResource("/simple-ehcache.xml"));
+		Cache cache = manager.getCache("sampleCache1");
+		Element element = new Element("key1", "value1");
+		cache.put(element);
+		Element element1 = cache.get("key1");
+		assertEquals("value1", element1.getObjectValue());
+		assertEquals(1, cache.getSize());
+	}
+}
---
+++ branches/osgi-work/osgi-integration-test/src/test/java/net/sf/ehcache/util/TestUtils.java
@@ -0,0 +1,54 @@
+package net.sf.ehcache.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.ops4j.pax.exam.options.MavenArtifactProvisionOption;
+
+public class TestUtils {
+	private static final Map<String, String> dependencies = getDependencies();
+
+	private static Map<String, String> getDependencies() {
+		Pattern mavenCoords = Pattern.compile("^\\s*([^:]+):([^:]+):([^:]+):([^:]+):(\\w+)$");
+		Map<String, String> deps = new HashMap<String, String>();
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader("target/dependencies.list"));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				Matcher m = mavenCoords.matcher(line);
+				if (m.matches()) {
+					String groupIdPlusArtifactId = m.group(1) + ":" + m.group(2);
+					String version = m.group(4);
+					deps.put(groupIdPlusArtifactId, version);
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// ignored
+				}
+			}
+		}
+		return deps;
+	}
+
+	public static final String versionOf(String groupIdPlusArtifactId) {
+		return dependencies.get(groupIdPlusArtifactId);
+	}
+
+	public static MavenArtifactProvisionOption testMavenBundle(String groupId, String artifactId) {
+		String version = versionOf(groupId + ":" + artifactId);
+		return new MavenArtifactProvisionOption().groupId(groupId).artifactId(artifactId)
+		    .version(version);
+	}
+}
