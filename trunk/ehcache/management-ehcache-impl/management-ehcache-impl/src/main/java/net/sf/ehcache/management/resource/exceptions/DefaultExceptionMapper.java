4e8adfd394b50c656caf0a8c51a1fc8a6a280782
lorban <lorban@b9324663-ca0f-0410-8574-be9b3887307d>
2013-01-30 15:13:01 +0000

    default handler now handles Throwable
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7018 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/management-ehcache-impl/management-ehcache-impl/src/main/java/net/sf/ehcache/management/resource/exceptions/DefaultExceptionMapper.java
+++ trunk/ehcache/management-ehcache-impl/management-ehcache-impl/src/main/java/net/sf/ehcache/management/resource/exceptions/DefaultExceptionMapper.java
@@ -1,3 +1,7 @@
+/*
+ * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
 package net.sf.ehcache.management.resource.exceptions;
 
 import org.terracotta.management.resource.exceptions.ExceptionUtils;
@@ -11,10 +15,10 @@ import javax.ws.rs.ext.Provider;
  * @author Ludovic Orban
  */
 @Provider
-public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
+public class DefaultExceptionMapper implements ExceptionMapper<Throwable> {
 
   @Override
-  public Response toResponse(Exception exception) {
+  public Response toResponse(Throwable exception) {
     return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
         .type(MediaType.APPLICATION_JSON_TYPE)
         .entity(ExceptionUtils.toJsonError(exception)).build();
--- trunk/ehcache/management-ehcache-impl/management-ehcache-impl/src/main/java/net/sf/ehcache/management/resource/exceptions/ResourceRuntimeExceptionMapper.java
+++ trunk/ehcache/management-ehcache-impl/management-ehcache-impl/src/main/java/net/sf/ehcache/management/resource/exceptions/ResourceRuntimeExceptionMapper.java
@@ -1,3 +1,7 @@
+/*
+ * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
 package net.sf.ehcache.management.resource.exceptions;
 
 import org.terracotta.management.resource.exceptions.ExceptionUtils;
