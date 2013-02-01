cc79c8cf8e701da7e6dfbdf9995c399169edcb48
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2013-02-01 18:32:36 +0000

    simple quartz osgi test
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7044 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/osgi-work/osgi-integration-test/src/test/java/net/sf/ehcache/osgi/SimpleQuartzTest.java
@@ -0,0 +1,71 @@
+package net.sf.ehcache.osgi;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
+
+import java.util.Date;
+
+import net.sf.ehcache.osgi.util.TestUtil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.quartz.DateBuilder;
+import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+/**
+ * Test a simple BigMemory usage with BM Go license key. The product name should
+ * include "BigMemory" and not "Ehcache"
+ * 
+ * 
+ * @author hhuynh
+ * 
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+public class SimpleQuartzTest {
+
+  @Configuration
+  public Option[] config() {
+    return options(mavenBundle("org.quartz-scheduler", "quartz").versionAsInProject(),
+        wrappedBundle(maven("c3p0", "c3p0").versionAsInProject()), TestUtil.commonOptions());
+  }
+
+  @Test
+  public void testQuartz() throws Exception {
+    SchedulerFactory sf = new StdSchedulerFactory();
+    Scheduler sched = sf.getScheduler();
+    Date runTime = DateBuilder.evenMinuteDate(new Date());
+    JobDetail job = JobBuilder.newJob(HelloJob.class)
+        .withIdentity("job1", "group1").build();
+    org.quartz.Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1")
+        .startAt(runTime).build();
+    sched.scheduleJob(job, trigger);
+    sched.start();
+    try {
+      Thread.sleep(5000L);
+    } catch (Exception e) {
+    }
+    sched.shutdown(true);
+  }
+
+  private static class HelloJob implements Job {
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+      System.out.println("Hello world!");
+    }
+  }
+}
