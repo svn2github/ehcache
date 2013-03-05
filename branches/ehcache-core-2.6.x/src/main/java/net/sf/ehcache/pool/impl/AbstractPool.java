7eab74226b2b6c88beb503ca6ca24df42ebbab6c
cdennis <cdennis@b9324663-ca0f-0410-8574-be9b3887307d>
2013-03-05 18:56:24 +0000

    EHC-1001 : merge 7229 from trunk
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7233 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-core-2.6.x/src/main/java/net/sf/ehcache/pool/impl/AbstractPool.java
+++ branches/ehcache-core-2.6.x/src/main/java/net/sf/ehcache/pool/impl/AbstractPool.java
@@ -18,7 +18,6 @@ package net.sf.ehcache.pool.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
