4f8562cedd81c648fef86ad699730331e6c32a54
hhuynh <hhuynh@b9324663-ca0f-0410-8574-be9b3887307d>
2013-03-21 01:04:53 +0000

    add REST osgi test, upgraded pax exam to 3.0.1
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7314 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/osgi-work/osgi-integration-test/src/test/java/net/sf/ehcache/osgi/SimpleEhcacheTest.java
+++ branches/osgi-work/osgi-integration-test/src/test/java/net/sf/ehcache/osgi/SimpleEhcacheTest.java
@@ -1,10 +1,18 @@
 package net.sf.ehcache.osgi;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.ops4j.pax.exam.CoreOptions.bootDelegationPackages;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
@@ -22,12 +30,12 @@ import org.ops4j.pax.exam.util.PathUtils;
 import org.terracotta.context.ContextManager;
 
 /**
- * Test a simple BigMemory usage with BM Go license key.
- * The product name should include "BigMemory" and not "Ehcache"
+ * Test a simple BigMemory usage with BM Go license key. The product name should
+ * include "BigMemory" and not "Ehcache"
  * 
  * 
  * @author hhuynh
- *
+ * 
  */
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
@@ -36,6 +44,7 @@ public class SimpleEhcacheTest {
   @Configuration
   public Option[] config() {
     return options(
+        bootDelegationPackages("javax.xml.transform,org.w3c.dom"), // need this for REST agent test
         mavenBundle("org.terracotta.bigmemory", "bigmemory").versionAsInProject(),
         mavenBundle("net.sf.ehcache", "ehcache-ee").versionAsInProject(),
         TestUtil.commonOptions(),
@@ -49,12 +58,18 @@ public class SimpleEhcacheTest {
     assertEquals("BigMemory", pi.getName());
     CacheManager manager = new CacheManager(
         SimpleEhcacheTest.class.getResource("/simple-ehcache.xml"));
-    Cache cache = manager.getCache("sampleCache1");
-    Element element = new Element("key1", "value1");
-    cache.put(element);
-    Element element1 = cache.get("key1");
-    assertEquals("value1", element1.getObjectValue());
-    assertEquals(1, cache.getSize());
+    try {
+      Cache cache = manager.getCache("sampleCache1");
+      Element element = new Element("key1", "value1");
+      cache.put(element);
+      Element element1 = cache.get("key1");
+      assertEquals("value1", element1.getObjectValue());
+      assertEquals(1, cache.getSize());
+    } finally {
+      if (manager != null) {
+        manager.shutdown();
+      }
+    }
   }
 
   @Test
@@ -64,6 +79,35 @@ public class SimpleEhcacheTest {
       fail("Expected class not found exception");
     } catch (Throwable e) {
       // expected
+    }
+  }
+
+  @Test
+  public void testRestAgent() throws Exception {
+    CacheManager manager = new CacheManager(
+        SimpleEhcacheTest.class.getResource("/rest-enabled-ehcache.xml"));
+    InputStream in = null;
+    try {
+      Cache testCache = manager.getCache("testCache");
+      testCache.put(new Element("k", "v"));
+      assertEquals(1, testCache.getSize());
+      URL url = new URL("http://localhost:9888/tc-management-api/agents");
+      in = url.openStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+      String line = null;
+      StringBuilder sb = new StringBuilder();
+      while ((line = reader.readLine()) != null) {
+        sb.append(line.trim()).append("\n");
+      }
+      System.out.println("Rest response: " + sb);
+      assertTrue(sb.toString().contains("\"agentId\":\"embedded\""));
+    } finally {
+      if (manager != null) {
+        manager.shutdown();
+      }
+      if (in != null) {
+        in.close();
+      }
     }
   }
 }
