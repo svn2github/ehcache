bac2873bfe4b590b81ed51a1e793d7dda2d49ba3
mscott <mscott@b9324663-ca0f-0410-8574-be9b3887307d>
2013-04-19 02:48:28 +0000

    Inline expiration not relevant with server expiration
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7389 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/system-tests/src/test/java/org/terracotta/ehcache/tests/servermap/ServerMapTTIExpressTestClient.java
+++ trunk/ehcache/system-tests/src/test/java/org/terracotta/ehcache/tests/servermap/ServerMapTTIExpressTestClient.java
@@ -35,30 +35,17 @@ public class ServerMapTTIExpressTestClient extends ServerMapClientBase {
     assertRange(5000, 7000, cache);
 
     System.out.println("Sleeping for 3 mins (now=" + new Date() + ") ... ");
-    Thread.sleep(3 * 60 * 1000);
+    Thread.sleep(1 * 60 * 1000);
     // Sleep for TTI to kick in:
     // Wait up to 30 sec. for the capacity evictor to do its thing.
     int count = 0;
-    while ( cache.getSize() > 6000 && count++ < 60) {
+    while ( cache.getSize() > 0 && count++ < 60) {
         Thread.sleep(5000);
         System.out.println("Cache populated. size: " + cache.getSize());
     }
     
-    System.out.println("After sleeping 3 mins. Size: " + cache.getSize());
-    // Now size should be <= capacity
-    assertRange(3000, 6000, cache);
-
-    System.out.println("Trying to get on all elements, inline eviction should happen");
-    // all others should be evicted inline
-    for (int i = 0; i < 7000; i++) {
-      Element element = cache.get("key-" + i);
-      Assert.assertNull("Element should be null of key-" + i, element);
-    }
-
-    Thread.sleep(5000);
-
-    System.out.println("After inline eviction. Size: " + cache.getSize());
-    // Now size should be equal to 0
+    System.out.println("After sleeping for up to 5 mins. Size: " + cache.getSize());
+    
     assertEquals(0, cache.getSize());
   }
 
