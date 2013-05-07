539acaced2250faf6275286885818d6fad8a718e
twu <twu@b9324663-ca0f-0410-8574-be9b3887307d>
2013-05-07 02:17:05 +0000

    No need to log, it's already being thrown
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7500 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/store/nonstop/NonStopStoreWrapper.java
+++ trunk/ehcache/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/store/nonstop/NonStopStoreWrapper.java
@@ -151,7 +151,6 @@ public class NonStopStoreWrapper implements TerracottaStore {
       }
     } catch (Throwable t) {
       String message = "Error while creating store inline ";
-      LOGGER.warn(message, t);
       handleException(message, t);
     }
   }
