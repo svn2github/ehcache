c4942066e16e214b2d22f09bf744a41f7dc98651
mchoudha <mchoudha@b9324663-ca0f-0410-8574-be9b3887307d>
2013-05-13 07:21:13 +0000

    EHC-950
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7521 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-2.7.x/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/store/nonstop/NonStopStoreWrapper.java
+++ branches/ehcache-2.7.x/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/store/nonstop/NonStopStoreWrapper.java
@@ -16,6 +16,7 @@ import net.sf.ehcache.config.InvalidConfigurationException;
 import net.sf.ehcache.config.NonstopConfiguration;
 import net.sf.ehcache.config.TimeoutBehaviorConfiguration;
 import net.sf.ehcache.constructs.nonstop.NonStopCacheException;
+import net.sf.ehcache.event.CacheEventListener;
 import net.sf.ehcache.search.Attribute;
 import net.sf.ehcache.search.Results;
 import net.sf.ehcache.search.SearchException;
@@ -114,6 +115,8 @@ public class NonStopStoreWrapper implements TerracottaStore {
                                                                                                                          .of(this)
                                                                                                                          .tag("cache")
                                                                                                                          .build();
+
+  private CacheEventListener                                                       cacheEventListener;
 
   public NonStopStoreWrapper(Callable<TerracottaStore> clusteredStoreCreator,
                              ToolkitInstanceFactory toolkitInstanceFactory, Ehcache cache,
@@ -265,7 +268,7 @@ public class NonStopStoreWrapper implements TerracottaStore {
 
     // create this to be sure that it's present on each node to receive clustered events,
     // even if this node is not sending out its events
-    cache.getCacheManager().createTerracottaEventReplicator(cache);
+    cacheEventListener = cache.getCacheManager().createTerracottaEventReplicator(cache);
 
     synchronized (this) {
       if (delegate == null) {
@@ -306,6 +309,7 @@ public class NonStopStoreWrapper implements TerracottaStore {
   @Override
   public void dispose() {
     // THIS IS HAND MADE CODE -- DO NOT GENERATED
+    cacheEventListener.dispose();
     if (delegate != null) {
       this.delegate.dispose();
     }
