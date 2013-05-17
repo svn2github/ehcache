b0ec105aa74f9c1fbd49404c9403dab2e2ba9665
lorban <lorban@b9324663-ca0f-0410-8574-be9b3887307d>
2013-05-17 13:03:21 +0000

    merged DEV-9651 and MNK-4978
    git-svn-id: http://svn.terracotta.org/svn/ehcache@7559 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-2.7.x/management-ehcache-impl/management-ehcache-impl/src/main/java/net/sf/ehcache/management/resource/exceptions/DefaultExceptionMapper.java
+++ branches/ehcache-2.7.x/management-ehcache-impl/management-ehcache-impl/src/main/java/net/sf/ehcache/management/resource/exceptions/DefaultExceptionMapper.java
@@ -4,6 +4,8 @@
  */
 package net.sf.ehcache.management.resource.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terracotta.management.resource.exceptions.ExceptionUtils;
 
 import javax.ws.rs.core.MediaType;
@@ -17,8 +19,11 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class DefaultExceptionMapper implements ExceptionMapper<Throwable> {
 
+  private static final Logger LOG = LoggerFactory.getLogger(DefaultExceptionMapper.class);
+
   @Override
   public Response toResponse(Throwable exception) {
+    LOG.debug("DefaultExceptionMapper caught exception", exception);
     return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
         .type(MediaType.APPLICATION_JSON_TYPE)
         .entity(ExceptionUtils.toJsonError(exception)).build();
