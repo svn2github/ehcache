4b16c47ade52bc26704c1b635173f9bd858ea835
ljacomet <ljacomet@b9324663-ca0f-0410-8574-be9b3887307d>
2013-08-21 12:31:21 +0000

    ENG-225 Rework test to reduce running time
    git-svn-id: http://svn.terracotta.org/svn/ehcache@8010 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/event/BulkOpsEventListenerTest.java
+++ trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/event/BulkOpsEventListenerTest.java
@@ -16,18 +16,6 @@
 
 package net.sf.ehcache.event;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import net.sf.ehcache.AbstractCacheTest;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheException;
@@ -37,10 +25,35 @@ import net.sf.ehcache.config.CacheConfiguration;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terracotta.test.categories.CheckShorts;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 @Category(CheckShorts.class)
 public class BulkOpsEventListenerTest extends AbstractCacheTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BulkOpsEventListenerTest.class);
+    private static final int BATCH_SIZE = 1000;
+    private static final int MAX_RETRY = 100;
 
     @Test
     public void testBulkOpsEventListener() throws Throwable {
@@ -69,7 +82,7 @@ public class BulkOpsEventListenerTest extends AbstractCacheTest {
         assertEquals(numOfElements, eventListener.elementsPut.size());
         assertEquals(elements, eventListener.elementsPut);
 
-        Set keySet1 = new HashSet<String>();
+        Set<String> keySet1 = new HashSet<String>();
         for(int i = 0; i < numOfElements; i++){
             keySet1.add("key"+i);
         }
@@ -87,7 +100,7 @@ public class BulkOpsEventListenerTest extends AbstractCacheTest {
         }
 
         Random rand = new Random();
-        Set keySet2 = new HashSet<String>();
+        Set<String> keySet2 = new HashSet<String>();
         for(int i = 0; i < numOfElements/2; i++){
             keySet2.add("key" + rand.nextInt(numOfElements));
         }
@@ -106,7 +119,7 @@ public class BulkOpsEventListenerTest extends AbstractCacheTest {
         assertEquals(keySet2.size(), eventListener.elementsRemoved.size());
         Set<String> removedKeySet = new HashSet<String>();
         for(Element element : eventListener.elementsRemoved){
-            removedKeySet.add(element.getKey().toString());
+            removedKeySet.add(element.getObjectKey().toString());
         }
         assertEquals(keySet2, removedKeySet);
 
@@ -119,107 +132,177 @@ public class BulkOpsEventListenerTest extends AbstractCacheTest {
     }
 
     @Test
-    public void testMultiThreadedBulkOps() throws InterruptedException{
-        Cache cache = new Cache("cache", 1000000, true, false, 100000, 200000, false, 1);
+    public void testMultiThreadedBulkOpsPut() throws InterruptedException{
+        final Cache cache = new Cache("putCache", 1000000, true, false, 100000, 200000, false, 1);
         manager.addCache(cache);
 
-        TestCacheEventListener eventListener = new TestCacheEventListener();
+        final TestCacheEventListener eventListener = new TestCacheEventListener();
         cache.getCacheEventNotificationService().registerListener(eventListener);
 
-        Producer p1 = new Producer(cache, 0, 3 * 60 * 1000);
-        Producer p2 = new Producer(cache, 1000000, 3 * 60 * 1000);
-        Thread[] th = new Thread[4];
-        th[0] = new Thread(p1, "p1");
-        th[1] = new Thread(p2, "p2");
-        th[0].start();
-        th[1].start();
+        final AtomicBoolean stopCondition = new AtomicBoolean(false);
+        final String p1Value = "p1Value";
+        final String p2Value = "p2Value";
 
-        Consumer c1 = new Consumer(cache, 0, 2 * 60 * 1000);
-        Consumer c2 = new Consumer(cache, 1000000, 2 * 60 * 1000);
-        th[2] = new Thread(c1, "c1");
-        th[3] = new Thread(c2, "c2");
+        CyclicBarrier barrier = new CyclicBarrier(2, new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("Checking put did interleave");
+                boolean p1Seen = false;
+                boolean p2Seen = false;
+                for (Object key : cache.getKeys()) {
+                    if (cache.get(key).getObjectValue().equals(p1Value)) {
+                        p1Seen = true;
+                    }
+                    if (cache.get(key).getObjectValue().equals(p2Value)) {
+                        p2Seen = true;
+                    }
+                    if (p1Seen && p2Seen) {
+                        stopCondition.set(true);
+                        return;
+                    }
+                }
+                cache.removeAll();
+                eventListener.reset();
+            }
+        });
 
-        Thread.sleep(10000);
-        th[2].start();
-        th[3].start();
+        CountDownLatch endLatch = new CountDownLatch(2);
+        Producer p1 = new Producer(cache, p1Value, stopCondition, barrier, endLatch);
+        new Thread(p1, "p1").start();
+        Producer p2 = new Producer(cache, p2Value, stopCondition, barrier, endLatch);
+        new Thread(p2, "p2").start();
 
-        for(Thread t : th){
-            t.join();
-        }
+        LOGGER.info("Waiting for multiple putters to end");
+        endLatch.await(1, TimeUnit.MINUTES);
 
-        assertEquals(p1.numPuts.intValue() + p2.numPuts.intValue(), eventListener.elementsPut.size());
-        assertEquals(c1.numRemoved.intValue() + c2.numRemoved.intValue(), eventListener.elementsRemoved.size());
+        assertThat(stopCondition.get(), is(true));
+        assertThat(eventListener.putCount.get(), is(p1.batchPut + p2.batchPut));
+    }
+
+    @Test
+    public void testMultiThreadedBulkOpsPutAndRemove() throws InterruptedException{
+        final Cache cache = new Cache("putRemoveCache", 1000000, true, false, 100000, 200000, false, 1);
+        manager.addCache(cache);
+
+        final TestCacheEventListener eventListener = new TestCacheEventListener();
+        cache.getCacheEventNotificationService().registerListener(eventListener);
+
+        final AtomicBoolean stopCondition = new AtomicBoolean(false);
+
+      final CountDownLatch endLatch = new CountDownLatch(2);
+        final CyclicBarrier barrier = new CyclicBarrier(2, new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("Checking put/remove did interleave");
+                if (cache.getSize() != 0 && cache.getSize() != BATCH_SIZE) {
+                    stopCondition.set(true);
+                } else {
+                    LOGGER.info("Cache size:" + cache.getSize());
+                    cache.removeAll();
+                    eventListener.reset();
+                }
+            }
+        });
+        Producer p3 = new Producer(cache, "p1Value", stopCondition, barrier, endLatch);
+        new Thread(p3, "p3").start();
+        Consumer consumer = new Consumer(cache, stopCondition, barrier, endLatch);
+        new Thread(consumer, "c1").start();
+
+        LOGGER.info("Waiting for put and remove to end");
+        endLatch.await(1, TimeUnit.MINUTES);
+
+        assertThat(stopCondition.get(), is(true));
+        assertThat(eventListener.putCount.get(), is(p3.batchPut));
+        assertThat(eventListener.elementsRemoved.size(), is(consumer.batchRemoved));
+
     }
 
     private static class Producer implements Runnable {
-        private final AtomicInteger numPuts = new AtomicInteger(0);
+        private final int batchPut = BATCH_SIZE;
         private final Cache cache;
-        private final int startIndex;
-        private final int timeToRunMills;
-        private final long startTime = System.currentTimeMillis();
+        private final CyclicBarrier barrier;
+        private final String value;
+        private final AtomicBoolean stopCondition;
+        private final CountDownLatch endLatch;
 
-        public Producer(Cache cache, int start, int timeToRunMills) {
+        public Producer(Cache cache, String value, AtomicBoolean stopCondition, CyclicBarrier barrier, CountDownLatch endLatch) {
             this.cache = cache;
-            this.startIndex = start;
-            this.timeToRunMills = timeToRunMills;
+            this.value = value;
+            this.stopCondition = stopCondition;
+            this.barrier = barrier;
+            this.endLatch = endLatch;
         }
 
         public void run() {
-            int i = startIndex;
-            Random rand = new Random();
-            while(System.currentTimeMillis() - startTime <= timeToRunMills){
-                int batch = rand.nextInt(100);
-                Set<Element> elements = new HashSet<Element>();
-                for(int j = 0; j < batch; j++){
-                    elements.add(new Element("key" + i, "value" + i));
-                    i++;
+            int retry = 0;
+            try {
+                while (!stopCondition.get() && ++retry < MAX_RETRY) {
+                    Set<Element> elements = new HashSet<Element>();
+                    for (int j = 0; j < batchPut; j++) {
+                        elements.add(new Element("key" + j, value));
+                    }
+                    this.cache.putAll(elements);
+                    LOGGER.info("Producer done with run " + retry);
+                    try {
+                        barrier.await(1, TimeUnit.MINUTES);
+                    } catch (Exception e) {
+                        LOGGER.error("Failed while waiting on barrier", e);
+                        fail("Failed while waiting on barrier");
+                        return;
+                    }
                 }
-                this.cache.putAll(elements);
-                numPuts.addAndGet(batch);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            } catch (Exception e) {
+                LOGGER.error("Producer failed with exception", e);
+                fail("Produce failed with exception");
+            } finally {
+                endLatch.countDown();
             }
         }
     }
 
     private static class Consumer implements Runnable{
-        private final AtomicInteger numRemoved = new AtomicInteger(0);
+        private final int batchRemoved = BATCH_SIZE;
         private final Cache cache;
-        private final int startIndex;
-        private final int timeToRunMills;
-        private final long startTime = System.currentTimeMillis();
+        private final AtomicBoolean stopCondition;
+        private final CyclicBarrier barrier;
+        private final CountDownLatch endLatch;
 
-        public Consumer(Cache cache, int start, int timeToRunMills) {
+        public Consumer(Cache cache, AtomicBoolean stopCondition, CyclicBarrier barrier, CountDownLatch endLatch) {
             this.cache = cache;
-            this.startIndex = start;
-            this.timeToRunMills = timeToRunMills;
+            this.stopCondition = stopCondition;
+            this.barrier = barrier;
+            this.endLatch = endLatch;
         }
 
         public void run() {
-            int i = startIndex;
-            Random rand = new Random();
-            while(System.currentTimeMillis() - startTime <= timeToRunMills){
-                int batch = rand.nextInt(100);
-                Set elements = new HashSet<String>();
-                for(int j = 0; j < batch; j++){
-                    elements.add("key" + i);
-                    i++;
+            int retry = 0;
+            try {
+                while (!stopCondition.get() && ++retry < MAX_RETRY) {
+                    Set<String> elements = new HashSet<String>();
+                    for (int j = batchRemoved - 1; j >= 0; j--) {
+                        elements.add("key" + j);
+                    }
+                    this.cache.removeAll(elements);
+                    LOGGER.info("Consumer done with run " + retry);
+                    try {
+                        barrier.await(1, TimeUnit.MINUTES);
+                    } catch (Exception e) {
+                        LOGGER.error("Failed while waiting on barrier", e);
+                        fail("Failed while waiting on barrier");
+                        return;
+                    }
                 }
-                this.cache.removeAll(elements);
-                numRemoved.addAndGet(batch);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            } catch (Exception e) {
+                LOGGER.error("Consumer failed with exception", e);
+                fail("Consumer failed with exception");
+            } finally {
+                endLatch.countDown();
             }
         }
     }
 
     private static class TestCacheEventListener implements CacheEventListener{
+        AtomicInteger putCount = new AtomicInteger(0);
         Set<Element> elementsPut = Collections.synchronizedSet(new HashSet<Element>());
         Set<Element> elementsUpdated = Collections.synchronizedSet(new HashSet<Element>());
         Set<Element> elementsRemoved = Collections.synchronizedSet(new HashSet<Element>());
@@ -240,6 +323,7 @@ public class BulkOpsEventListenerTest extends AbstractCacheTest {
         }
 
         public void notifyElementPut(Ehcache cache, Element element) throws CacheException {
+            putCount.incrementAndGet();
             elementsPut.add(element);
         }
 
@@ -260,5 +344,11 @@ public class BulkOpsEventListenerTest extends AbstractCacheTest {
             return super.clone();
         }
 
+        public void reset() {
+            elementsUpdated.clear();
+            elementsPut.clear();
+            putCount.set(0);
+
+        }
     }
 }
