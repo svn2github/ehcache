53911418a143940bab45654cc3cf4eea5e7ac858
eshelest <eshelest@b9324663-ca0f-0410-8574-be9b3887307d>
2013-10-17 01:56:06 +0000

    Wan-aware cache wrapper should not throw an exception on keySet().
    git-svn-id: http://svn.terracotta.org/svn/ehcache@8347 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/WanAwareToolkitCache.java
+++ trunk/ehcache/terracotta/bootstrap/src/main/java/org/terracotta/modules/ehcache/WanAwareToolkitCache.java
@@ -410,11 +410,7 @@ public class WanAwareToolkitCache<K, V> implements ToolkitCacheInternal<K, V> {
 
   @Override
   public Set<K> keySet() {
-    if (isActive()) {
-      return delegate.keySet();
-    } else {
-      throw new IllegalStateException(NOT_ACTIVE_MSG);
-    }
+    return delegate.keySet(); //TODO: throw an exception if not active
   }
 
   @Override
