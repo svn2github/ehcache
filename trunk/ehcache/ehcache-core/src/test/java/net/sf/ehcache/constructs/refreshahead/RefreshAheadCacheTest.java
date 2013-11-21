d2216209b67cd5a0630d5b80e819a0089ee9cf29
ljacomet <ljacomet@b9324663-ca0f-0410-8574-be9b3887307d>
2013-11-21 15:58:12 +0000

    Improve test setup and replace sleep with assertBy
    git-svn-id: http://svn.terracotta.org/svn/ehcache@8475 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/constructs/refreshahead/RefreshAheadCacheTest.java
+++ trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/constructs/refreshahead/RefreshAheadCacheTest.java
@@ -1,11 +1,11 @@
 package net.sf.ehcache.constructs.refreshahead;
 
-import junit.framework.Assert;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Element;
 import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
 import net.sf.ehcache.loader.CacheLoader;
 import net.sf.ehcache.statistics.extended.ExtendedStatistics;
 
@@ -14,183 +14,166 @@ import org.junit.experimental.categories.Category;
 import org.terracotta.test.categories.CheckShorts;
 
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import static net.sf.ehcache.util.RetryAssert.assertBy;
+import static net.sf.ehcache.util.RetryAssert.sleepFor;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 @Category(CheckShorts.class)
 public class RefreshAheadCacheTest {
 
     private static CacheLoader stringifyCacheLoader = new StringifyCacheLoaderFactory().createCacheLoader(null, null);
 
-    private static void sleepySeconds(int secs) {
-        sleepy(secs * 1000);
-    }
-
-    private static void sleepy(int millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-
-        }
-    }
-
-    // OK. we want to create an ehcache, then programmitaclly decorate it with
-    // locks.
     @Test
     public void testSimpleCaseProgrammatic() {
 
-        CacheManager manager = new CacheManager();
-        manager.removeAllCaches();
+        CacheManager manager = new CacheManager(new Configuration().name("programmatic"));
 
-        manager.addCache(new Cache(new CacheConfiguration().name("test").timeToLiveSeconds(10).overflowToDisk(false)
-                .maxElementsInMemory(100)));
-        Ehcache cache = manager.getEhcache("test");
+        try {
+            manager.addCache(new Cache(new CacheConfiguration().name("test").timeToLiveSeconds(10).overflowToDisk(false)
+                    .maxElementsInMemory(100)));
+            Ehcache cache = manager.getEhcache("test");
 
-        RefreshAheadCacheConfiguration refreshConfig = new RefreshAheadCacheConfiguration().timeToRefreshSeconds(7).numberOfThreads(4)
-                .maximumRefreshBacklogItems(100)
-                .build();
-        RefreshAheadCache decorator = new RefreshAheadCache(cache, refreshConfig);
+            RefreshAheadCacheConfiguration refreshConfig = new RefreshAheadCacheConfiguration().timeToRefreshSeconds(3).numberOfThreads(4)
+                    .maximumRefreshBacklogItems(100)
+                    .build();
+            final RefreshAheadCache decorator = new RefreshAheadCache(cache, refreshConfig);
 
-        cache.registerCacheLoader(stringifyCacheLoader);
+            cache.registerCacheLoader(stringifyCacheLoader);
 
-        Set<ExtendedStatistics.Statistic<Number>> offerStat = RefreshAheadCache.findOfferStatistic(decorator);
-        Set<ExtendedStatistics.Statistic<Number>> processedStat = RefreshAheadCache.findProcessedStatistic(decorator);
-        Set<ExtendedStatistics.Statistic<Number>> successStat = RefreshAheadCache.findRefreshedStatistic(decorator);
-        Set<ExtendedStatistics.Statistic<Number>> droppedStat = RefreshAheadCache.findDroppedStatistic(decorator);
-        Set<ExtendedStatistics.Statistic<Number>> backlogStat = RefreshAheadCache.findBacklogStatistic(decorator);
-        Assert.assertFalse(offerStat.isEmpty());
-        Assert.assertFalse(processedStat.isEmpty());
-        Assert.assertFalse(droppedStat.isEmpty());
-        Assert.assertFalse(successStat.isEmpty());
-        Assert.assertFalse(backlogStat.isEmpty());
+            Set<ExtendedStatistics.Statistic<Number>> offerStat = RefreshAheadCache.findOfferStatistic(decorator);
+            Set<ExtendedStatistics.Statistic<Number>> processedStat = RefreshAheadCache.findProcessedStatistic(decorator);
+            Set<ExtendedStatistics.Statistic<Number>> successStat = RefreshAheadCache.findRefreshedStatistic(decorator);
+            Set<ExtendedStatistics.Statistic<Number>> droppedStat = RefreshAheadCache.findDroppedStatistic(decorator);
+            Set<ExtendedStatistics.Statistic<Number>> backlogStat = RefreshAheadCache.findBacklogStatistic(decorator);
+            assertFalse(offerStat.isEmpty());
+            assertFalse(processedStat.isEmpty());
+            assertFalse(droppedStat.isEmpty());
+            assertFalse(successStat.isEmpty());
+            assertFalse(backlogStat.isEmpty());
 
-        decorator.put(new Element(new Integer(1), new String("1")));
-        decorator.put(new Element(new Integer(2), new String("2")));
-        decorator.put(new Element(new Integer(3), new String("3")));
-        decorator.put(new Element(new Integer(4), new String("4")));
+            final Integer key1 = 1;
+            decorator.put(new Element(key1, "1"));
+            decorator.put(new Element(2, "2"));
+            decorator.put(new Element(3, "3"));
+            decorator.put(new Element(4, "4"));
 
-        // get the first one
-        Element got = decorator.get(new Integer(1));
-        long creationTime = got.getCreationTime();
-        Assert.assertNotNull(got);
-        Assert.assertEquals(0, decorator.getRefreshSuccessCount());
+            // get the first one
+            Element got = decorator.get(key1);
+            long creationTime = got.getCreationTime();
+            assertNotNull(got);
+            assertEquals(0, decorator.getRefreshSuccessCount());
 
-        sleepySeconds(1);
-        // now, you should get the same one, no refresh ahead
-        got = decorator.get(new Integer(1));
-        Assert.assertNotNull(got);
-        Assert.assertEquals(0, decorator.getRefreshSuccessCount());
-        Assert.assertEquals(0, successStat.iterator().next().value().longValue());
-        Assert.assertEquals(got.getCreationTime(), creationTime);
+            sleepFor(1, TimeUnit.SECONDS);
+            // now, you should get the same one, no refresh ahead
+            got = decorator.get(key1);
+            assertNotNull(got);
+            assertEquals(0, decorator.getRefreshSuccessCount());
+            assertEquals(0, successStat.iterator().next().value().longValue());
+            assertEquals(got.getCreationTime(), creationTime);
 
-        // wait long enough for refresh ahead to trigger. 7+1=8 seconds
-        // you'll get the original back right away, but a call done soon should get a new one.
-        sleepySeconds(7);
-        got = decorator.get(new Integer(1));
-        Assert.assertNotNull(got);
-
-        // processing time,
-        sleepySeconds(1);
-        // better be a new object this time, faulted in via refresh ahead
-        // better not have the same creation time as originally
-        got = decorator.get(new Integer(1));
-        Assert.assertEquals(1, decorator.getRefreshSuccessCount());
-        Assert.assertEquals(1, successStat.iterator().next().value().longValue());
-        Assert.assertEquals(1, processedStat.iterator().next().value().longValue());
-        Assert.assertEquals(0, droppedStat.iterator().next().value().longValue());
-        Assert.assertEquals(0, backlogStat.iterator().next().value().longValue());
-        Assert.assertFalse(creationTime == got.getCreationTime());
-
-        manager.removeAllCaches();
-        manager.shutdown();
+            // wait long enough for refresh ahead to trigger.
+            assertBy(5, TimeUnit.SECONDS, new Callable<Element>() {
+                @Override
+                public Element call() throws Exception {
+                    return decorator.get(key1);
+                }
+            }, not(sameInstance(got)));
+            got = decorator.get(key1);
+            assertEquals(1, decorator.getRefreshSuccessCount());
+            assertEquals(1, successStat.iterator().next().value().longValue());
+            assertEquals(1, processedStat.iterator().next().value().longValue());
+            assertEquals(0, droppedStat.iterator().next().value().longValue());
+            assertEquals(0, backlogStat.iterator().next().value().longValue());
+            assertFalse(creationTime == got.getCreationTime());
+        } finally {
+            manager.shutdown();
+        }
     }
 
     @Test
     public void testSimpleCaseXML() {
 
-        CacheManager manager = new CacheManager();
-        manager.removeAllCaches();
-        manager.shutdown();
-
         CacheManager cacheManager = new CacheManager(getClass().getResourceAsStream
                 ("/ehcache-refresh-ahead-simple.xml"));
 
-        Ehcache decorator=cacheManager.getEhcache("testRefreshAhead1");
+        try {
+            final Ehcache decorator = cacheManager.getEhcache("testRefreshAhead1");
 
-        decorator.put(new Element(new Integer(1), new String("1")));
-        decorator.put(new Element(new Integer(2), new String("2")));
-        decorator.put(new Element(new Integer(3), new String("3")));
-        decorator.put(new Element(new Integer(4), new String("4")));
+            final Integer key1 = 1;
+            decorator.put(new Element(key1, "1"));
+            decorator.put(new Element(2, "2"));
+            decorator.put(new Element(3, "3"));
+            decorator.put(new Element(4, "4"));
 
-        // get the first one
-        Element got = decorator.get(new Integer(1));
-        long creationTime = got.getCreationTime();
-        Assert.assertNotNull(got);
+            // get the first one
+            Element got = decorator.get(key1);
+            long creationTime = got.getCreationTime();
+            assertNotNull(got);
 
-        sleepySeconds(1);
-        // now, you should get the same one, no refresh ahead
-        got = decorator.get(new Integer(1));
-        Assert.assertNotNull(got);
-        Assert.assertEquals(got.getCreationTime(), creationTime);
+            sleepFor(1, TimeUnit.SECONDS);
+            // now, you should get the same one, no refresh ahead
+            got = decorator.get(key1);
+            assertNotNull(got);
+            assertEquals(got.getCreationTime(), creationTime);
 
-        // wait long enough for refresh ahead to trigger. 7+1=8 seconds
-        // you'll get the original back right away, but a call done soon should get a new one.
-        sleepySeconds(7);
-        got = decorator.get(new Integer(1));
-        Assert.assertNotNull(got);
+            assertBy(5, TimeUnit.SECONDS, new Callable<Element>() {
+                @Override
+                public Element call() throws Exception {
+                    return decorator.get(key1);
+                }
+            }, not(sameInstance(got)));
+            // better not have the same creation time as originally
+            got = decorator.get(key1);
+            assertFalse(creationTime == got.getCreationTime());
 
-        // processing time,
-        sleepySeconds(1);
-        // better be a new object this time, faulted in via refresh ahead
-        // better not have the same creation time as originally
-        got = decorator.get(new Integer(1));
-        Assert.assertFalse(creationTime == got.getCreationTime());
-
-        cacheManager.shutdown();
-
+        } finally {
+            cacheManager.shutdown();
+        }
     }
 
     @Test
     public void testSimpleCaseXMLNullEvicts() {
 
-        CacheManager manager = new CacheManager();
-        manager.removeAllCaches();
-        manager.shutdown();
-
         CacheManager cacheManager = new CacheManager(getClass().getResourceAsStream
                 ("/ehcache-refresh-ahead-simple.xml"));
 
-        Ehcache decorator=cacheManager.getEhcache("testRefreshAhead2");
+        try {
+            final Ehcache decorator=cacheManager.getEhcache("testRefreshAhead2");
 
 
-        decorator.put(new Element(new Integer(1), new String("1")));
-        decorator.put(new Element(new Integer(2), new String("2")));
-        decorator.put(new Element(new Integer(3), new String("3")));
-        decorator.put(new Element(new Integer(4), new String("4")));
+            final Integer key1 = 1;
+            decorator.put(new Element(key1, "1"));
+            decorator.put(new Element(2, "2"));
+            decorator.put(new Element(3, "3"));
+            decorator.put(new Element(4, "4"));
 
-        // get the first one
-        Element got = decorator.get(new Integer(1));
-        long creationTime = got.getCreationTime();
-        Assert.assertNotNull(got);
+            // get the first one
+            Element got = decorator.get(key1);
+            long creationTime = got.getCreationTime();
+            assertNotNull(got);
 
-        sleepySeconds(1);
-        // now, you should get the same one, no refresh ahead
-        got = decorator.get(new Integer(1));
-        Assert.assertNotNull(got);
-        Assert.assertEquals(got.getCreationTime(), creationTime);
+            sleepFor(1, TimeUnit.SECONDS);
+            // now, you should get the same one, no refresh ahead
+            got = decorator.get(key1);
+            assertNotNull(got);
+            assertEquals(got.getCreationTime(), creationTime);
 
-        // wait long enough for refresh ahead to trigger. 7+1=8 seconds
-        // you'll get the original back right away, but a call done soon should get a new one.
-        sleepySeconds(7);
-        got = decorator.get(new Integer(1));
-        Assert.assertNotNull(got);
-
-        // processing time,
-        sleepySeconds(1);
-        // better be a new object this time, faulted in via refresh ahead
-        // better not have the same creation time as originally
-        got = decorator.get(new Integer(1));
-        Assert.assertNull(got);
-
-        cacheManager.shutdown();
-
+            assertBy(5, TimeUnit.SECONDS, new Callable<Element>() {
+                @Override
+                public Element call() throws Exception {
+                    return decorator.get(key1);
+                }
+            }, nullValue());
+        } finally {
+            cacheManager.shutdown();
+        }
     }
 }
