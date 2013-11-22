0fa5aafe159c496580856b5ed41cdde19769a1d5
cschanck <cschanck@b9324663-ca0f-0410-8574-be9b3887307d>
2013-11-22 14:49:56 +0000

    MNK-5808 Mod test to be less timing dependent, so as not to barf on new Monkeys.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@8484 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/constructs/refreshahead/ThreadedWorkQueueTest.java
+++ trunk/ehcache/ehcache-core/src/test/java/net/sf/ehcache/constructs/refreshahead/ThreadedWorkQueueTest.java
@@ -1,81 +1,97 @@
 package net.sf.ehcache.constructs.refreshahead;
 
+import net.sf.ehcache.constructs.refreshahead.ThreadedWorkQueue.BatchWorker;
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.Collection;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import junit.framework.Assert;
-import net.sf.ehcache.constructs.refreshahead.ThreadedWorkQueue.BatchWorker;
-
-import org.junit.Test;
 
 public class ThreadedWorkQueueTest {
 
-    ThreadFactory daemonFactory = new ThreadFactory() {
+  ThreadFactory daemonFactory = new ThreadFactory() {
 
-        @Override
-        public Thread newThread(Runnable arg0) {
-            Thread t = new Thread(arg0, "test");
-            t.setDaemon(true);
-            return t;
+    @Override
+    public Thread newThread(Runnable arg0) {
+      Thread t = new Thread(arg0, "test");
+      t.setDaemon(true);
+      return t;
+    }
+  };
+
+  @Test
+  public void testInitShutdown() {
+    ThreadedWorkQueue<Integer> queue = new ThreadedWorkQueue<Integer>(null, 4, daemonFactory, 1000, 10);
+    Assert.assertTrue(queue.isAlive());
+    queue.shutdown();
+    Assert.assertFalse(queue.isAlive());
+  }
+
+  @Test
+  public void TestSimpleProcessing() {
+    final AtomicInteger counter = new AtomicInteger(0);
+    final BatchWorker<Integer> nothingWorker = new BatchWorker<Integer>() {
+      @Override
+      public void process(Collection<? extends Integer> collection) {
+        counter.addAndGet(collection.size());
+      }
+    };
+    ThreadedWorkQueue<Integer> queue = new ThreadedWorkQueue<Integer>(nothingWorker, 4, daemonFactory, 1000, 10);
+
+    for (int i = 0; i < 100; i++) {
+      queue.offer(new Integer(i));
+    }
+    for (; queue.getBacklogCount() > 0; ) {
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+      }
+    }
+    Assert.assertTrue(counter.get() == 100);
+    queue.shutdown();
+  }
+
+  @Test
+  public void TestOverflowProcessing() {
+    final AtomicInteger counter = new AtomicInteger(0);
+    // use pause to force a delay, sos ome things fall off.
+    final AtomicBoolean pause = new AtomicBoolean(true);
+
+    // create a worker
+    final BatchWorker<Integer> counterWorker = new BatchWorker<Integer>() {
+      @Override
+      public void process(Collection<? extends Integer> collection) {
+        counter.addAndGet(collection.size());
+        while(pause.get()) {
+          Thread.yield();
         }
+      }
     };
 
-    @Test
-    public void testInitShutdown() {
-        ThreadedWorkQueue<Integer> queue = new ThreadedWorkQueue<Integer>(null, 4, daemonFactory, 1000, 10);
-        Assert.assertTrue(queue.isAlive());
-        queue.shutdown();
-        Assert.assertFalse(queue.isAlive());
+    // create the queue, add stuff.
+    ThreadedWorkQueue<Integer> queue = new ThreadedWorkQueue<Integer>(counterWorker, 1, daemonFactory, 1000, 1);
+    final int TESTSIZE = 10000;
+    for (int i = 0; i < TESTSIZE; i++) {
+      if(i==100) {
+        // let the queue process now. Things have fallen off.
+        pause.set(false);
+      }
+      queue.offer(new Integer(i));
     }
 
-    @Test
-    public void TestSimpleProcessing() {
-        final AtomicInteger counter = new AtomicInteger(0);
-        final BatchWorker<Integer> nothingWorker = new BatchWorker<Integer>() {
-            @Override
-            public void process(Collection<? extends Integer> collection) {
-                counter.addAndGet(collection.size());
-            }
-        };
-        ThreadedWorkQueue<Integer> queue = new ThreadedWorkQueue<Integer>(nothingWorker, 4, daemonFactory, 1000, 10);
-
-        for (int i = 0; i < 100; i++) {
-            queue.offer(new Integer(i));
-        }
-        for (; queue.getBacklogCount() > 0;) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-            }
-        }
-        Assert.assertTrue(counter.get() == 100);
-        queue.shutdown();
+    // wait for done.
+    for (; queue.getBacklogCount() > 0; ) {
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+      }
     }
 
-    @Test
-    public void TestOverflowProcessing() {
-        final AtomicInteger counter = new AtomicInteger(0);
-        final BatchWorker<Integer> counterWorker = new BatchWorker<Integer>() {
-            @Override
-            public void process(Collection<? extends Integer> collection) {
-                counter.addAndGet(collection.size());
-            }
-        };
-        ThreadedWorkQueue<Integer> queue = new ThreadedWorkQueue<Integer>(counterWorker, 1, daemonFactory, 1000, 1);
-        final int TESTSIZE = 10000;
-        for (int i = 0; i < TESTSIZE; i++) {
-            queue.offer(new Integer(i));
-        }
-        for (; queue.getBacklogCount() > 0;) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-            }
-        }
-        // this should be a lot less. I lack time or inclination to figure out
-        // how to do that at the moment. Too many machines/threads/configs.
-        Assert.assertTrue(counter.get() < TESTSIZE);
-        queue.shutdown();
-    }
+    // should have been overflow. lossage.
+    Assert.assertTrue(counter.get() < TESTSIZE);
+    Assert.assertTrue(queue.getDroppedCount()>0);
+    queue.shutdown();
+  }
 }
