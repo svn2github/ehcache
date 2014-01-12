63749f748e6ffd2a03f3c9b4fde9db7666f26b54
brw <brw@b9324663-ca0f-0410-8574-be9b3887307d>
2014-01-12 15:09:39 +0000

    INT-2908 / INT-2909: committing branches for 4.1.1-rc and corresponding updates to 4.1.x.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@8612 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/4.1.1-rc/system-tests/src/test/java/org/terracotta/ehcache/tests/container/hibernate/nontransactional/NonEternalSecondLevelCacheTestServlet.java
@@ -0,0 +1,81 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package org.terracotta.ehcache.tests.container.hibernate.nontransactional;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.stat.SecondLevelCacheStatistics;
+import org.hibernate.stat.Statistics;
+import org.terracotta.ehcache.tests.container.hibernate.BaseClusteredRegionFactoryTestServlet;
+import org.terracotta.ehcache.tests.container.hibernate.domain.Item;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import junit.framework.Assert;
+
+public class NonEternalSecondLevelCacheTestServlet extends BaseClusteredRegionFactoryTestServlet {
+
+  @Override
+  protected void doServer0(HttpSession session, Map<String, String[]> parameters) throws Exception {
+    HibernateUtil.dropAndCreateDatabaseSchema();
+
+    Statistics stats = HibernateUtil.getSessionFactory().getStatistics();
+    stats.setStatisticsEnabled(true);
+    SecondLevelCacheStatistics statistics = stats.getSecondLevelCacheStatistics(Item.class.getName());
+
+    Session s = HibernateUtil.getSessionFactory().openSession();
+    Transaction t = s.beginTransaction();
+    Item i = new Item();
+    i.setName("widget");
+    i.setDescription("A really top-quality, full-featured widget.");
+    s.persist(i);
+    long id = i.getId();
+    t.commit();
+    s.close();
+
+    Assert.assertEquals(1, statistics.getPutCount());
+    Assert.assertEquals(0, statistics.getHitCount());
+
+    s = HibernateUtil.getSessionFactory().openSession();
+    t = s.beginTransaction();
+    s.get(Item.class, id);
+    t.commit();
+    s.close();
+
+    Assert.assertEquals(1, statistics.getPutCount());
+    Assert.assertEquals(1, statistics.getHitCount());
+
+    Thread.sleep(15000);
+
+    s = HibernateUtil.getSessionFactory().openSession();
+    t = s.beginTransaction();
+    s.get(Item.class, id);
+    t.commit();
+    s.close();
+
+    Assert.assertEquals(2, statistics.getPutCount());
+    Assert.assertEquals(1, statistics.getHitCount());
+
+    s = HibernateUtil.getSessionFactory().openSession();
+    t = s.beginTransaction();
+    s.get(Item.class, id);
+    t.commit();
+    s.close();
+
+    Assert.assertEquals(2, statistics.getPutCount());
+    Assert.assertEquals(2, statistics.getHitCount());
+  }
+
+  @Override
+  protected void doServer1(HttpSession session, Map<String, String[]> parameters) throws Exception {
+    Statistics stats = HibernateUtil.getSessionFactory().getStatistics();
+    SecondLevelCacheStatistics cacheStats = stats.getSecondLevelCacheStatistics(Item.class.getName());
+
+    HibernateUtil.getSessionFactory().evictEntity(Item.class.getName());
+
+    Assert.assertEquals(0L, cacheStats.getElementCountInMemory());
+  }
+}
