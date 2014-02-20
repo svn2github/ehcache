475bd42bf45ed068f4262969f15aebc408041432
twu <twu@b9324663-ca0f-0410-8574-be9b3887307d>
2014-02-20 19:24:44 +0000

    MNK-6077: Merged r6776
    git-svn-id: http://svn.terracotta.org/svn/ehcache@8777 b9324663-ca0f-0410-8574-be9b3887307d

--- branches/ehcache-2.8.x/ehcache-core/src/main/java/net/sf/ehcache/config/CacheConfiguration.java
+++ branches/ehcache-2.8.x/ehcache-core/src/main/java/net/sf/ehcache/config/CacheConfiguration.java
@@ -2998,6 +2998,13 @@ public class CacheConfiguration implements Cloneable {
     /**
      * Intended for internal use only, and subject to change.
      */
+    public void internalSetEternal(boolean eternal) {
+        this.eternal = eternal;
+    }
+
+    /**
+     * Intended for internal use only, and subject to change.
+     */
     public void internalSetMemCapacity(int capacity) {
         this.maxEntriesLocalHeap = capacity;
     }
