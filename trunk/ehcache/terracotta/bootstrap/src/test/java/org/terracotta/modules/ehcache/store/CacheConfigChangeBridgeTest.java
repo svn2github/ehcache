b47c6e2a9091b1d316d0eb06c767230cfcbe7919
twu <twu@b9324663-ca0f-0410-8574-be9b3887307d>
2014-02-20 00:36:25 +0000

    Handle the case of the empty toolkit cache configuration.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@8772 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/terracotta/bootstrap/src/test/java/org/terracotta/modules/ehcache/store/CacheConfigChangeBridgeTest.java
+++ trunk/ehcache/terracotta/bootstrap/src/test/java/org/terracotta/modules/ehcache/store/CacheConfigChangeBridgeTest.java
@@ -3,15 +3,23 @@ package org.terracotta.modules.ehcache.store;
 import net.sf.ehcache.config.CacheConfiguration;
 import org.junit.Before;
 import org.junit.Test;
+import org.terracotta.toolkit.config.AbstractConfiguration;
 import org.terracotta.toolkit.config.Configuration;
 import org.terracotta.toolkit.events.ToolkitNotifier;
 import org.terracotta.toolkit.internal.cache.ToolkitCacheInternal;
 import org.terracotta.toolkit.store.ToolkitConfigFields;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 /**
@@ -22,11 +30,11 @@ public class CacheConfigChangeBridgeTest {
   private ToolkitCacheInternal backend;
   private CacheConfiguration cacheConfiguration;
   private ToolkitNotifier notifier;
-  private Configuration toolkitCacheConfig;
+  private TestConfiguration toolkitCacheConfig;
 
   @Before
   public void setUp() throws Exception {
-    toolkitCacheConfig = mock(Configuration.class);
+    toolkitCacheConfig = new TestConfiguration();
     backend = when(mock(ToolkitCacheInternal.class).getConfiguration())
         .thenReturn(toolkitCacheConfig).getMock();
     cacheConfiguration = spy(new CacheConfiguration());
@@ -38,13 +46,13 @@ public class CacheConfigChangeBridgeTest {
     CacheConfigChangeBridge bridge = new CacheConfigChangeBridge("foo",
         backend, notifier, cacheConfiguration);
 
-    when(toolkitCacheConfig.getInt(ToolkitConfigFields.MAX_TOTAL_COUNT_FIELD_NAME)).thenReturn(123);
-    when(toolkitCacheConfig.getInt(ToolkitConfigFields.MAX_TTI_SECONDS_FIELD_NAME)).thenReturn(321);
-    when(toolkitCacheConfig.getInt(ToolkitConfigFields.MAX_TTL_SECONDS_FIELD_NAME)).thenReturn(456);
-    when(toolkitCacheConfig.getInt(ToolkitConfigFields.MAX_COUNT_LOCAL_HEAP_FIELD_NAME)).thenReturn(42);
-    when(toolkitCacheConfig.getLong(ToolkitConfigFields.MAX_BYTES_LOCAL_HEAP_FIELD_NAME)).thenReturn(1L);
-    when(toolkitCacheConfig.getLong(ToolkitConfigFields.MAX_BYTES_LOCAL_OFFHEAP_FIELD_NAME)).thenReturn(2L);
-    when(toolkitCacheConfig.getBoolean(ToolkitConfigFields.OFFHEAP_ENABLED_FIELD_NAME)).thenReturn(true);
+    toolkitCacheConfig.internalSetConfigMapping(ToolkitConfigFields.MAX_TOTAL_COUNT_FIELD_NAME, 123);
+    toolkitCacheConfig.internalSetConfigMapping(ToolkitConfigFields.MAX_TTI_SECONDS_FIELD_NAME, 321);
+    toolkitCacheConfig.internalSetConfigMapping(ToolkitConfigFields.MAX_TTL_SECONDS_FIELD_NAME, 456);
+    toolkitCacheConfig.internalSetConfigMapping(ToolkitConfigFields.MAX_COUNT_LOCAL_HEAP_FIELD_NAME, 42);
+    toolkitCacheConfig.internalSetConfigMapping(ToolkitConfigFields.MAX_BYTES_LOCAL_HEAP_FIELD_NAME, 1L);
+    toolkitCacheConfig.internalSetConfigMapping(ToolkitConfigFields.MAX_BYTES_LOCAL_OFFHEAP_FIELD_NAME, 2L);
+    toolkitCacheConfig.internalSetConfigMapping(ToolkitConfigFields.OFFHEAP_ENABLED_FIELD_NAME, true);
 
     bridge.connectConfigs();
 
@@ -56,6 +64,18 @@ public class CacheConfigChangeBridgeTest {
     assertThat(cacheConfiguration.getMaxBytesLocalHeap(), is(1L));
     assertThat(cacheConfiguration.getMaxBytesLocalOffHeap(), is(2L));
     assertThat(cacheConfiguration.isOverflowToOffHeap(), is(true));
+  }
+
+  @Test
+  public void testEmptyConfigFromToolkitCache() throws Exception {
+    CacheConfigChangeBridge bridge = new CacheConfigChangeBridge("foo",
+        backend, notifier, cacheConfiguration);
+
+    bridge.connectConfigs();
+
+    // Empty config so only check for registration of the listener and nothing else.
+    verify(cacheConfiguration).addConfigurationListener(bridge);
+    verifyNoMoreInteractions(cacheConfiguration);
   }
 
   @Test
@@ -72,5 +92,24 @@ public class CacheConfigChangeBridgeTest {
     assertThat(cacheConfiguration.isEternal(), is(true));
     assertThat(cacheConfiguration.getTimeToLiveSeconds(), is(0L));
     assertThat(cacheConfiguration.getTimeToIdleSeconds(), is(0L));
+  }
+
+  private static class TestConfiguration extends AbstractConfiguration {
+    private final Map<String, Serializable> map = new HashMap<String, Serializable>();
+
+    @Override
+    protected void internalSetConfigMapping(final String key, final Serializable value) {
+      map.put(key, value);
+    }
+
+    @Override
+    public Set<String> getKeys() {
+      return map.keySet();
+    }
+
+    @Override
+    public Serializable getObjectOrNull(final String name) {
+      return map.get(name);
+    }
   }
 }
