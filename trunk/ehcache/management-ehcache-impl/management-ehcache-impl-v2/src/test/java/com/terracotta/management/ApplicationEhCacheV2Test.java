96d0d1640763996abd1f90dea8664d8e2f018eef
adahanne <adahanne@b9324663-ca0f-0410-8574-be9b3887307d>
2014-06-20 13:24:22 +0000

    TAB-4715 : fixing jax rs classes scanning test : since we ignore everything jersey related, but, we manually add SseFeature, the tests thought that this class was not found on the classpath. Adding an exception to the rule for SseFeature
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9065 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/management-ehcache-impl/management-ehcache-impl-v2/src/test/java/com/terracotta/management/ApplicationEhCacheV2Test.java
+++ trunk/ehcache/management-ehcache-impl/management-ehcache-impl-v2/src/test/java/com/terracotta/management/ApplicationEhCacheV2Test.java
@@ -1,10 +1,13 @@
 package com.terracotta.management;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
 
+import org.glassfish.jersey.media.sse.SseFeature;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -17,7 +20,28 @@ public class ApplicationEhCacheV2Test extends JerseyApplicationTestCommon {
     ApplicationEhCacheV2 applicationEhCache = new ApplicationEhCacheV2();
     Set<Class<?>> applicationClasses = applicationEhCache.getRestResourceClasses();
     Set<Class<?>> annotatedClasses = annotatedClassesFound();
+    Set<Class<?>> classesToIgnoreDuringComparison = new HashSet<Class<?>>();
+
+    if (applicationClasses.size() > annotatedClasses.size()) {
+      for (Class<?> applicationClass : applicationClasses) {
+        if(!annotatedClasses.contains(applicationClass)) {
+          // one exception so far, because we filter jersey and jackson resources : SSeFeature
+          if (applicationClass.equals(SseFeature.class)) {
+            classesToIgnoreDuringComparison.add(applicationClass);
+            continue;
+          }
+          fail("While scanning the classpath, we could not find " + applicationClass);
+        }
+      }
+    } else {
+      for (Class<?> annotatedClass : annotatedClasses) {
+        if(!applicationClasses.contains(annotatedClass)) {
+          fail("Should  " + annotatedClass + " be added to ApplicationEhCacheV2 ?");
+        }
+      }
+    }
+    applicationClasses.removeAll(classesToIgnoreDuringComparison);
     Assert.assertThat(annotatedClasses, equalTo(applicationClasses));
   }
-
 }
+
