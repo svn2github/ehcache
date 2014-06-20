3ecce5a936ff525790af490612171e94c97a679a
adahanne <adahanne@b9324663-ca0f-0410-8574-be9b3887307d>
2014-06-20 18:41:40 +0000

    TAB-4715 : fixing /configs resources
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9067 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/management-ehcache-v2/src/main/java/net/sf/ehcache/management/resource/CacheManagerConfigEntityV2.java
+++ trunk/ehcache/management-ehcache-v2/src/main/java/net/sf/ehcache/management/resource/CacheManagerConfigEntityV2.java
@@ -1,36 +1,15 @@
 package net.sf.ehcache.management.resource;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import org.terracotta.management.resource.AbstractEntityV2;
-import org.w3c.dom.Element;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-import java.io.StringReader;
-
-import javax.xml.bind.annotation.XmlAnyElement;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * @author brandony
  */
-@XmlRootElement(name = "configuration")
 public class CacheManagerConfigEntityV2 extends AbstractEntityV2 {
   private String cacheManagerName;
 
-  // include this only in JSON
-  @JsonProperty
   private String xml;
 
-  @XmlAttribute
   public String getCacheManagerName() {
     return cacheManagerName;
   }
@@ -39,16 +18,6 @@ public class CacheManagerConfigEntityV2 extends AbstractEntityV2 {
     this.cacheManagerName = cacheManagerName;
   }
 
-  // include this only in XML
-  @XmlAnyElement
-  @JsonIgnore
-  public Element getParsedXml() throws ParserConfigurationException, IOException, SAXException {
-    DocumentBuilderFactory domFact = DocumentBuilderFactory.newInstance();
-    DocumentBuilder domBuilder = domFact.newDocumentBuilder();
-    return domBuilder.parse(new InputSource(new StringReader(xml))).getDocumentElement();
-  }
-
-  @XmlTransient
   public String getXml() {
     return xml;
   }
