6c850194c052b6ff3e51dc921a8941df6298a593
adahanne <adahanne@b9324663-ca0f-0410-8574-be9b3887307d>
2014-07-28 19:30:27 +0000

    TAB-4901 : bumping management version + adapting to new management common resources
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9193 b9324663-ca0f-0410-8574-be9b3887307d

--- trunk/ehcache/management-ehcache-impl/management-ehcache-impl-v1/src/test/java/com/terracotta/management/ApplicationEhCacheTest.java
+++ trunk/ehcache/management-ehcache-impl/management-ehcache-impl-v1/src/test/java/com/terracotta/management/ApplicationEhCacheTest.java
@@ -1,10 +1,12 @@
 package com.terracotta.management;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.fail;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.Set;
 /**
  * @author: Anthony Dahanne
@@ -14,9 +16,26 @@ public class ApplicationEhCacheTest extends JerseyApplicationTestCommon {
   @Test
   public void testGetClasses() throws Exception {
     ApplicationEhCacheV1 applicationEhCache = new ApplicationEhCacheV1();
-    Set<Class<?>> applicationClasses = applicationEhCache.getRestResourceClasses();
+    Set<Class<?>> filteredApplicationClasses = filterClassesFromJaxRSPackages(applicationEhCache.getRestResourceClasses());
+
     Set<Class<?>> annotatedClasses = annotatedClassesFound();
-    Assert.assertThat(annotatedClasses, equalTo(applicationClasses));
+    Set<Class<?>> classesToIgnoreDuringComparison = new HashSet<Class<?>>();
+
+    if (filteredApplicationClasses.size() > annotatedClasses.size()) {
+      for (Class<?> applicationClass : filteredApplicationClasses) {
+        if(!annotatedClasses.contains(applicationClass)) {
+          fail("While scanning the classpath, we could not find " + applicationClass);
+        }
+      }
+    } else {
+      for (Class<?> annotatedClass : annotatedClasses) {
+        if (!filteredApplicationClasses.contains(annotatedClass)) {
+          fail("Should  " + annotatedClass + " be added to ApplicationEhCacheV1 ?");
+        }
+      }
+    }
+    filteredApplicationClasses.removeAll(classesToIgnoreDuringComparison);
+    Assert.assertThat(annotatedClasses, equalTo(filteredApplicationClasses));
   }
 
 }
