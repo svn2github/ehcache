d383c187ade0e35ccc4a23c1cf78070fcca0d7ed
cruise <cruise@b9324663-ca0f-0410-8574-be9b3887307d>
2014-08-11 07:40:13 +0000

    Tag ehcache-2.8.4 from 4.1.4-rc branch
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9238 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ tags/ehcache-2.8.4/ehcache-core/src/test/java/net/sf/ehcache/OverflowCacheTest.java
@@ -0,0 +1,73 @@
+package net.sf.ehcache;
+
+import net.sf.ehcache.config.CacheConfiguration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Alex Snaps
+ */
+public class OverflowCacheTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CacheTest.class.getName());
+
+    private CacheManager manager;
+
+    @Before
+    public void setUp() throws Exception {
+        manager = CacheManager.create();
+    }
+
+    /**
+     * teardown
+     */
+    @After
+    public void tearDown() throws Exception {
+        if (manager != null) {
+            manager.shutdown();
+        }
+    }
+
+
+    /**
+     * Shows the effect of jamming large amounts of puts into a cache that overflows to disk.
+     * The DiskStore should cause puts to back off and avoid an out of memory error.
+     */
+    @Test
+    public void testBehaviourOnDiskStoreBackUp() throws Exception {
+        Cache cache = new Cache(new CacheConfiguration().name("testBehaviourOnDiskStoreBackUp")
+            .maxElementsInMemory(1000)
+            .overflowToDisk(true)
+            .eternal(false)
+            .timeToLiveSeconds(100)
+            .timeToIdleSeconds(200)
+            .diskPersistent(false)
+            .diskExpiryThreadIntervalSeconds(0)
+            .diskSpoolBufferSizeMB(10));
+        manager.addCache(cache);
+
+        assertEquals(0, cache.getStatistics().getLocalHeapSize());
+
+        Element a;
+        int i = 0;
+        try {
+            for (; i < 150000; i++) {
+                String key = i + "";
+                String value = key;
+                a = new Element(key, value + "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+                cache.put(a);
+            }
+        } catch (OutOfMemoryError e) {
+            LOG.info("OutOfMemoryError: " + e.getMessage() + " " + i);
+            fail();
+        }
+    }
+
+
+}
