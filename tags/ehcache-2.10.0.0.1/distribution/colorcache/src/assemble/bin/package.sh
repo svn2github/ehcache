9f705c6a0b66013768cecebb9f0973a3e35f5eb9
cruise <cruise@b9324663-ca0f-0410-8574-be9b3887307d>
2014-09-11 11:28:13 +0000

    Tag ehcache-2.10.0.0.1 from 4.3.0-rc branch
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9320 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ tags/ehcache-2.10.0.0.1/distribution/colorcache/src/assemble/bin/package.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cygwin=false
+if [ `uname | grep CYGWIN` ]; then
+  cygwin=true
+fi
+
+if [ "$JAVA_HOME" = "" ]; then
+  echo "JAVA_HOME is not defined"
+  exit 1
+fi
+
+appname=colorcache
+
+
+unset CDPATH
+root=`dirname $0`/..
+root=`cd $root && pwd`
+
+jetty1=$root/jetty6.1/9081
+jetty2=$root/jetty6.1/9082
+webapp_lib=$root/webapps/$appname/WEB-INF/lib
+
+tc_install_dir=$root/bin/`$root/bin/relative-paths.sh tc_install_dir`
+ehcache_jars_dir=$root/bin/`$root/bin/relative-paths.sh ehcache_jars_dir`
+
+# package ehcache-core and ehcache-terracotta
+cp $ehcache_jars_dir/lib/ehcache*.jar $webapp_lib
+
+# package toolkit runtime. It could be in 2 different places depending on which kit (ehcache vs tc)
+toolkit_runtime=$tc_install_dir/common/terracotta-toolkit*-runtime*.jar
+
+if [ ! -f $toolkit_runtime ]; then
+  # not found under 'common', try 'lib'
+  toolkit_runtime=$ehcache_jars_dir/lib/terracotta-toolkit*-runtime*.jar
+  if [ ! -f $toolkit_runtime ]; then
+    echo "Couldn't locate toolkit runtime jar"
+    exit 1
+  fi
+fi
+
+cp $toolkit_runtime $webapp_lib
+
+if [ $? -eq 0 ]; then
+  echo "Deploying demo..."
+  cp -r $root/webapps $jetty1
+  cp -r $root/webapps $jetty2
+  echo "Done."
+  exit 0
+else
+  echo "Error packaging the sample"
+  exit 1
+fi
