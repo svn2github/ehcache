f26adf5ca41f615ac228266bdc14b6532a880ab1
cruise <cruise@b9324663-ca0f-0410-8574-be9b3887307d>
2014-09-11 11:28:11 +0000

    Delete ehcache-2.10.0.0.1 tag to retag.
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9319 b9324663-ca0f-0410-8574-be9b3887307d

--- tags/ehcache-2.10.0.0.1/ehcache-scheduled-refresh/src/test/java/net/sf/ehcache/constructs/scheduledrefresh/EvenCacheLoader.java
+++
@@ -1,9 +0,0 @@
-package net.sf.ehcache.constructs.scheduledrefresh;
-
-public class EvenCacheLoader extends IncrementingCacheLoader {
-
-    public EvenCacheLoader() {
-        super(true, 20000);
-    }
-
-}
