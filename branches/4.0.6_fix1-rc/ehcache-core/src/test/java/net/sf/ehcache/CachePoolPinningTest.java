2e6b7562545fcb57e68321a263ab980d8f959d2b
speddir <speddir@b9324663-ca0f-0410-8574-be9b3887307d>
2014-09-13 00:17:32 +0000

    TABINF-3984: create branch for 4.0.6_fix1-rc build
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9340 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ branches/4.0.6_fix1-rc/ehcache-core/src/test/java/net/sf/ehcache/CachePoolPinningTest.java
@@ -0,0 +1,122 @@
+/**
+ *  Copyright Terracotta, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sf.ehcache;
+
+import junit.framework.Assert;
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.DiskStoreConfiguration;
+import net.sf.ehcache.config.MemoryUnit;
+import net.sf.ehcache.config.PinningConfiguration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Tests for Cache pinning with pools
+ *
+ * @author Ludovic Orban
+ */
+public class CachePoolPinningTest {
+
+    private static final int ELEMENT_COUNT = 4000;
+
+    private CacheManager cacheManager;
+
+    @Before
+    public void setUp() throws Exception {
+        cacheManager = new CacheManager(
+                new Configuration()
+                        .diskStore(new DiskStoreConfiguration().path(System.getProperty("java.io.tmpdir")))
+                        .maxBytesLocalHeap(100, MemoryUnit.KILOBYTES)
+                        .maxBytesLocalDisk(200, MemoryUnit.KILOBYTES)
+        );
+    }
+
+    @After
+    public void tearDown() {
+        cacheManager.shutdown();
+        cacheManager = null;
+    }
+
+    @Test
+    public void testClassicLru() throws Exception {
+        System.setProperty(Cache.NET_SF_EHCACHE_USE_CLASSIC_LRU, "true");
+        try {
+            testMemoryOnly();
+        } finally {
+            System.setProperty(Cache.NET_SF_EHCACHE_USE_CLASSIC_LRU, "false");
+        }
+    }
+
+    @Test
+    public void testMemoryOnly() throws Exception {
+        cacheManager.addCache(new Cache(
+                new CacheConfiguration()
+                        .name("memoryOnlyCache_inCache")
+                        .overflowToDisk(false)
+                        .pinning(new PinningConfiguration().store(PinningConfiguration.Store.INCACHE))
+        ));
+        doAssertions(cacheManager.getCache("memoryOnlyCache_inCache"), ELEMENT_COUNT, 0);
+    }
+
+    @Test
+    @Ignore
+    public void testOverflowToDisk() throws Exception {
+        cacheManager.addCache(new Cache(
+                new CacheConfiguration()
+                        .name("overflowToDiskCache_inCache")
+                        .pinning(new PinningConfiguration().store(PinningConfiguration.Store.INCACHE))
+        ));
+        doAssertions(cacheManager.getCache("overflowToDiskCache_inCache"), 0, ELEMENT_COUNT);
+    }
+
+    @Test
+    @Ignore
+    public void testDiskPersistent() throws Exception {
+        cacheManager.addCache(new Cache(
+                new CacheConfiguration()
+                        .diskPersistent(true)
+                        .name("diskPersistentCache_inCache")
+                        .pinning(new PinningConfiguration().store(PinningConfiguration.Store.INCACHE))
+        ));
+        doAssertions(cacheManager.getCache("diskPersistentCache_inCache"), 0, ELEMENT_COUNT);
+    }
+
+    private void doAssertions(Cache cache, long expectedMemoryHits, long expectedDiskHits) {
+        for (int i = 0; i < ELEMENT_COUNT; i++) {
+            cache.put(new Element(i, i));
+        }
+
+        Assert.assertEquals(ELEMENT_COUNT, cache.getSize());
+
+        for (int i = 0; i < ELEMENT_COUNT; i++) {
+            assertNotNull(cache.get(i));
+        }
+
+        Assert.assertEquals(expectedMemoryHits, cache.getStatistics().localHeapHitCount());
+        Assert.assertEquals(ELEMENT_COUNT - expectedMemoryHits, cache.getStatistics().localHeapMissCount());
+        Assert.assertEquals(expectedDiskHits, cache.getStatistics().localDiskHitCount());
+        Assert.assertEquals(0, cache.getStatistics().localDiskMissCount());
+    }
+
+}
+
---
+++ branches/4.0.6_fix1-rc/system-tests/src/test/java/org/terracotta/modules/ehcache/writebehind/CacheSeparationWriteBehindTest.java
@@ -0,0 +1,125 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package org.terracotta.modules.ehcache.writebehind;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
+
+import org.terracotta.ehcache.tests.AbstractCacheTestBase;
+import org.terracotta.ehcache.tests.ClientBase;
+import org.terracotta.modules.ehcache.async.AsyncCoordinatorImpl;
+import org.terracotta.toolkit.Toolkit;
+import org.terracotta.toolkit.concurrent.ToolkitBarrier;
+import org.terracotta.toolkit.concurrent.atomic.ToolkitAtomicLong;
+
+import com.tc.l2.L2DebugLogging.LogLevel;
+import com.tc.test.config.model.TestConfig;
+
+import junit.framework.Assert;
+
+public class CacheSeparationWriteBehindTest extends AbstractCacheTestBase {
+
+  private static final int NODE_COUNT = 2;
+
+  public CacheSeparationWriteBehindTest(TestConfig testConfig) {
+    super("cache-separation-writebehind-test.xml", testConfig, App.class, App.class);
+    configureTCLogging(AsyncCoordinatorImpl.class.getName(), LogLevel.DEBUG);
+  }
+
+  public static class App extends ClientBase {
+
+    private final ToolkitBarrier     barrier;
+
+    final ToolkitAtomicLong totalWriteCount1;
+    final ToolkitAtomicLong totalDeleteCount1;
+    final ToolkitAtomicLong totalWriteCount2;
+    final ToolkitAtomicLong totalDeleteCount2;
+
+    public App(String[] args) {
+      super(args);
+      this.barrier = getClusteringToolkit().getBarrier("barrier", NODE_COUNT);
+      this.totalWriteCount1 = getClusteringToolkit().getAtomicLong("long1");
+      this.totalDeleteCount1 = getClusteringToolkit().getAtomicLong("long2");
+      this.totalWriteCount2 = getClusteringToolkit().getAtomicLong("long3");
+      this.totalDeleteCount2 = getClusteringToolkit().getAtomicLong("long3");
+    }
+
+    public static void main(String[] args) {
+      new App(args).run();
+    }
+
+    @Override
+    protected void runTest(Cache cache, Toolkit clusteringToolkit) throws Throwable {
+      final int index = barrier.await();
+
+      Cache cache1 = cacheManager.getCache("test1");
+      Cache cache2 = cacheManager.getCache("test2");
+
+      WriteBehindCacheWriter writer1;
+      WriteBehindCacheWriter writer2;
+
+      if (0 == index) {
+        writer1 = new WriteBehindCacheWriter("WriteBehindCacheWriter1", index, 20L);
+        cache1.registerCacheWriter(writer1);
+        writer2 = new WriteBehindCacheWriter("WriteBehindCacheWriter2", index, 20L);
+        cache2.registerCacheWriter(writer2);
+
+        for (int i = 0; i < 1000; i++) {
+          cache1.putWithWriter(new Element("key" + i % 200, "value" + i));
+          if (0 == i % 10) {
+            cache1.removeWithWriter("key" + i % 200 / 10);
+          }
+        }
+
+        for (int i = 0; i < 100; i++) {
+          cache2.putWithWriter(new Element("key" + i % 200, "value" + i));
+          if (0 == i % 10) {
+            cache2.removeWithWriter("key" + i % 200 / 10);
+          }
+        }
+      } else {
+        writer1 = new WriteBehindCacheWriter("WriteBehindCacheWriter1", index, 10L);
+        cache1.registerCacheWriter(writer1);
+        writer2 = new WriteBehindCacheWriter("WriteBehindCacheWriter2", index, 10L);
+        cache2.registerCacheWriter(writer2);
+
+        cache1.putWithWriter(new Element("key", "value"));
+        cache1.removeWithWriter("key");
+        cache2.putWithWriter(new Element("key", "value"));
+        cache2.removeWithWriter("key");
+      }
+
+      cache1.dispose();
+      cache2.dispose();
+      barrier.await();
+
+      System.out.println("[Client " + index + " processed " + writer1.getWriteCount() + " writes for writer 1]");
+      System.out.println("[Client " + index + " processed " + writer2.getWriteCount() + " writes for writer 2]");
+      System.out.println("[Client " + index + " processed " + writer1.getDeleteCount() + " deletes for writer 1]");
+      System.out.println("[Client " + index + " processed " + writer2.getDeleteCount() + " deletes for writer 2]");
+
+      totalWriteCount1.addAndGet(writer1.getWriteCount());
+      totalDeleteCount1.addAndGet(writer1.getDeleteCount());
+      totalWriteCount2.addAndGet(writer2.getWriteCount());
+      totalDeleteCount2.addAndGet(writer2.getDeleteCount());
+
+      barrier.await();
+
+      if (0 == index) {
+        System.out.println("[Clients processed a total of " + totalWriteCount1.get() + " writes for writer 1]");
+        System.out.println("[Clients processed a total of " + totalWriteCount2.get() + " writes for writer 2]");
+        System.out.println("[Clients processed a total of " + totalDeleteCount1.get() + " deletes for writer 1]");
+        System.out.println("[Clients processed a total of " + totalDeleteCount2.get() + " deletes for writer 2]");
+
+        Assert.assertEquals(1001, totalWriteCount1.get());
+        Assert.assertEquals(112, totalWriteCount2.get());
+        Assert.assertEquals(101, totalDeleteCount1.get());
+        Assert.assertEquals(112, totalDeleteCount2.get());
+      }
+
+      barrier.await();
+    }
+
+  }
+}
