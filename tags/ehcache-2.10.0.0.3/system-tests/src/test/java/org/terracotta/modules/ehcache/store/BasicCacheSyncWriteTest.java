d26cbdcaa12fab020be3d3321bd44002738bee16
cruise <cruise@b9324663-ca0f-0410-8574-be9b3887307d>
2014-09-29 20:50:13 +0000

    Tag ehcache-2.10.0.0.3 from 4.3.0-rc branch
    git-svn-id: http://svn.terracotta.org/svn/ehcache@9422 b9324663-ca0f-0410-8574-be9b3887307d

---
+++ tags/ehcache-2.10.0.0.3/system-tests/src/test/java/org/terracotta/modules/ehcache/store/BasicCacheSyncWriteTest.java
@@ -0,0 +1,71 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package org.terracotta.modules.ehcache.store;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
+
+import org.terracotta.toolkit.Toolkit;
+import org.terracotta.toolkit.concurrent.ToolkitBarrier;
+import org.terracotta.ehcache.tests.AbstractCacheTestBase;
+import org.terracotta.ehcache.tests.ClientBase;
+
+import com.tc.test.config.model.TestConfig;
+
+import junit.framework.Assert;
+
+public class BasicCacheSyncWriteTest extends AbstractCacheTestBase {
+
+  private static final int NODE_COUNT = 3;
+
+  public BasicCacheSyncWriteTest(TestConfig testConfig) {
+    super("basic-cache-sync-write-test.xml", testConfig, App.class, App.class, App.class);
+  }
+
+  public static class App extends ClientBase {
+    private final ToolkitBarrier barrier;
+
+    public App(String[] args) {
+      super(args);
+      this.barrier = getClusteringToolkit().getBarrier("test-barrier", NODE_COUNT);
+    }
+
+    public static void main(String[] args) {
+      new App(args).run();
+    }
+
+    @Override
+    protected void runTest(Cache cache, Toolkit clusteringToolkit) throws Throwable {
+      final int index = barrier.await();
+
+      Assert.assertTrue(cache.getCacheConfiguration().getTerracottaConfiguration().isSynchronousWrites());
+
+      Assert.assertEquals(0, cache.getSize());
+
+      barrier.await();
+
+      if (index == 0) {
+        cache.put(new Element("key", "value"));
+      }
+
+      barrier.await();
+
+      Assert.assertEquals(1, cache.getSize());
+      Assert.assertEquals("value", cache.get("key").getObjectValue());
+
+      barrier.await();
+
+      if (index == 0) {
+        boolean removed = cache.remove("key");
+        Assert.assertTrue(removed);
+      }
+
+      barrier.await();
+
+      Assert.assertEquals(0, cache.getSize());
+    }
+
+  }
+
+}
